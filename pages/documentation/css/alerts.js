33ce59c2b7cac1fd27f1f59d8f3231d9edaa669e
Sajad <nawabisajadahmad@gmail.com>
2021-02-08 23:45:19 +0430

    Add css alerts component

---
+++ pages/documentation/css/alerts.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Head from 'next/head';
+import Sidebar from 'components/Documentation/Sidebar.js';
+import routes from 'routes.js';
+import Navbar from 'components/Documentation/Navbar.js';
+import Footer from 'components/Documentation/Footer.js';
+
+import AlertsCode from 'components/Documentation/CSS/AlertsCode.js';
+
+import Heading from 'components/Documentation/Heading.js';
+import SmallHeading from 'components/Documentation/SmallHeading.js';
+
+export default function Alerts() {
+  const [copy, setCopy] = React.useState(null);
+  return (
+    <>
+      <Head>
+        {/* Canonical SEO */}
+        <link
+          rel="canonical"
+          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/css/alerts"
+        />
+        {/* Open Graph data */}
+        <meta
+          property="og:url"
+          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/css/alerts"
+        />
+        <title>CSS Alerts | Tailwind Starter Kit by Creative Tim</title>
+      </Head>
+      <Navbar fixed />
+      <div className="container mx-auto mt-4 pt-12">
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
+            <Sidebar routes={routes} />
+          </div>
+          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
+            <div className="my-8">
+              <Heading
+                title="Alerts"
+                description="Elements that provide contextual feedback messages for user actions.
+          The notification is a simple colored block meant to draw the
+          attention to the user about something."
+              />
+              <div className="mb-8">
+                <SmallHeading
+                  title="Alert Examples"
+                  description={
+                    <>
+                      Alerts can have how many words you want, as well as an
+                      optional close button. For styling, use one of the color
+                      classes presented below. (e.g.,{' '}
+                      <code className="text-pink-600 text-sm bg-gray-200">
+                        .bg-red-500
+                      </code>
+                      ).
+                    </>
+                  }
+                />
+              </div>
+              <AlertsCode
+                copyText={copy === 'Alerts' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Alerts' : '')}
+              />
+            </div>
+          </div>
+          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
--- routes.js
+++ routes.js
@@ -14,6 +14,10 @@ const routes = [
     path: '/documentation/css/alerts',
     routes: [
       {
+        path: '/documentation/css/alerts',
+        name: 'Alerts',
+      },
+      {
         name: 'Buttons',
         state: 'buttonsState',
         subRoutes: [
