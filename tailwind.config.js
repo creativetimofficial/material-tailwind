41b2d1494b1aec3313ec12b4f12ea3a927512a29
Sajad <nawabisajadahmad@gmail.com>
2021-02-12 14:25:37 +0430

    Update material design 2 shadow values for components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,31 @@
+const umbra = 0.2;
+const penumbra = 0.14;
+const ambient = 0.12;
+const black = '0,0,0';
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
     boxShadow: {
       /* Shadow 1dp */
-      sm:
-        '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
+      sm: `0 1px 1px 0 rgba(${black}, ${penumbra}), 0 2px 1px -1px rgba(${black}, ${umbra}), 0 1px 3px 0 rgba(${black}, ${ambient})`,
       /* Shadow 2dp */
-      DEFAULT:
-        '0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)',
+      DEFAULT: `0 2px 2px 0 rgba(${black}, ${penumbra}), 0 3px 1px -2px rgba(${black}, ${umbra}), 0 1px 5px 0 rgba(${black}, ${ambient})`,
       /* Shadow 3dp */
-      md:
-        '0px 1px 8px 0px rgba(0, 0, 0, 0.2), 0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 3px 3px -2px rgba(0, 0, 0, 0.12)',
+      md: `0 3px 4px 0 rgba(${black}, ${penumbra}), 0 3px 3px -2px rgba(${black}, ${umbra}), 0 1px 8px 0 rgba(${black}, ${ambient})`,
       /* Shadow 4dp */
-      lg:
-        '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
-      /* Shadow 5dp */
-      xl:
-        '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12)',
+      lg: `0 4px 5px 0 rgba(${black}, ${penumbra}), 0 1px 10px 0 rgba(${black}, ${umbra}), 0 2px 4px -1px rgba(${black}, ${ambient})`,
       /* Shadow 6dp */
-      '2xl':
-        '0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12)',
-      /* Shadow 7dp */
-      '3xl':
-        '0px 4px 5px -2px rgba(0, 0, 0, 0.2), 0px 7px 10px 1px rgba(0, 0, 0, 0.14), 0px 2px 16px 1px rgba(0, 0, 0, 0.12)',
+      xl: `0 6px 10px 0 rgba(${black}, ${penumbra}), 0 1px 18px 0 rgba(${black}, ${umbra}), 0 3px 5px -1px rgba(${black}, ${ambient})`,
       /* Shadow 8dp */
-      '4xl':
-        '0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)',
-      /* Shadow 9dp */
-      '5xl':
-        '0px 5px 6px -3px rgba(0, 0, 0, 0.2), 0px 9px 12px 1px rgba(0, 0, 0, 0.14), 0px 3px 16px 2px rgba(0, 0, 0, 0.12)',
-      /* Shadow 10dp */
-      '6xl':
-        '0px 6px 6px -3px rgba(0, 0, 0, 0.2), 0px 10px 14px 1px rgba(0, 0, 0, 0.14), 0px 4px 18px 3px rgba(0, 0, 0, 0.12)',
-      /* Shadow 11dp */
-      '7xl':
-        '0px 6px 7px -4px rgba(0, 0, 0, 0.2), 0px 11px 15px 1px rgba(0, 0, 0, 0.14), 0px 4px 20px 3px rgba(0, 0, 0, 0.12)',
+      '2xl': `0 8px 10px 1px rgba(${black}, ${penumbra}), 0 3px 14px 2px rgba(${black}, ${umbra}), 0 5px 5px -3px rgba(${black}, ${ambient})`,
       /* Shadow 12dp */
-      '8xl':
-        '0px 7px 8px -4px rgba(0, 0, 0, 0.2), 0px 12px 17px 2px rgba(0, 0, 0, 0.14), 0px 5px 22px 4px rgba(0, 0, 0, 0.12)',
-      /* Shadow 13dp */
-      '9xl':
-        '0px 7px 8px -4px rgba(0, 0, 0, 0.2), 0px 13px 19px 2px rgba(0, 0, 0, 0.14), 0px 5px 24px 4px rgba(0, 0, 0, 0.12)',
-      /* Shadow 14dp */
-      '10xl':
-        '0px 7px 9px -4px rgba(0, 0, 0, 0.2), 0px 14px 21px 2px rgba(0, 0, 0, 0.14), 0px 5px 26px 4px rgba(0, 0, 0, 0.12)',
-      /* Shadow 15dp */
-      '11xl':
-        '0px 8px 9px -5px rgba(0, 0, 0, 0.2), 0px 15px 22px 2px rgba(0, 0, 0, 0.14), 0px 6px 28px 5px rgba(0, 0, 0, 0.12)',
+      '3xl': `0 12px 17px 2px rgba(${black}, ${penumbra}), 0 5px 22px 4px rgba(${black}, ${umbra}), 0 7px 8px -4px rgba(${black}, ${ambient})`,
       /* Shadow 16dp */
-      '12xl':
-        '0px 8px 10px -5px rgba(0, 0, 0, 0.2), 0px 16px 24px 2px rgba(0, 0, 0, 0.14), 0px 6px 30px 5px rgba(0, 0, 0, 0.12)',
-      /* Shadow 17dp */
-      '13xl':
-        '0px 8px 11px -5px rgba(0, 0, 0, 0.2), 0px 17px 26px 2px rgba(0, 0, 0, 0.14), 0px 6px 32px 5px rgba(0, 0, 0, 0.12)',
-      /* Shadow 18dp */
-      '14xl':
-        '0px 9px 11px -5px rgba(0, 0, 0, 0.2), 0px 18px 28px 2px rgba(0, 0, 0, 0.14), 0px 7px 34px 6px rgba(0, 0, 0, 0.12)',
-      /* Shadow 19dp */
-      '15xl':
-        '0px 9px 12px -6px rgba(0, 0, 0, 0.2), 0px 19px 29px 2px rgba(0, 0, 0, 0.14), 0px 7px 36px 6px rgba(0, 0, 0, 0.12)',
-      /* Shadow 20dp */
-      '16xl':
-        '0px 10px 13px -6px rgba(0, 0, 0, 0.2), 0px 20px 31px 3px rgba(0, 0, 0, 0.14), 0px 8px 38px 7px rgba(0, 0, 0, 0.12)',
-      /* Shadow 21dp */
-      '17xl':
-        '0px 10px 13px -6px rgba(0, 0, 0, 0.2), 0px 21px 33px 3px rgba(0, 0, 0, 0.14), 0px 8px 40px 7px rgba(0, 0, 0, 0.12)',
-      /* Shadow 22dp */
-      '18xl':
-        '0px 10px 14px -6px rgba(0, 0, 0, 0.2), 0px 22px 35px 3px rgba(0, 0, 0, 0.14), 0px 8px 42px 7px rgba(0, 0, 0, 0.12)',
-      /* Shadow 23dp */
-      '19xl':
-        '0px 11px 14px -7px rgba(0, 0, 0, 0.2), 0px 23px 36px 3px rgba(0, 0, 0, 0.14), 0px 9px 44px 8px rgba(0, 0, 0, 0.12)',
+      '4xl': `0 16px 24px 2px rgba(${black}, ${penumbra}), 0 6px 30px 5px rgba(${black}, ${umbra}), 0 8px 10px -5px rgba(${black}, ${ambient})`,
       /* Shadow 24dp */
-      '20xl':
-        '0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12)',
+      '5xl': `0 9px 46px 8px rgba(${black}, ${penumbra}), 0 11px 15px -7px rgba(${black}, ${umbra}), 0 24px 38px 3px rgba(${black}, ${ambient})`,
       /* Shadow 0dp */
       none: 'none',
     },
