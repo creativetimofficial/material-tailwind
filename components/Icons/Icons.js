0f36965fb643462febf8a12ad5a4a03b35ffec17
Sajad <nawabisajadahmad@gmail.com>
2021-02-17 19:36:08 +0430

    Add Icons component

---
+++ components/Icons/Icons.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Icon = ({ family = 'material-icons', name, color, size }) => {
+  let iconUI;
+
+  if (family === 'material-icons') {
+    iconUI = (
+      <span
+        className={`${family} text-${color} ${
+          size ? `text-${size}` : 'text-base'
+        } mx-1`}
+      >
+        {name}
+      </span>
+    );
+  } else if (family === 'font-awesome') {
+    iconUI = <i className={`fas ${name} ${color} ${size} mx-1`} />;
+  }
+
+  return iconUI;
+};
+
+export default Icon;
