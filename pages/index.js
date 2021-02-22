8127ac4c9d86f77d28fb6c07755cbb62d75c8897
Sajad <nawabisajadahmad@gmail.com>
2021-02-22 18:24:53 +0430

    Small change to the presentation page

--- pages/index.js
+++ pages/index.js
@@ -437,7 +437,7 @@ export default class Index extends Component {
 
         <div className="p-10 bg-gray-100 border border-gray-300 mb-10">
           <div className="mb-10 mx-96">
-            <h2 className="mb-5 text-2xl font-bold">Paginations</h2>
+            <h2 className="mb-5 text-2xl font-bold">Progress</h2>
 
             <Progress color="pink" value="30" />
             <Progress color="pink" value="50" percentage />
