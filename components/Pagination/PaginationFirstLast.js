d718e1cc1961b0bedf54be4b735baaa51052e0f4
Sajad <nawabisajadahmad@gmail.com>
2021-02-26 19:07:55 +0430

    Update Pagination component

--- components/Pagination/PaginationFirstLast.js
+++ components/Pagination/PaginationFirstLast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'ripple/ripple';
 
 const colors = [
   'blueGray',
@@ -32,7 +33,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm tracking-wider flex w-14 h-8 mx-1 p-0 rounded items-center justify-center leading-tight relative bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm tracking-wider flex w-14 h-8 mx-1 p-0 rounded items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 First
               </a>
@@ -41,7 +43,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 <span className="material-icons">keyboard_arrow_left</span>
               </a>
@@ -50,7 +53,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-${color}-500 text-white shadow-md transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-${color}-500 text-white shadow-md transition-all duration-300`}
+                data-md-ripple={true}
               >
                 1
               </a>
@@ -59,7 +63,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 2
               </a>
@@ -68,7 +73,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 3
               </a>
@@ -77,7 +83,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 4
               </a>
@@ -86,7 +93,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-700 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 5
               </a>
@@ -95,7 +103,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 <span className="material-icons">keyboard_arrow_right</span>
               </a>
@@ -105,7 +114,8 @@ const PaginationFirstLast = ({ color }) => {
               <a
                 href="#pablo"
                 onClick={(e) => e.preventDefault()}
-                className={`text-sm tracking-wider flex w-14 h-8 mx-1 p-0 rounded items-center justify-center leading-tight relative bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                className={`text-sm tracking-wider flex w-14 h-8 mx-1 p-0 rounded items-center justify-center leading-tight relative overflow-hidden bg-transparent text-gray-800 hover:bg-gray-500 hover:bg-opacity-20 focus:bg-gray-500 focus:bg-opacity-20 active:bg-gray-500 active:bg-opacity-40 transition-all duration-300`}
+                data-md-ripple={true}
               >
                 Last
               </a>
