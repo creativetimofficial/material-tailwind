1748af020fa68831dc6452bcbdbc1423f259994e
Sajad <nawabisajadahmad@gmail.com>
2021-03-02 21:49:43 +0430

    Add the Popover component

---
+++ components/Popovers/Popover.js
@@ -0,0 +1,26 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/animations/shift-away.css';
+
+const Popover = forwardRef(({ children, placement, trigger }, ref) => {
+  return (
+    <Tippy
+      content={children}
+      placement={placement}
+      reference={ref}
+      trigger={trigger}
+      animation="shift-away"
+      interactive
+    />
+  );
+});
+
+Popover.propTypes = {
+  children: PropTypes.node,
+  placement: PropTypes.string,
+  // reference: PropTypes.object,
+  trigger: PropTypes.string,
+};
+
+export default Popover;
