db01087b297689e0db746fd05e4969b0b54ed5ef
Sajad <nawabisajadahmad@gmail.com>
2021-03-02 17:07:25 +0430

    Add example template of a navbar with icons only

---
+++ components/Navbar/NavbarIconExample.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Navbar from 'components/Navbar/Navbar';
+import NavbarContainer from 'components/Navbar/NavbarContainer';
+import NavbarWrapper from 'components/Navbar/NavbarWrapper';
+import NavbarBrand from 'components/Navbar/NavbarBrand';
+import NavbarToggler from 'components/Navbar/NavbarToggler';
+import Collapse from 'components/Navbar/Collapse';
+import Nav from 'components/Nav/Nav';
+import NavItem from 'components/Nav/NavItem';
+import Icon from 'components/Icon/Icon';
+import 'ripple/ripple';
+
+const NavbarIconExample = () => {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  return (
+    <Navbar color="pink">
+      <NavbarContainer>
+        <NavbarWrapper>
+          <NavbarBrand color="white">Navbar with Icon</NavbarBrand>
+          <NavbarToggler
+            color="white"
+            onClick={() => setOpenNavbar(!openNavbar)}
+          />
+        </NavbarWrapper>
+
+        <Collapse open={openNavbar}>
+          <Nav navbar>
+            <NavItem
+              active="light"
+              color="white"
+              href="#navbar"
+              data-md-ripple-light={true}
+            >
+              <Icon name="language" size="xl" />
+            </NavItem>
+            <NavItem color="white" href="#navbar" data-md-ripple-light={true}>
+              <Icon name="account_circle" size="xl" />
+            </NavItem>
+            <NavItem color="white" href="#navbar" data-md-ripple-light={true}>
+              <Icon name="settings" size="xl" />
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </NavbarContainer>
+    </Navbar>
+  );
+};
+
+export default NavbarIconExample;
