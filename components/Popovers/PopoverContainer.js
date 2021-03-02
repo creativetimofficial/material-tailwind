e9c7b4f9220e52b09a4b989e59efda1b6e5a8752
Sajad <nawabisajadahmad@gmail.com>
2021-03-02 21:50:00 +0430

    Add the Popover Over component

---
+++ components/Popovers/PopoverContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PopoverContainer = ({ children }) => {
+  return (
+    <div className="bg-white border-0 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded shadow-md">
+      {children}
+    </div>
+  );
+};
+
+PopoverContainer.propTypes = {
+  children: PropTypes.node,
+};
+
+export default PopoverContainer;
