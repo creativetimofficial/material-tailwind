059c0b5fa6d514bc6b1fe3ced91cb9028ad5398d
Sajad <nawabisajadahmad@gmail.com>
2021-03-02 18:02:29 +0430

    Add example template for Pagination with First and Last buttons

---
+++ components/Pagination/PaginationFirstLastExample.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Pagination from './Pagination';
+import PaginationItem from './PaginationItem';
+import Icon from 'components/Icon/Icon';
+import 'ripple/ripple';
+
+const PaginationFirstLastExample = () => {
+  return (
+    <Pagination>
+      <PaginationItem button href="#last" data-md-ripple-dark={true}>
+        First
+      </PaginationItem>
+      <PaginationItem href="#last" data-md-ripple-dark={true}>
+        <Icon name="keyboard_arrow_left" size="md" />
+      </PaginationItem>
+      <PaginationItem color="pink" href="#1" data-md-ripple-light={true}>
+        1
+      </PaginationItem>
+      <PaginationItem href="#2" data-md-ripple-dark={true}>
+        2
+      </PaginationItem>
+      <PaginationItem href="#3" data-md-ripple-dark={true}>
+        3
+      </PaginationItem>
+      <PaginationItem href="#4" data-md-ripple-dark={true}>
+        4
+      </PaginationItem>
+      <PaginationItem href="#5" data-md-ripple-dark={true}>
+        5
+      </PaginationItem>
+      <PaginationItem href="#last" data-md-ripple-dark={true}>
+        <Icon name="keyboard_arrow_right" size="md" />
+      </PaginationItem>
+      <PaginationItem button href="#last" data-md-ripple-dark={true}>
+        Last
+      </PaginationItem>
+    </Pagination>
+  );
+};
+
+export default PaginationFirstLastExample;
