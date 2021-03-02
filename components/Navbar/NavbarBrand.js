5bcef3f97e919acbf2f7f9a388a0ce049204c144
Sajad <nawabisajadahmad@gmail.com>
2021-03-02 17:02:31 +0430

    Update Navbar Brand component

--- components/Navbar/NavbarBrand.js
+++ components/Navbar/NavbarBrand.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NavbarBrand = ({ children, toggler }) => {
+const colors = {
+  white: 'text-white',
+  black: 'text-black',
+  blueGray: 'text-blue-gray-500',
+  gray: 'text-gray-500',
+  brown: 'text-brown-500',
+  deepOrange: 'text-deep-orange-500',
+  orange: 'text-orange-500',
+  amber: 'text-amber-500',
+  yellow: 'text-yellow-500',
+  lime: 'text-lime-500',
+  lightGreen: 'text-light-green-500',
+  green: 'text-green-500',
+  teal: 'text-teal-500',
+  cyan: 'text-cyan-500',
+  lightBlue: 'text-light-blue-500',
+  blue: 'text-blue-500',
+  indigo: 'text-indigo-500',
+  deepPurple: 'text-deep-purple-500',
+  purple: 'text-purple-500',
+  pink: 'text-pink-500',
+  red: 'text-red-500',
+};
+
+const NavbarBrand = ({ children, color }) => {
   return (
-    <div className="w-full flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-      <a
-        className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-        href="#pablo"
-      >
-        {children}
-      </a>
-      <button
-        className="cursor-pointer text-xl leading-none py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-        type="button"
-        onClick={toggler}
-      >
-        <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-      </button>
-    </div>
+    <a
+      className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase ${colors[color]}`}
+      href="#pablo"
+    >
+      {children}
+    </a>
   );
 };
 
 NavbarBrand.propTypes = {
   children: PropTypes.node,
-  toggler: PropTypes.func,
+  color: PropTypes.string,
 };
 
 export default NavbarBrand;
