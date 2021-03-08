cee7d951ba84674f305a99ae9967e49ffa9404e2
Sajad <nawabisajadahmad@gmail.com>
2021-03-08 22:58:43 +0430

    Add inputs for documentation pages

---
+++ pages/documentation/react/inputs.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import Head from 'next/head';
+import Sidebar from 'components/Documentation/Sidebar.js';
+import routes from 'routes.js';
+import Navbar from 'components/Documentation/Navbar.js';
+import Footer from 'components/Documentation/Footer.js';
+import InputCode from 'components/Documentation/JavaScript/React/Inputs/InputCode';
+import InputIconCode from 'components/Documentation/JavaScript/React/Inputs/InputIconCode';
+import Heading from 'components/Documentation/Heading.js';
+import SmallHeading from 'components/Documentation/SmallHeading.js';
+
+export default function Buttons() {
+  const [copy, setCopy] = React.useState(null);
+  return (
+    <>
+      <Head>
+        {/* Canonical SEO */}
+        <link
+          rel="canonical"
+          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+        />
+        {/* Open Graph data */}
+        <meta
+          property="og:url"
+          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+        />
+        <title>React Input | Tailwind Starter Kit by Creative Tim</title>
+      </Head>
+      <Navbar fixed />
+      <div className="container mx-auto mt-4 pt-12">
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
+            <Sidebar routes={routes} />
+          </div>
+          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
+            <div className="my-8">
+              <Heading
+                title="React Input"
+                description="Beautiful Input using React."
+              />
+              <SmallHeading title="Input" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Input"
+                size="regular"
+                outline={false}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Outline Input" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Outline Input"
+                size="regular"
+                outline={true}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Small Input" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Small Input"
+                size="sm"
+                outline={false}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Large Input" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Large Input"
+                size="lg"
+                outline={false}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Small Outline Input" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Small Outline Input"
+                size="sm"
+                outline={true}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Large Outline Input" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Large Outline Input"
+                size="lg"
+                outline={true}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Input with Icon" />
+              <InputIconCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Input with Icon"
+                size="Regular"
+                outline={false}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Outline Input with Icon" />
+              <InputIconCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Outline Input with Icon"
+                size="Regular"
+                outline={true}
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Input with Error" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Input with Error"
+                size="Regular"
+                outline={false}
+                errorMessage="Its an error"
+                error="Its an error"
+              />
+
+              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+
+              <SmallHeading title="Outline Input with Error" />
+              <InputCode
+                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
+                placeholder="Outline Input with Error"
+                size="Regular"
+                outline={true}
+                errorMessage="Its an error"
+                error="Its an error"
+              />
+            </div>
+          </div>
+          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
