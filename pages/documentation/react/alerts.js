d50f0b414ffa360a3794a09024f053230b4d7d43
Sajad <nawabisajadahmad@gmail.com>
2021-03-08 17:37:05 +0430

    Update Alerts code for documentation pages

--- pages/documentation/react/alerts.js
+++ pages/documentation/react/alerts.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-
-import AlertsCode from 'components/Documentation/JavaScript/React/Alerts/AlertsCode.js';
-import ClosingAlertsCode from 'components/Documentation/JavaScript/React/Alerts/ClosingAlertsCode.js';
-
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
+import Sidebar from 'components/Documentation/Sidebar';
+import routes from 'routes';
+import Navbar from 'components/Documentation/Navbar';
+import Footer from 'components/Documentation/Footer';
+import AlertsCode from 'components/Documentation/JavaScript/React/Alerts/AlertsCode';
+import ClosingAlertsCode from 'components/Documentation/JavaScript/React/Alerts/ClosingAlertsCode';
+import Heading from 'components/Documentation/Heading';
+import SmallHeading from 'components/Documentation/SmallHeading';
 
 export default function Alerts() {
   const [copy, setCopy] = React.useState(null);
