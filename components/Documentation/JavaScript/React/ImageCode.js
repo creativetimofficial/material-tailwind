4a51115103d41fe9a59d9fdf2bcad6c3fa95ab2d
Sajad <nawabisajadahmad@gmail.com>
2021-03-08 21:32:11 +0430

    Add Image Code component

---
+++ components/Documentation/JavaScript/React/ImageCode.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import DocsSnippet from 'components/Documentation/DocsSnippet.js';
+import Image from 'components/Image/Image';
+
+export default function ButtonsCode({
+  copyText,
+  onCopy,
+  src,
+  rounded,
+  raised,
+  alt,
+}) {
+  const codeToShow = `import React from "react";
+import Image from "@md-tailwind/react/Image";
+
+export default function Image() {
+  return (
+    <Image
+      src="${src}"
+      rounded={${rounded}}
+      raised={${raised}}
+      alt="${alt}"
+    />
+  )
+}`;
+
+  return (
+    <>
+      <DocsSnippet copyText={copyText} onCopy={onCopy} codeToShow={codeToShow}>
+        <div className="flex justify-center w-full">
+          <div className="w-72">
+            <Image src={src} rounded={rounded} raised={raised} alt={alt} />
+          </div>
+        </div>
+      </DocsSnippet>
+    </>
+  );
+}
