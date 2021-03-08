ed3e6669f0a50b894edcc5c8439ee6e8fd45c17a
Sajad <nawabisajadahmad@gmail.com>
2021-03-08 22:58:02 +0430

    Add Input Icon Code component

---
+++ components/Documentation/JavaScript/React/Inputs/InputIconCode.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import DocsSnippet from 'components/Documentation/DocsSnippet.js';
+import InputIcon from 'components/Input/InputIcon';
+
+export default function InputIconCode({
+  copyText,
+  onCopy,
+  placeholder,
+  size,
+  outline,
+}) {
+  const [color, setColor] = useState('pink');
+  const [type, setType] = useState('react');
+
+  const codeToShow = `import React from "react";
+import InputIcon from "@md-tailwind/react/InputIcon";
+
+export default function Input() {
+  return (
+    <InputIcon
+      type="text"
+      color="${color}"
+      size="${size}"
+      outline={${outline}}
+      placeholder="${placeholder}"
+      iconFamily="material-icons"
+      iconName="person"
+    />
+  )
+}`;
+
+  return (
+    <>
+      <DocsSnippet
+        copyText={copyText}
+        onCopy={onCopy}
+        activeColor={color}
+        activeFramework={type}
+        codeToShow={codeToShow}
+        onColorClick={(color) => setColor(color)}
+        onFrameworkClick={(type) => setType(type)}
+      >
+        <div className="w-72">
+          <InputIcon
+            type="text"
+            color={color}
+            size={size}
+            outline={outline}
+            placeholder={placeholder}
+            iconFamily="material-icons"
+            iconName="person"
+          />
+        </div>
+      </DocsSnippet>
+    </>
+  );
+}
