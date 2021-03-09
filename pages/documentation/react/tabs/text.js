6a1780da3028b0f2959aec612bd51ecf56038250
Sajad <nawabisajadahmad@gmail.com>
2021-03-09 18:47:07 +0430

    Update tab with icons code for documentation pages

--- pages/documentation/react/tabs/text.js
+++ pages/documentation/react/tabs/text.js
@@ -1,15 +1,13 @@
-import React from "react";
-import Head from "next/head";
-import Sidebar from "components/Documentation/Sidebar.js";
-import routes from "routes.js";
-import Navbar from "components/Documentation/Navbar.js";
-import Footer from "components/Documentation/Footer.js";
+import React from 'react';
+import Head from 'next/head';
+import Sidebar from 'components/Documentation/Sidebar';
+import routes from 'routes';
+import Navbar from 'components/Documentation/Navbar';
+import Footer from 'components/Documentation/Footer';
+import TabTextCode from 'components/Documentation/JavaScript/React/Tabs/TabTextCode';
+import Heading from 'components/Documentation/Heading';
 
-import TabsCode from "components/Documentation/JavaScript/React/TabsCode.js";
-
-import Heading from "components/Documentation/Heading.js";
-
-export default function TabsText() {
+export default function SmallModal() {
   const [copy, setCopy] = React.useState(null);
   return (
     <>
@@ -17,14 +15,14 @@ export default function TabsText() {
         {/* Canonical SEO */}
         <link
           rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/tabs/text"
+          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/small"
         />
         {/* Open Graph data */}
         <meta
           property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/tabs/text"
+          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/small"
         />
-        <title>React Dynamic Tabs | Tailwind Starter Kit by Creative Tim</title>
+        <title>React Tabs | Tailwind Starter Kit by Creative Tim</title>
       </Head>
       <Navbar fixed />
       <div className="container mx-auto mt-4 pt-12">
@@ -35,12 +33,13 @@ export default function TabsText() {
           <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
             <div className="my-8">
               <Heading
-                title="React Text Tabs"
+                title="React Tab Text"
                 description="React navigation component with menu items and content."
               />
-              <TabsCode
-                copyText={copy === "Code" ? "Copied" : "Copy"}
-                onCopy={text => setCopy(text ? "Code" : "")}
+
+              <TabTextCode
+                copyText={copy === 'Modal' ? 'Copied' : 'Copy'}
+                onCopy={(text) => setCopy(text ? 'Modal' : '')}
               />
             </div>
           </div>
