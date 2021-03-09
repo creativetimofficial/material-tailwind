c2073882e89bd5c978e8c06abfcce50760b9ac38
Sajad <nawabisajadahmad@gmail.com>
2021-03-09 12:20:26 +0430

    Update Navbar Links Code component

--- components/Documentation/JavaScript/React/Navbars/NavbarLinksCode.js
+++ components/Documentation/JavaScript/React/Navbars/NavbarLinksCode.js
@@ -1,204 +1,159 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import DocsSnippet from 'components/Documentation/DocsSnippet.js';
+import Frameworks from 'components/Documentation/Frameworks.js';
+import Navbar from 'components/Navbar/Navbar';
+import NavbarContainer from 'components/Navbar/NavbarContainer';
+import NavbarWrapper from 'components/Navbar/NavbarWrapper';
+import NavbarBrand from 'components/Navbar/NavbarBrand';
+import NavbarToggler from 'components/Navbar/NavbarToggler';
+import Collapse from 'components/Navbar/Collapse';
+import Nav from 'components/Nav/Nav';
+import NavItem from 'components/Nav/NavItem';
+import 'ripple/ripple';
 
-export default class NavbarLinksCode extends React.Component {
-  state = {
-    color: 'pink',
-    type: 'react',
-    navbarOpen: false,
-  };
-  onFrameworkClick = (type) => {
-    switch (type) {
-      case 'html':
-        window.location =
-          '/learning-lab/tailwind-starter-kit#/documentation/javascript/navbars';
-        break;
-      case 'vue':
-        window.location =
-          '/learning-lab/tailwind-starter-kit#/documentation/vue/navbars';
-        break;
-      case 'react':
-        break;
-      case 'angular':
-        window.location =
-          '/learning-lab/tailwind-starter-kit#/documentation/angular/navbars';
-        break;
-      default:
-        this.setState({ showModal: true, modalText: type });
-        break;
-    }
-  };
-  render() {
-    let codeToShow =
-      `import React from "react";
+export default function NavbarLinksCode({ copyText, onCopy }) {
+  const [color, setColor] = useState('pink');
+  const [type] = useState('react');
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState();
+  const [openNavbar, setOpenNavbar] = useState(false);
 
-export default function Navbar({ fixed }) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const onFrameworkClick = Frameworks(type, setShowModal, setModalText);
+
+  const codeToShow = `import React, { useState } from "react";
+import Navbar from 'components/Navbar/Navbar';
+import NavbarContainer from 'components/Navbar/NavbarContainer';
+import NavbarWrapper from 'components/Navbar/NavbarWrapper';
+import NavbarBrand from 'components/Navbar/NavbarBrand';
+import NavbarToggler from 'components/Navbar/NavbarToggler';
+import Collapse from 'components/Navbar/Collapse';
+import Nav from 'components/Nav/Nav';
+import NavItem from 'components/Nav/NavItem';
+import "@md-ripple-effect"
+
+export default function Navbar() {
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  return (
+    <Navbar color="${color}" navbar>
+      <NavbarContainer>
+        <NavbarWrapper>
+          <NavbarBrand color="white">Navbar</NavbarBrand>
+          <NavbarToggler
+            color="white"
+            onClick={() => setOpenNavbar(!openNavbar)}
+          />
+        </NavbarWrapper>
+
+        <Collapse open={openNavbar}>
+          <Nav>
+            <NavItem
+              active="light"
+              color="white"
+              href="#navbar"
+              data-md-ripple-light={true}
+            >
+              Discover
+            </NavItem>
+            <NavItem color="white" href="#navbar" data-md-ripple-light={true}>
+              Profile
+            </NavItem>
+            <NavItem color="white" href="#navbar" data-md-ripple-light={true}>
+              Settings
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </NavbarContainer>
+    </Navbar>
+  );
+}`;
+
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-` +
-      this.state.color +
-      `-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-              href="#pablo"
-            >
-              ` +
-      this.state.color +
-      `
-            </a>
-            <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-            >
-              <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-              <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-              <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-            </button>
-          </div>
-          <div className="flex flex-grow items-center" id="example-navbar-info">
-            <ul className="flex flex-col lg:flex-row list-none ml-auto">
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  Discover
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  Profile
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  Settings
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
-}
-`;
-    return (
-      <>
-        {this.state.showModal ? (
-          <>
-            <div
-              className="block overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-              onClick={() => this.setState({ showModal: false })}
-            >
-              <div className="relative w-auto my-6 mx-auto max-w-sm">
-                {/*content*/}
-                <div className="border-0 rounded shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                    <h5 className="text-2xl font-normal mt-0 mb-0 ">
-                      Component not supported
-                    </h5>
-                    <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => this.setState({ showModal: false })}
-                    >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                        ×
-                      </span>
-                    </button>
-                  </div>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-4">
-                      At the moment we do not support the choosen framework (
-                      {this.state.modalText}) for this component.
-                    </p>
-                  </div>
+      {showModal ? (
+        <>
+          <div
+            className="block overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-sm">
+              {/*content*/}
+              <div className="border-0 rounded shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                  <h5 className="text-2xl font-normal mt-0 mb-0 ">
+                    Component not supported
+                  </h5>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-4">
+                    At the moment we do not support the choosen framework (
+                    {modalText}) for this component.
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-          </>
-        ) : null}
-        <DocsSnippet
-          copyText={this.props.copyText}
-          onCopy={this.props.onCopy}
-          activeColor={this.state.color}
-          activeFramework={this.state.type}
-          codeToShow={codeToShow}
-          onColorClick={(color) => this.setState({ color: color })}
-          onFrameworkClick={this.onFrameworkClick}
-        >
-          <nav
-            className={
-              'relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-' +
-              this.state.color +
-              '-500 mb-3'
-            }
-          >
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-              <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                <a
-                  className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-                  href="#pablo"
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+      <DocsSnippet
+        copyText={copyText}
+        onCopy={onCopy}
+        activeColor={color}
+        activeFramework={type}
+        codeToShow={codeToShow}
+        onColorClick={(color) => setColor(color)}
+        onFrameworkClick={onFrameworkClick}
+      >
+        <Navbar color={color} navbar>
+          <NavbarContainer>
+            <NavbarWrapper>
+              <NavbarBrand color="white">Navbar</NavbarBrand>
+              <NavbarToggler
+                color="white"
+                onClick={() => setOpenNavbar(!openNavbar)}
+              />
+            </NavbarWrapper>
+
+            <Collapse open={openNavbar}>
+              <Nav>
+                <NavItem
+                  active="light"
+                  color="white"
+                  href="#navbar"
+                  data-md-ripple-light={true}
                 >
-                  {this.state.color} Tailwind Starter Kit
-                </a>
-                <button
-                  className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                  type="button"
+                  Discover
+                </NavItem>
+                <NavItem
+                  color="white"
+                  href="#navbar"
+                  data-md-ripple-light={true}
                 >
-                  <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-                  <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-                  <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-                </button>
-              </div>
-              <div
-                className="flex flex-grow items-center"
-                id="example-navbar-info"
-              >
-                <ul className="flex flex-col lg:flex-row list-none ml-auto">
-                  <li className="nav-item">
-                    <a
-                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      href="#pablo"
-                    >
-                      Discover
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      href="#pablo"
-                    >
-                      Profile
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      href="#pablo"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </DocsSnippet>
-      </>
-    );
-  }
+                  Profile
+                </NavItem>
+                <NavItem
+                  color="white"
+                  href="#navbar"
+                  data-md-ripple-light={true}
+                >
+                  Settings
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </NavbarContainer>
+        </Navbar>
+      </DocsSnippet>
+    </>
+  );
 }
