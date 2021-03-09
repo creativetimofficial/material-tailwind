b864f19d932b7324c67c0d0db984a8b8b6034975
Sajad <nawabisajadahmad@gmail.com>
2021-03-09 12:48:22 +0430

    Update Modals Code component

--- components/Documentation/JavaScript/React/ModalsCode.js
+++ components/Documentation/JavaScript/React/ModalsCode.js
@@ -1,97 +1,83 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import DocsSnippet from 'components/Documentation/DocsSnippet.js';
+import Frameworks from 'components/Documentation/Frameworks.js';
+import Modal from 'components/Modals/Modal';
+import ModalHeader from 'components/Modals/ModalHeader';
+import ModalBody from 'components/Modals/ModalBody';
+import ModalFooter from 'components/Modals/ModalFooter';
+import Buttons from 'components/Buttons/Buttons';
 
-export default class AlertsCode extends React.Component {
-  state = {
-    showModal: false,
-    showErrorModal: false,
-    color: false,
-    type: 'react',
-  };
-  onFrameworkClick = (type) => {
-    switch (type) {
-      case 'react':
-        break;
-      case 'angular':
-        if (this.props.type === 'small') {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/angular/modal/small';
-        } else if (this.props.type === 'regular') {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/angular/modal/regular';
-        } else {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/angular/modal/large';
-        }
-        break;
-      case 'html':
-        if (this.props.type === 'small') {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/javascript/modal/small';
-        } else if (this.props.type === 'regular') {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/javascript/modal/regular';
-        } else {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/javascript/modal/large';
-        }
-        break;
-      case 'vue':
-        if (this.props.type === 'small') {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/vue/modal/small';
-        } else if (this.props.type === 'regular') {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/vue/modal/regular';
-        } else {
-          window.location =
-            '/learning-lab/tailwind-starter-kit#/documentation/vue/modal/large';
-        }
-        break;
-      default:
-        this.setState({ showErrorModal: true, modalText: type });
-        break;
-    }
-  };
-  render() {
-    let codeToShow =
-      `import React from "react";
+export default function AlertsCode({ copyText, onCopy, size, modalSize }) {
+  const [color, setColor] = useState('pink');
+  const [type] = useState('react');
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState();
+  const [showModalCode, setShowModalCode] = React.useState(false);
 
-export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
+  const onFrameworkClick = Frameworks(type, setShowModal, setModalText);
+
+  const codeToShow = `import React, { useState } from "react";
+import Modal from 'components/Modals/Modal';
+import ModalHeader from 'components/Modals/ModalHeader';
+import ModalBody from 'components/Modals/ModalBody';
+import ModalFooter from 'components/Modals/ModalFooter';
+import Buttons from 'components/Buttons/Buttons';
+
+export default function Modals() {
   return (
     <>
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+      <Buttons
+        color="${color}"
         type="button"
-        style={{ transition: "all .15s ease" }}
-        onClick={() => setShowModal(true)}
+        onClick={() => setShowModalCode(true)}
+        data-md-ripple-light={true}
       >
-        Open ` +
-      this.props.type +
-      ` modal
-      </button>
+        Open ${modalSize} Modal
+      </Buttons>
+
+      <Modal size="${size}" active={showModalCode} toggler={() => setShowModalCode(false)}>
+        <ModalHeader toggler={() => setShowModalCode(false)}>
+          Regular Modal
+        </ModalHeader>
+        <ModalBody>
+          <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-12">
+            I always felt like I could do anything. That’s the main thing people
+            are controlled by! Thoughts- their perception of themselves! They're
+            slowed down by their perception of themselves. If you're taught you
+            can’t do anything, you won’t do anything. I was taught I could do
+            everything.
+          </p>
+        </ModalBody>
+        <ModalFooter>
+          <Buttons color="red" buttonType="link" onClick={() => setShowModalCode(false)}>
+            Close
+          </Buttons>
+
+          <Buttons color="green" onClick={() => setShowModalCode(false)}>
+            Save Changes
+          </Buttons>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+}`;
+
+  return (
+    <>
       {showModal ? (
         <>
           <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            className="block overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             onClick={() => setShowModal(false)}
           >
-            <div className="relative w-auto my-6 mx-auto ` +
-      (this.props.type === 'large'
-        ? `max-w-6xl`
-        : this.props.type === 'regular'
-        ? `max-w-3xl`
-        : `max-w-sm`) +
-      `">
+            <div className="relative w-auto my-6 mx-auto max-w-sm">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
+                  <h5 className="text-2xl font-normal mt-0 mb-0 ">
+                    Component not supported
+                  </h5>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -103,32 +89,10 @@ export default function Modal() {
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-gray-600 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                  <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-4">
+                    At the moment we do not support the choosen framework (
+                    {modalText}) for this component.
                   </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    style={{ transition: "all .15s ease" }}
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    style={{ transition: "all .15s ease" }}
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
                 </div>
               </div>
             </div>
@@ -136,133 +100,58 @@ export default function Modal() {
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
+      <DocsSnippet
+        copyText={copyText}
+        onCopy={onCopy}
+        activeColor={color}
+        activeFramework={type}
+        codeToShow={codeToShow}
+        onColorClick={(color) => setColor(color)}
+        onFrameworkClick={onFrameworkClick}
+      >
+        <div>
+          <Buttons
+            color={color}
+            type="button"
+            onClick={() => setShowModalCode(true)}
+            data-md-ripple-light={true}
+          >
+            Open {modalSize} Modal
+          </Buttons>
+
+          <Modal
+            size={size}
+            active={showModalCode}
+            toggler={() => setShowModalCode(false)}
+          >
+            <ModalHeader toggler={() => setShowModalCode(false)}>
+              Regular Modal
+            </ModalHeader>
+            <ModalBody>
+              <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-12">
+                I always felt like I could do anything. That’s the main thing
+                people are controlled by! Thoughts- their perception of
+                themselves! They're slowed down by their perception of
+                themselves. If you're taught you can’t do anything, you won’t do
+                anything. I was taught I could do everything.
+              </p>
+            </ModalBody>
+            <ModalFooter>
+              <Buttons
+                color="red"
+                buttonType="link"
+                onClick={() => setShowModalCode(false)}
+              >
+                Close
+              </Buttons>
+
+              <Buttons color="green" onClick={() => setShowModalCode(false)}>
+                Save Changes
+              </Buttons>
+            </ModalFooter>
+          </Modal>
+        </div>
+      </DocsSnippet>
     </>
   );
-}
-`;
-    return (
-      <>
-        {this.state.showErrorModal ? (
-          <>
-            <div
-              className="block overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-              onClick={() => this.setState({ showErrorModal: false })}
-            >
-              <div className="relative w-auto my-6 mx-auto max-w-sm">
-                {/*content*/}
-                <div className="border-0 rounded shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                    <h5 className="text-2xl font-normal mt-0 mb-0 ">
-                      Component not supported
-                    </h5>
-                    <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => this.setState({ showErrorModal: false })}
-                    >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                        ×
-                      </span>
-                    </button>
-                  </div>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-4">
-                      At the moment we do not support the choosen framework (
-                      {this.state.modalText}) for this component.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-          </>
-        ) : null}
-        <DocsSnippet
-          copyText={this.props.copyText}
-          onCopy={this.props.onCopy}
-          activeColor={this.state.color}
-          activeFramework={this.state.type}
-          codeToShow={codeToShow}
-          onColorClick={(color) => this.setState({ color: color })}
-          onFrameworkClick={this.onFrameworkClick}
-        >
-          <button
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-            type="button"
-            style={{ transition: 'all .15s ease' }}
-            onClick={() => this.setState({ showModal: true })}
-          >
-            Open {this.props.type} modal
-          </button>
-          {this.state.showModal ? (
-            <>
-              <div
-                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                onClick={() => this.setState({ showModal: false })}
-              >
-                <div
-                  className={
-                    'relative w-auto my-6 mx-auto ' +
-                    (this.props.type === 'large'
-                      ? 'max-w-6xl'
-                      : this.props.type === 'regular'
-                      ? 'max-w-3xl'
-                      : 'max-w-sm')
-                  }
-                >
-                  {/*content*/}
-                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                    {/*header*/}
-                    <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                      <h3 className="text-3xl font-semibold">Modal Title</h3>
-                      <button
-                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                        onClick={() => this.setState({ showModal: false })}
-                      >
-                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                          ×
-                        </span>
-                      </button>
-                    </div>
-                    {/*body*/}
-                    <div className="relative p-6 flex-auto">
-                      <p className="my-4 text-gray-600 text-lg leading-relaxed">
-                        I always felt like I could do anything. That’s the main
-                        thing people are controlled by! Thoughts- their
-                        perception of themselves! They're slowed down by their
-                        perception of themselves. If you're taught you can’t do
-                        anything, you won’t do anything. I was taught I could do
-                        everything.
-                      </p>
-                    </div>
-                    {/*footer*/}
-                    <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
-                      <button
-                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-                        type="button"
-                        style={{ transition: 'all .15s ease' }}
-                        onClick={() => this.setState({ showModal: false })}
-                      >
-                        Close
-                      </button>
-                      <button
-                        className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                        type="button"
-                        style={{ transition: 'all .15s ease' }}
-                        onClick={() => this.setState({ showModal: false })}
-                      >
-                        Save Changes
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            </>
-          ) : null}
-        </DocsSnippet>
-      </>
-    );
-  }
 }
