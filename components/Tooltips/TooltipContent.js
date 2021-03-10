ff9dabd4fb4ef09d37e6382a68e163f577aeff47
Sajad <nawabisajadahmad@gmail.com>
2021-03-10 20:06:16 +0430

    Update Tooltip Content component

--- components/Tooltips/TooltipContent.js
+++ components/Tooltips/TooltipContent.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const TooltipContent = ({ children }) => {
   return (
-    <div className="px-2.5 py-1.5 bg-black bg-opacity-75 text-white text-sm rounded">
+    <div className="px-2.5 py-1.5 bg-black bg-opacity-80 text-white text-sm rounded">
       {children}
     </div>
   );
