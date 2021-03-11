3cc23b7eb2d50a75ae4dbcfb58efbaa50f6b820c
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 20:54:24 +0430

    Update Card Footer component

--- components/Card/CardFooter.js
+++ components/Card/CardFooter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CardFooter = ({ children }) => {
-  return <div className="px-6 pt-4 pb-6">{children}</div>;
+  return <div className="px-4 pb-4">{children}</div>;
 };
 
 CardFooter.propTypes = {
