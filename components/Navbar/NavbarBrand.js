e453ef580e08f75915e30c691aafcf62542c0fe7
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 14:29:06 +0430

    Update Navbar Brand component

--- components/Navbar/NavbarBrand.js
+++ components/Navbar/NavbarBrand.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const colors = {
-  white: 'text-white',
-  black: 'text-black',
-  blueGray: 'text-blue-gray-500',
-  gray: 'text-gray-500',
-  brown: 'text-brown-500',
-  deepOrange: 'text-deep-orange-500',
-  orange: 'text-orange-500',
-  amber: 'text-amber-500',
-  yellow: 'text-yellow-600',
-  lime: 'text-lime-500',
-  lightGreen: 'text-light-green-500',
-  green: 'text-green-500',
-  teal: 'text-teal-500',
-  cyan: 'text-cyan-500',
-  lightBlue: 'text-light-blue-500',
-  blue: 'text-blue-500',
-  indigo: 'text-indigo-500',
-  deepPurple: 'text-deep-purple-500',
-  purple: 'text-purple-500',
-  pink: 'text-pink-500',
-  red: 'text-red-500',
-};
-
 const NavbarBrand = ({ children, color }) => {
   return (
     <a
-      className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase ${colors[color]}`}
+      className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white`}
       href="#pablo"
     >
       {children}
