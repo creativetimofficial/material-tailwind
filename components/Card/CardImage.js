92831014d0154b6381fd291b8e3c8031ea700c5a
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 20:54:01 +0430

    Update Card Image component

--- components/Card/CardImage.js
+++ components/Card/CardImage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CardImage = ({ src, ...rest }) => {
-  return <img {...rest} className="w-full" src={src} />;
+  return (
+    <img {...rest} className="w-full rounded-lg -mt-8 shadow-lg" src={src} />
+  );
 };
 
 CardImage.propTypes = {
