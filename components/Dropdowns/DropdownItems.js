1bc756e5147372515f1f688e1574955b1a67930b
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 21:10:04 +0430

    Update Dropdown Items component

--- components/Dropdowns/DropdownItems.js
+++ components/Dropdowns/DropdownItems.js
@@ -5,7 +5,7 @@ const DropdownItems = ({ children, ...rest }) => {
   return (
     <a
       {...rest}
-      className="text-sm py-3 px-4 font-normal block whitespace-no-wrap text-gray-800 hover:bg-gray-500 hover:bg-opacity-10"
+      className="text-sm py-3 px-4 font-normal block whitespace-no-wrap text-gray-900 hover:bg-gray-500 hover:bg-opacity-10"
     >
       {children}
     </a>
