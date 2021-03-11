dfe5a3be965f852e6927e15240d610c5bb69a3d3
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 20:53:38 +0430

    Update Card Body component

--- components/Card/CardBody.js
+++ components/Card/CardBody.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CardBody = ({ children }) => {
-  return <div className="px-6 py-4">{children}</div>;
+  return <div className="px-4 pt-5 pb-4">{children}</div>;
 };
 
 CardBody.propTypes = {
