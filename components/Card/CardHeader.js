52c442067c6b32b8522e00d4b837f55aa9369db3
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 20:53:27 +0430

    Update Card Header component

--- components/Card/CardHeader.js
+++ components/Card/CardHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CardHeader = ({ children }) => {
-  return <div className="px-6 pt-5">{children}</div>;
+  return <div className="px-4 py-5">{children}</div>;
 };
 
 CardHeader.propTypes = {
