52fefa3c56ee1587df0a629b03f57d4eba37a592
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 16:41:21 +0430

    Update Tab Item component

--- components/Tabs/TabItem.js
+++ components/Tabs/TabItem.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const TabItem = ({ children, active, ...rest }) => {
   return (
-    <li className="text-center w-full">
+    <li className="text-center">
       <a
         {...rest}
-        className={`flex items-center justify-center gap-1 relative overflow-hidden text-sm font-medium py-4 px-6 leading-normal text-white hover:bg-white hover:bg-opacity-10 transition-all duration-300 ${
-          active && 'bg-white bg-opacity-10'
+        className={`flex items-center justify-center gap-1 relative overflow-hidden rounded-lg text-sm font-medium py-4 px-6 leading-normal text-white hover:bg-white hover:bg-opacity-20 transition-all duration-300 ${
+          active && 'bg-white bg-opacity-20'
         }`}
         role="tablist"
       >
