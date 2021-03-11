406f76c7163c372cbb5246c11266fc581cfe5eb4
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 18:29:31 +0430

    Update Input component

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const colors = {
+const mdInputColors = {
   blueGray: 'md-input-blue-gray-500',
   gray: 'md-input-gray-500',
   brown: 'md-input-brown-500',
   deepOrange: 'md-input-deep-orange-500',
   orange: 'md-input-orange-500',
   amber: 'md-input-amber-500',
-  yellow: 'md-input-yellow-500',
+  yellow: 'md-input-yellow-600',
   lime: 'md-input-lime-500',
   lightGreen: 'md-input-light-green-500',
   green: 'md-input-green-500',
@@ -23,6 +23,28 @@ const colors = {
   red: 'md-input-red-500',
 };
 
+const mdInputOutlineColors = {
+  blueGray: 'md-input-outline-blue-gray-500',
+  gray: 'md-input-outline-gray-500',
+  brown: 'md-input-outline-brown-500',
+  deepOrange: 'md-input-outline-deep-orange-500',
+  orange: 'md-input-outline-orange-500',
+  amber: 'md-input-outline-amber-500',
+  yellow: 'md-input-outline-yellow-600',
+  lime: 'md-input-outline-lime-500',
+  lightGreen: 'md-input-outline-light-green-500',
+  green: 'md-input-outline-green-500',
+  teal: 'md-input-outline-teal-500',
+  cyan: 'md-input-outline-cyan-500',
+  lightBlue: 'md-input-outline-light-blue-500',
+  blue: 'md-input-outline-blue-500',
+  indigo: 'md-input-outline-indigo-500',
+  deepPurple: 'md-input-outline-deep-purple-500',
+  purple: 'md-input-outline-purple-500',
+  pink: 'md-input-outline-pink-500',
+  red: 'md-input-outline-red-500',
+};
+
 const borderColors = {
   blueGray: 'border-blue-gray-500',
   gray: 'border-gray-500',
@@ -30,7 +52,7 @@ const borderColors = {
   deepOrange: 'border-deep-orange-500',
   orange: 'border-orange-500',
   amber: 'border-amber-500',
-  yellow: 'border-yellow-500',
+  yellow: 'border-yellow-600',
   lime: 'border-lime-500',
   lightGreen: 'border-light-green-500',
   green: 'border-green-500',
@@ -51,22 +73,44 @@ const Input = ({
   size = 'regular',
   outline,
   error,
+  success,
   ...rest
 }) => {
-  let inputClasses = [];
+  let labelBorderColor,
+    mdInputBorderColor,
+    mdInputOutlineColor,
+    mdInputOutlineFocusColor,
+    inputClasses = [];
 
   let container = ['w-full', 'relative'];
 
+  if (error) {
+    labelBorderColor = borderColors['red'];
+    mdInputBorderColor = mdInputColors['red'];
+    mdInputOutlineColor = mdInputOutlineColors['red'];
+    mdInputOutlineFocusColor = borderColors['red'];
+  } else if (success) {
+    labelBorderColor = borderColors['green'];
+    mdInputBorderColor = mdInputColors['green'];
+    mdInputOutlineColor = mdInputOutlineColors['green'];
+    mdInputOutlineFocusColor = borderColors['green'];
+  } else {
+    labelBorderColor = 'border-gray-300';
+    mdInputBorderColor = mdInputColors[color];
+    mdInputOutlineColor = mdInputOutlineColors[color];
+    mdInputOutlineFocusColor = borderColors[color];
+  }
+
   let label = [
-    'text-gray-500',
+    'text-gray-400',
     'absolute',
     'left-0',
     `${outline ? '-top-1.5' : '-top-0.5'}`,
     'w-full',
     'h-full',
-    `${error ? 'border-red-500' : 'border-gray-500'}`,
-    'pointer-events-none',
     `${!outline && 'border border-t-0 border-l-0 border-r-0 border-b-1'}`,
+    labelBorderColor,
+    'pointer-events-none',
     `${outline && 'flex'}`,
     `${outline && size === 'sm' && 'text-sm'}`,
     `${outline && 'leading-10'}`,
@@ -75,7 +119,6 @@ const Input = ({
   ];
 
   const sharedClasses = [
-    colors[color],
     'w-full',
     'h-full',
     'leading-normal',
@@ -102,22 +145,24 @@ const Input = ({
     `${outline && 'pt-3.5 pb-2.5'}`,
   ];
 
-  const inputFilled = ['md-input', 'bg-transparent', 'border', 'border-none'];
+  const inputFilled = [
+    mdInputBorderColor,
+    'md-input',
+    'bg-transparent',
+    'border-none',
+  ];
 
   const inputOutline = [
+    mdInputOutlineColor,
+    labelBorderColor,
     'md-input-outline',
     'bg-transparent',
     'border',
     'border-1',
-    'border-gray-500',
-    'rounded',
+    'border-gray-300',
+    'rounded-lg',
     'focus:border-2',
-    `focus:${borderColors[color]}`,
-  ];
-
-  const inputError = [
-    `border-${error ? 'red-500' : 'gray-400'}`,
-    `focus:${borderColors[color]}`,
+    `focus:${mdInputOutlineFocusColor}`,
   ];
 
   if (size === 'sm') {
@@ -134,8 +179,6 @@ const Input = ({
   outline
     ? inputClasses.push(...inputOutline)
     : inputClasses.push(...inputFilled);
-
-  error && inputClasses.push(...inputError);
 
   container = container.join(' ');
   label = label.join(' ');
@@ -159,6 +202,9 @@ const Input = ({
       </label>
       {error && (
         <span className="block mt-1 text-xs text-red-500">{error}</span>
+      )}
+      {success && (
+        <span className="block mt-1 text-xs text-green-500">{success}</span>
       )}
     </div>
   );
