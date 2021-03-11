1cc0b7a5f79508dc988b0387a0c50e08603d77dd
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 21:10:28 +0430

    Update Modal Header component

--- components/Modals/ModalHeader.js
+++ components/Modals/ModalHeader.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ModalHeader = ({ children, toggler }) => {
   return (
     <div className="flex items-center justify-between mb-6">
-      <h5 className="text-gray-800 text-2xl font-bold mt-0 mb-0">{children}</h5>
+      <h5 className="text-gray-900 text-2xl font-bold mt-0 mb-0">{children}</h5>
       <button
         className="p-1 bg-transparent absolute top-2 right-4 text-gray-900 text-3xl leading-none outline-none focus:outline-none"
         onClick={toggler}
