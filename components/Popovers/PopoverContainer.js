55063bca9393feaee959b23e378d94a7b519b055
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 13:05:13 +0430

    Update Popover Container component

--- components/Popovers/PopoverContainer.js
+++ components/Popovers/PopoverContainer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const PopoverContainer = ({ children }) => {
   return (
-    <div className="bg-white border-0 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-md shadow-2xl">
+    <div className="bg-white border-0 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg shadow-2xl">
       {children}
     </div>
   );
