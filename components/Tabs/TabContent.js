2c93f144d335990e9f7f1864b823dac22e1c7594
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 19:20:37 +0430

    Update Tab Content component

--- components/Tabs/TabContent.js
+++ components/Tabs/TabContent.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 
 const TabContent = ({ children }) => {
   return (
-    <div className="relative flex flex-col min-w-0 break-words bg-white rounded-lg shadow w-full p-8 pt-16 mb-4 -mt-10">
-      <div className="flex-auto text-center font-light">{children}</div>
+    <div className="relative flex flex-col min-w-0 break-words w-full px-4 pb-4">
+      <div className="flex-auto text-center font-light leading-normal">
+        {children}
+      </div>
     </div>
   );
 };
