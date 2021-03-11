b920daa2a9c9e83f99ca9cfdfc11bbcbe2f3de2d
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 12:50:50 +0430

    Update Modal Body component

--- components/Modals/ModalBody.js
+++ components/Modals/ModalBody.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ModalBody = ({ children }) => {
-  return <div className="flex-auto">{children}</div>;
+  return <div className="relative flex-auto">{children}</div>;
 };
 
 ModalBody.propTypes = {
