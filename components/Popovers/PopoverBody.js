0e1066685af8a5c5446e634054da4462816c61a3
Sajad <nawabisajadahmad@gmail.com>
2021-03-11 13:03:42 +0430

    Update Popover Body component

--- components/Popovers/PopoverBody.js
+++ components/Popovers/PopoverBody.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PopoverHeader = ({ children }) => {
-  return <div className="text-gray-800 px-4 pb-4">{children}</div>;
+  return <div className="text-gray-800 font-normal px-4 pb-4">{children}</div>;
 };
 
 PopoverHeader.propTypes = {
