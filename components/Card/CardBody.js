e3546cd944fab425484131a5c259700f10cc969c
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 15:50:28 +0430

    Refactor Card Body component

--- components/Card/CardBody.js
+++ components/Card/CardBody.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CardBody = ({ children }) => {
+export default function CardBody({ children }) {
   return <div className="px-4 pt-5 pb-4">{children}</div>;
-};
+}
 
 CardBody.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default CardBody;
