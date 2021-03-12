001e2970560c04f331f958a0d65360c311fcfce5
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:00:48 +0430

    Refactor Popover Container component

--- components/Popovers/PopoverContainer.js
+++ components/Popovers/PopoverContainer.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PopoverContainer = ({ children }) => {
+export default function PopoverContainer({ children }) {
   return (
     <div className="bg-white border-0 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg shadow-2xl">
       {children}
     </div>
   );
-};
+}
 
 PopoverContainer.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default PopoverContainer;
