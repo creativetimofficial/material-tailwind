29d4b7f69b72257726eaa393b7f0dc941daa0b35
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:04:02 +0430

    Refactor Tooltip Content component

--- components/Tooltips/TooltipContent.js
+++ components/Tooltips/TooltipContent.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TooltipContent = ({ children }) => {
+export default function TooltipContent({ children }) {
   return (
     <div className="px-2.5 py-1.5 bg-black bg-opacity-80 text-white text-sm rounded">
       {children}
     </div>
   );
-};
+}
 
 TooltipContent.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default TooltipContent;
