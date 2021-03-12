016ec618fe0616b6cce1efbe51fe77648d1f4a24
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:03:07 +0430

    Refactor Tab Item component

--- components/Tabs/TabItem.js
+++ components/Tabs/TabItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TabItem = ({ children, active, ...rest }) => {
+export default function TabItem({ children, active, ...rest }) {
   return (
     <li className="text-center">
       <a
         {...rest}
-        className={`flex items-center justify-center gap-1 relative overflow-hidden rounded-lg text-sm font-medium py-4 px-6 leading-normal text-white hover:bg-white hover:bg-opacity-20 transition-all duration-300 ${
+        className={`flex items-center justify-center gap-1 rounded-lg text-sm font-medium py-4 px-6 leading-normal text-white hover:bg-white hover:bg-opacity-20 transition-all duration-300 ${
           active && 'bg-white bg-opacity-20'
         }`}
         role="tablist"
@@ -15,11 +15,9 @@ const TabItem = ({ children, active, ...rest }) => {
       </a>
     </li>
   );
-};
+}
 
 TabItem.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
   active: PropTypes.bool,
 };
-
-export default TabItem;
