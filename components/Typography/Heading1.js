548e2afc175e2d1755e2af42b592a140f87e3111
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:04:42 +0430

    Refactor Heading 1 component

--- components/Typography/Heading1.js
+++ components/Typography/Heading1.js
@@ -23,7 +23,7 @@ const colors = {
   red: 'text-red-500',
 };
 
-const H1 = ({ children, color, ...rest }) => {
+export default function H1({ children, color, ...rest }) {
   return (
     <h1
       {...rest}
@@ -32,11 +32,9 @@ const H1 = ({ children, color, ...rest }) => {
       {children}
     </h1>
   );
-};
+}
 
 H1.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string.isRequired,
 };
-
-export default H1;
