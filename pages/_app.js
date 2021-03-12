f5733d79a5b3a88e775e92a768427c0a37e4ca0e
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 13:28:18 +0430

    Add the Font Awesome cdn

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,12 @@ export default function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+          integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+          crossorigin="anonymous"
+        />
         <link rel="shortcut icon" href={require('assets/img/favicon.ico')} />
         <link
           rel="apple-touch-icon"
