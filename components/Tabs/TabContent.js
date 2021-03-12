9ee6ffb7cb9cb3795d46c93a5245d011fd9e7104
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:02:56 +0430

    Refactor Tab Content component

--- components/Tabs/TabContent.js
+++ components/Tabs/TabContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TabContent = ({ children }) => {
+export default function TabContent({ children }) {
   return (
     <div className="relative flex flex-col min-w-0 break-words w-full px-4 pb-4">
       <div className="flex-auto text-center font-light leading-normal">
@@ -9,10 +9,8 @@ const TabContent = ({ children }) => {
       </div>
     </div>
   );
-};
+}
 
 TabContent.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default TabContent;
