c7a92c5983b9a8beff9653e2726bfde872584dd6
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:01:10 +0430

    Refactor Popover Body component

--- components/Popovers/PopoverBody.js
+++ components/Popovers/PopoverBody.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PopoverHeader = ({ children }) => {
+export default function PopoverHeader({ children }) {
   return <div className="text-gray-700 font-normal px-4 pb-4">{children}</div>;
-};
+}
 
 PopoverHeader.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default PopoverHeader;
