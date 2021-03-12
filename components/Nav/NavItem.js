9d70f6ab4d4c1e3750b8c8ed9db320e7e5a67e2e
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 15:55:30 +0430

    Refactor Nav Item component

--- components/Nav/NavItem.js
+++ components/Nav/NavItem.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NavbarItem = ({ children, active, ...rest }) => {
+export default function NavbarItem({ children, active, ...rest }) {
   return (
     <li>
       <a
         {...rest}
         className={`${active === 'dark' && 'bg-black bg-opacity-20'} ${
           active === 'light' && 'bg-white bg-opacity-20'
-        } relative overflow-hidden px-5 py-4 flex gap-1 items-center text-xs uppercase font-medium leading text-white rounded-lg`}
+        } px-5 py-4 flex gap-1 items-center text-xs uppercase font-medium leading text-white rounded-lg`}
       >
         {children}
       </a>
     </li>
   );
-};
+}
 
 NavbarItem.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
   active: PropTypes.string,
 };
-
-export default NavbarItem;
