f68a6a0a9c9f8618c41f6d999be0c1f875d693f7
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 15:56:24 +0430

    Refactor Navbar Container component

--- components/Navbar/NavbarContainer.js
+++ components/Navbar/NavbarContainer.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NavbarContainer = ({ children }) => {
+export default function NavbarContainer({ children }) {
   return (
     <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
       {children}
     </div>
   );
-};
+}
 
 NavbarContainer.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default NavbarContainer;
