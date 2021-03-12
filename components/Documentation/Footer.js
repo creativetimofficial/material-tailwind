7a63215638612cde65aeb6dbb9aeab7964239681
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:10:42 +0430

    Update Footer for documentation

--- components/Documentation/Footer.js
+++ components/Documentation/Footer.js
@@ -166,7 +166,7 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-600 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Tailwind Starter Kit by{' '}
+                Copyright © {new Date().getFullYear()} MD Tailwind by{' '}
                 <a
                   href="https://www.creative-tim.com"
                   className="text-gray-600 hover:text-gray-900"
