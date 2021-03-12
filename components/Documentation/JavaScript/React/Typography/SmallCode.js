8cffeba91e69434c33e3316435f62a09a8e816a2
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 10:29:17 +0430

    Update Small Code component

--- components/Documentation/JavaScript/React/Typography/SmallCode.js
+++ components/Documentation/JavaScript/React/Typography/SmallCode.js
@@ -12,7 +12,7 @@ export default function AlertsCode({ copyText, onCopy, value, percentage }) {
   const onFrameworkClick = Frameworks(type, setShowModal, setModalText);
 
   const codeToShow = `import React from "react";
-import Small from "components/Typography/Small";
+import Small from "@md-tailwind/react/Small";
 
 export default function Small() {
   return (
