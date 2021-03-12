02a7eb1835fb8dd6842714f8e94584c93d9d4452
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:12:02 +0430

    Update Menu Icons Code component

--- components/Documentation/JavaScript/React/Menus/MenuIconsCode.js
+++ components/Documentation/JavaScript/React/Menus/MenuIconsCode.js
@@ -6,7 +6,7 @@ import NavbarContainer from 'components/Navbar/NavbarContainer';
 import NavbarWrapper from 'components/Navbar/NavbarWrapper';
 import NavbarBrand from 'components/Navbar/NavbarBrand';
 import NavbarToggler from 'components/Navbar/NavbarToggler';
-import Collapse from 'components/Navbar/Collapse';
+import NavbarCollapse from 'components/Navbar/NavbarCollapse';
 import Nav from 'components/Nav/Nav';
 import NavItem from 'components/Nav/NavItem';
 import Icon from 'components/Icon/Icon';
@@ -27,7 +27,7 @@ import NavbarContainer from "@md-tailwind/react/NavbarContainer";
 import NavbarWrapper from "@md-tailwind/react/NavbarWrapper";
 import NavbarBrand from "@md-tailwind/react/NavbarBrand";
 import NavbarToggler from "@md-tailwind/react/NavbarToggler";
-import Collapse from "@md-tailwind/react/Collapse";
+import NavbarCollapse from "@md-tailwind/react/NavbarCollapse";
 import Nav from "@md-tailwind/react/Nav";
 import NavItem from "@md-tailwind/react/NavItem";
 import Icon from "@md-tailwind/react/Icon";
@@ -40,28 +40,27 @@ export default function Menus() {
     <Navbar color="${color}">
       <NavbarContainer>
         <NavbarWrapper>
-          <NavbarBrand color="white">Menu</NavbarBrand>
-          <NavbarToggler color="white" onClick={() => setOpenMenu(!openMenu)} />
+          <NavbarBrand>Menu</NavbarBrand>
+          <NavbarToggler onClick={() => setOpenMenu(!openMenu)} />
         </NavbarWrapper>
 
-        <Collapse open={openMenu}>
+        <NavbarCollapse open={openMenu}>
           <Nav>
             <NavItem
               active="light"
-              color="white"
               href="#navbar"
               data-md-ripple-light={true}
             >
               <Icon name="language" size="xl" />
             </NavItem>
-            <NavItem color="white" href="#navbar" data-md-ripple-light={true}>
+            <NavItem href="#navbar" data-md-ripple-light={true}>
               <Icon name="account_circle" size="xl" />
             </NavItem>
-            <NavItem color="white" href="#navbar" data-md-ripple-light={true}>
+            <NavItem href="#navbar" data-md-ripple-light={true}>
               <Icon name="settings" size="xl" />
             </NavItem>
           </Nav>
-        </Collapse>
+        </NavbarCollapse>
       </NavbarContainer>
     </Navbar>
   );
@@ -117,39 +116,27 @@ export default function Menus() {
         <Navbar color={color}>
           <NavbarContainer>
             <NavbarWrapper>
-              <NavbarBrand color="white">Menu</NavbarBrand>
-              <NavbarToggler
-                color="white"
-                onClick={() => setOpenMenu(!openMenu)}
-              />
+              <NavbarBrand>Menu</NavbarBrand>
+              <NavbarToggler onClick={() => setOpenMenu(!openMenu)} />
             </NavbarWrapper>
 
-            <Collapse open={openMenu}>
+            <NavbarCollapse open={openMenu}>
               <Nav>
                 <NavItem
                   active="light"
-                  color="white"
                   href="#navbar"
                   data-md-ripple-light={true}
                 >
                   <Icon name="language" size="xl" />
                 </NavItem>
-                <NavItem
-                  color="white"
-                  href="#navbar"
-                  data-md-ripple-light={true}
-                >
+                <NavItem href="#navbar" data-md-ripple-light={true}>
                   <Icon name="account_circle" size="xl" />
                 </NavItem>
-                <NavItem
-                  color="white"
-                  href="#navbar"
-                  data-md-ripple-light={true}
-                >
+                <NavItem href="#navbar" data-md-ripple-light={true}>
                   <Icon name="settings" size="xl" />
                 </NavItem>
               </Nav>
-            </Collapse>
+            </NavbarCollapse>
           </NavbarContainer>
         </Navbar>
       </DocsSnippet>
