2beb606003dca3e181f1740fdab10e30eb70059a
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 15:50:11 +0430

    Refactor Card component

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Card = ({ children }) => {
+export default function Card({ children }) {
   return (
     <div className="max-w-sm bg-white rounded-xl overflow-hdden shadow p-3 my-8">
       {children}
     </div>
   );
-};
+}
 
 Card.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default Card;
