96b17af9eb237528c715034e97a48944ee32d1f9
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 16:02:32 +0430

    Refactor Tab component

--- components/Tabs/Tab.js
+++ components/Tabs/Tab.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Tab = ({ children }) => {
+export default function Tab({ children }) {
   return (
     <div className="relative flex flex-col items-center justify-center bg-white rounded-lg shadow w-full p-4 my-8">
       {children}
     </div>
   );
-};
+}
 
 Tab.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default Tab;
