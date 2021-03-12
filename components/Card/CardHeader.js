92bbc66a7c5c0e578fd328679051e28a1095e21d
Sajad <nawabisajadahmad@gmail.com>
2021-03-12 15:50:38 +0430

    Refactor Card Header component

--- components/Card/CardHeader.js
+++ components/Card/CardHeader.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CardHeader = ({ children }) => {
+export default function CardHeader({ children }) {
   return <div className="px-4 py-5">{children}</div>;
-};
+}
 
 CardHeader.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
-
-export default CardHeader;
