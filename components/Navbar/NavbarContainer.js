cdd88b6f6631bec8a3710ea967406efd56552128
Sajad <nawabisajadahmad@gmail.com>
2021-03-16 23:18:49 +0430

    Update Navbar Container component

--- components/Navbar/NavbarContainer.js
+++ components/Navbar/NavbarContainer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function NavbarContainer({ children }) {
   return (
-    <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div className="container max-w-7xl px-4 mx-auto flex flex-wrap items-center justify-between">
       {children}
     </div>
   );
