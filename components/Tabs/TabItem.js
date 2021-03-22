1b2df70808f87e563adc9ede364057cce9cc0daf
Sajad <nawabisajadahmad@gmail.com>
2021-03-22 09:38:29 +0430

    Update the Tab Item component

--- components/Tabs/TabItem.js
+++ components/Tabs/TabItem.js
@@ -6,7 +6,7 @@ export default function TabItem({ children, active, ...rest }) {
     <li className="text-center">
       <a
         {...rest}
-        className={`flex items-center justify-center gap-1 rounded-lg text-sm font-medium py-4 px-6 leading-normal text-white hover:bg-white hover:bg-opacity-20 transition-all duration-300 ${
+        className={`flex items-center justify-center gap-1 rounded-lg text-sm font-medium py-4 px-6 leading-normal text-white transition-all duration-300 ${
           active && 'bg-white bg-opacity-20'
         }`}
         role="tablist"
