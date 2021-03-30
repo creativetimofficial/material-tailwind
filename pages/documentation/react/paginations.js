73354c633ed58f90f13623fed3813d4ba15dcb98
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:32:56 -0700

    Update documentation paginations component

--- pages/documentation/react/paginations.js
+++ pages/documentation/react/paginations.js
@@ -1,62 +1,37 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import PaginationCode from 'components/Documentation/JavaScript/React/Paginations/PaginationCode';
-import PaginationFirstLastCode from 'components/Documentation/JavaScript/React/Paginations/PaginationFirstLastCode';
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import SmallHeading from "components/Documentation/SmallHeading.js";
+import PaginationCode from "components/Documentation/JavaScript/React/Paginations/PaginationCode";
+import PaginationFirstLastCode from "components/Documentation/JavaScript/React/Paginations/PaginationFirstLastCode";
 
 export default function Paginations() {
   const [copy, setCopy] = useState(null);
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+      <Header title="React Paginations" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Paginations"
+          description="Material Tailwind pagination component consists of button-like styled links, that are arranged side by side in a horizontal list. Pagination is built with list HTML elements so screen readers can announce the number of available links."
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+        <SmallHeading title="Simple Pagination" />
+        <PaginationCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
         />
-        <title>React Paginations | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Paginations"
-                description="Different paginations using React."
-              />
-              <SmallHeading title="Simple Pagination" />
-              <PaginationCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-              />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Pagination with first & last buttons" />
-              <PaginationFirstLastCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-              />
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <SmallHeading title="Pagination with first & last buttons" />
+        <PaginationFirstLastCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+        />
+      </Container>
     </>
   );
 }
