8e87b75cbf692ff4c1e90c55c8f33fe21fe5a860
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:40:22 -0700

    Update documentation outline dropdowns component

--- pages/documentation/react/dropdowns/outline.js
+++ pages/documentation/react/dropdowns/outline.js
@@ -1,120 +1,96 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import DropdownCode from 'components/Documentation/JavaScript/React/DropdownCode';
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import SmallHeading from "components/Documentation/SmallHeading.js";
+import DropdownCode from "components/Documentation/JavaScript/React/DropdownCode";
+
 export default function Dropdowns() {
   const [copy, setCopy] = useState(null);
 
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+      <Header title="React Dropdowns" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Dropdowns"
+          description="Toggle contextual overlays for displaying lists of links and more with the Material Tailwind dropdown plugin."
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+        <SmallHeading title="Dropdown" />
+        <DropdownCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="outline"
+          placement="bottom-start"
+          size="regular"
+          rounded={false}
+          buttonText="Dropdown"
+          rippleEffect='ripple="dark"'
+          ripple="dark"
         />
-        <title>React Dropdowns | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Dropdowns"
-                description="Beautiful dropdowns using React."
-              />
-              <SmallHeading title="Dropdown" />
-              <DropdownCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="outline"
-                placement="bottom-start"
-                size="regular"
-                rounded={false}
-                buttonText="Dropdown"
-                rippleEffect='ripple="dark"'
-                ripple="dark"
-              />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Rounded Dropdown" />
-              <DropdownCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="outline"
-                placement="bottom-start"
-                size="regular"
-                rounded={true}
-                buttonText="Rounded Dropdown"
-                rippleEffect='ripple="dark"'
-                ripple="dark"
-              />
+        <SmallHeading title="Rounded Dropdown" />
+        <DropdownCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="outline"
+          placement="bottom-start"
+          size="regular"
+          rounded={true}
+          buttonText="Rounded Dropdown"
+          rippleEffect='ripple="dark"'
+          ripple="dark"
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Small Dropdown" />
-              <DropdownCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placement="bottom-start"
-                buttonType="outline"
-                size="sm"
-                rounded={false}
-                buttonText="Small Dropdown"
-                rippleEffect='ripple="dark"'
-                ripple="dark"
-              />
+        <SmallHeading title="Small Dropdown" />
+        <DropdownCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placement="bottom-start"
+          buttonType="outline"
+          size="sm"
+          rounded={false}
+          buttonText="Small Dropdown"
+          rippleEffect='ripple="dark"'
+          ripple="dark"
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Regular Dropdown" />
-              <DropdownCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placement="bottom-start"
-                buttonType="outline"
-                size="regular"
-                rounded={false}
-                buttonText="Regular Dropdown"
-                rippleEffect='ripple="dark"'
-                ripple="dark"
-              />
+        <SmallHeading title="Regular Dropdown" />
+        <DropdownCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placement="bottom-start"
+          buttonType="outline"
+          size="regular"
+          rounded={false}
+          buttonText="Regular Dropdown"
+          rippleEffect='ripple="dark"'
+          ripple="dark"
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Large Dropdown" />
-              <DropdownCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placement="bottom-start"
-                buttonType="outline"
-                size="lg"
-                rounded={false}
-                buttonText="Large Dropdown"
-                rippleEffect='ripple="dark"'
-                ripple="dark"
-              />
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <SmallHeading title="Large Dropdown" />
+        <DropdownCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placement="bottom-start"
+          buttonType="outline"
+          size="lg"
+          rounded={false}
+          buttonText="Large Dropdown"
+          rippleEffect='ripple="dark"'
+          ripple="dark"
+        />
+      </Container>
     </>
   );
 }
