5cb8e77dc5b3b6cf1b372aab1a2d32f48ce9d486
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:36:38 -0700

    Update documentation modals component

--- pages/documentation/react/modals.js
+++ pages/documentation/react/modals.js
@@ -1,76 +1,51 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import ModalCode from 'components/Documentation/JavaScript/React/ModalCode.js';
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import SmallHeading from "components/Documentation/SmallHeading.js";
+import ModalCode from "components/Documentation/JavaScript/React/ModalCode.js";
 
 export default function Modals() {
   const [copy, setCopy] = useState(null);
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/small"
+      <Header title="React Modals" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Modals"
+          description="React plugin that opens on top of the page content for extra details, notifications to the user or any other new content."
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/small"
+
+        <SmallHeading title="Small Modal" />
+        <ModalCode
+          copyText={copy === "Modal" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Modal" : "")}
+          size="sm"
+          modalSize="small"
         />
-        <title>React Modals | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Modals"
-                description="React plugin that opens on top of the page content for extra details, notifications to the user or any other new content."
-              />
 
-              <SmallHeading title="React Small Modal" />
-              <ModalCode
-                copyText={copy === 'Modal' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Modal' : '')}
-                size="sm"
-                modalSize="small"
-              />
+        <div className="my-36" />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <SmallHeading title="Regular Modal" />
+        <ModalCode
+          copyText={copy === "Modal" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Modal" : "")}
+          size="regular"
+          modalSize="regular"
+        />
 
-              <SmallHeading title="React Regular Modal" />
-              <ModalCode
-                copyText={copy === 'Modal' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Modal' : '')}
-                size="regular"
-                modalSize="regular"
-              />
+        <div className="my-36" />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
-
-              <SmallHeading title="React Large Modal" />
-              <ModalCode
-                copyText={copy === 'Modal' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Modal' : '')}
-                size="lg"
-                modalSize="large"
-              />
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <SmallHeading title="Large Modal" />
+        <ModalCode
+          copyText={copy === "Modal" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Modal" : "")}
+          size="lg"
+          modalSize="large"
+        />
+      </Container>
     </>
   );
 }
