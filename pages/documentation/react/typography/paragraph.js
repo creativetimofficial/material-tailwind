350b378d79523b177e4a260a1ecb8df623de2ca0
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:28:43 -0700

    Update documentation paragrahp component

--- pages/documentation/react/typography/paragraph.js
+++ pages/documentation/react/typography/paragraph.js
@@ -1,52 +1,27 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import ParagraphCode from 'components/Documentation/JavaScript/React/Typography/ParagraphCode';
-import Heading from 'components/Documentation/Heading.js';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import ParagraphCode from "components/Documentation/JavaScript/React/Typography/ParagraphCode";
 
 export default function Paragraph() {
   const [copy, setCopy] = useState(null);
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+      <Header title="React Paragraph" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Paragraph"
+          description="Material Tailwind paragraph for displaying long text."
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
-        />
-        <title>React Paragraph | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Paragraph"
-                description="Paragraph using React."
-              />
 
-              <ParagraphCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-              />
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <ParagraphCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+        />
+      </Container>
     </>
   );
 }
