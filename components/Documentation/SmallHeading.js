6809de061375d9c4f19f690fb8df3dd37ce74c52
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:45:40 -0700

    Update documentation Small Heading component

--- components/Documentation/SmallHeading.js
+++ components/Documentation/SmallHeading.js
@@ -4,15 +4,13 @@ import PropTypes from "prop-types";
 export default function LargeHeader(props) {
   return (
     <>
-      <h2 className="font-semibold text-xl mb-2 mt-8 text-gray-800">
-        {props.title}
-      </h2>
-      <p className="text-gray-700">{props.description}</p>
+      <h2 className="font-medium text-xl mb-2 text-gray-900">{props.title}</h2>
+      <p className="text-gray-700 mb-4 font-light">{props.description}</p>
     </>
   );
 }
 
 LargeHeader.propTypes = {
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node])
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
