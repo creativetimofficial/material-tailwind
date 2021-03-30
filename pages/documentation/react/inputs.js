7f731ea083273da2e107426b1a30ec051283582a
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:37:39 -0700

    Update documentation inputs component

--- pages/documentation/react/inputs.js
+++ pages/documentation/react/inputs.js
@@ -1,186 +1,161 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import InputCode from 'components/Documentation/JavaScript/React/Inputs/InputCode';
-import InputIconCode from 'components/Documentation/JavaScript/React/Inputs/InputIconCode';
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import SmallHeading from "components/Documentation/SmallHeading.js";
+import InputCode from "components/Documentation/JavaScript/React/Inputs/InputCode";
+import InputIconCode from "components/Documentation/JavaScript/React/Inputs/InputIconCode";
 
 export default function Inputs() {
   const [copy, setCopy] = useState(null);
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+      <Header title="React Inputs" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Inputs"
+          description="Easily create inputs with different status and sizes using Material Tailwind inputs"
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+        <SmallHeading title="Input" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Input"
+          size="regular"
+          outline={false}
         />
-        <title>React Input | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Input"
-                description="Beautiful inputs using react."
-              />
-              <SmallHeading title="Input" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Input"
-                size="regular"
-                outline={false}
-              />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Outline Input" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Outline Input"
-                size="regular"
-                outline={true}
-              />
+        <SmallHeading title="Outline Input" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Outline Input"
+          size="regular"
+          outline={true}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Small Input" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Small Input"
-                size="sm"
-                outline={false}
-              />
+        <SmallHeading title="Small Input" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Small Input"
+          size="sm"
+          outline={false}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Large Input" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Large Input"
-                size="lg"
-                outline={false}
-              />
+        <SmallHeading title="Large Input" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Large Input"
+          size="lg"
+          outline={false}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Small Outline Input" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Small Outline Input"
-                size="sm"
-                outline={true}
-              />
+        <SmallHeading title="Small Outline Input" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Small Outline Input"
+          size="sm"
+          outline={true}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Large Outline Input" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Large Outline Input"
-                size="lg"
-                outline={true}
-              />
+        <SmallHeading title="Large Outline Input" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Large Outline Input"
+          size="lg"
+          outline={true}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Input with Icon" />
-              <InputIconCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Input with Icon"
-                size="regular"
-                outline={false}
-              />
+        <SmallHeading title="Input with Icon" />
+        <InputIconCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Input with Icon"
+          size="regular"
+          outline={false}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Outline Input with Icon" />
-              <InputIconCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Outline Input with Icon"
-                size="regular"
-                outline={true}
-              />
+        <SmallHeading title="Outline Input with Icon" />
+        <InputIconCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Outline Input with Icon"
+          size="regular"
+          outline={true}
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Input with Error" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Input with Error"
-                size="Regular"
-                outline={false}
-                errorMessage="Its an error"
-                error="Its an error"
-              />
+        <SmallHeading title="Input with Error" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Input with Error"
+          size="Regular"
+          outline={false}
+          errorMessage="Its an error"
+          error="Its an error"
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Outline Input with Error" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Outline Input with Error"
-                size="Regular"
-                outline={true}
-                errorMessage="Its an error"
-                error="Its an error"
-              />
+        <SmallHeading title="Outline Input with Error" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Outline Input with Error"
+          size="Regular"
+          outline={true}
+          errorMessage="Its an error"
+          error="Its an error"
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Input with Success" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Input with Success"
-                size="Regular"
-                outline={false}
-                successMessage="Its a success"
-                success="Its a success"
-              />
+        <SmallHeading title="Input with Success" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Input with Success"
+          size="Regular"
+          outline={false}
+          successMessage="Its a success"
+          success="Its a success"
+        />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Outline Input with Success" />
-              <InputCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                placeholder="Outline Input with Success"
-                size="Regular"
-                outline={true}
-                successMessage="Its a success"
-                success="Its a success"
-              />
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <SmallHeading title="Outline Input with Success" />
+        <InputCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          placeholder="Outline Input with Success"
+          size="Regular"
+          outline={true}
+          successMessage="Its a success"
+          success="Its a success"
+        />
+      </Container>
     </>
   );
 }
