d14d072514c33924c7fcfcd9b2dcc03bd0f989cd
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:46:39 -0700

    Update Closing Label Code component

--- components/Documentation/JavaScript/React/Labels/ClosingLabelCode.js
+++ components/Documentation/JavaScript/React/Labels/ClosingLabelCode.js
@@ -1,77 +1,77 @@
-import React, { useState } from 'react';
-import DocsSnippet from 'components/Documentation/DocsSnippet.js';
-import Frameworks from 'components/Documentation/Frameworks.js';
-import Button from 'components/Button/Button';
+import React, { useState } from "react";
+import DocsSnippet from "components/Documentation/DocsSnippet.js";
+import Frameworks from "components/Documentation/Frameworks.js";
+import Button from "components/Button/Button";
 
 const divBgColors = {
-  blueGray: 'bg-blue-gray-100',
-  gray: 'bg-gray-100',
-  brown: 'bg-brown-100',
-  deepOrange: 'bg-deep-orange-100',
-  orange: 'bg-orange-100',
-  amber: 'bg-amber-100',
-  yellow: 'bg-yellow-100',
-  lime: 'bg-lime-100',
-  lightGreen: 'bg-light-green-100',
-  green: 'bg-green-100',
-  teal: 'bg-teal-100',
-  cyan: 'bg-cyan-100',
-  lightBlue: 'bg-light-blue-100',
-  blue: 'bg-blue-100',
-  indigo: 'bg-indigo-100',
-  deepPurple: 'bg-deep-purple-100',
-  purple: 'bg-purple-100',
-  pink: 'bg-pink-100',
-  red: 'bg-red-100',
+  blueGray: "bg-blue-gray-100",
+  gray: "bg-gray-100",
+  brown: "bg-brown-100",
+  deepOrange: "bg-deep-orange-100",
+  orange: "bg-orange-100",
+  amber: "bg-amber-100",
+  yellow: "bg-yellow-100",
+  lime: "bg-lime-100",
+  lightGreen: "bg-light-green-100",
+  green: "bg-green-100",
+  teal: "bg-teal-100",
+  cyan: "bg-cyan-100",
+  lightBlue: "bg-light-blue-100",
+  blue: "bg-blue-100",
+  indigo: "bg-indigo-100",
+  deepPurple: "bg-deep-purple-100",
+  purple: "bg-purple-100",
+  pink: "bg-pink-100",
+  red: "bg-red-100",
 };
 
 const spanBgColors = {
-  blueGray: 'bg-blue-gray-900',
-  gray: 'bg-gray-900',
-  brown: 'bg-brown-900',
-  deepOrange: 'bg-deep-orange-900',
-  orange: 'bg-orange-900',
-  amber: 'bg-amber-900',
-  yellow: 'bg-yellow-900',
-  lime: 'bg-lime-900',
-  lightGreen: 'bg-light-green-900',
-  green: 'bg-green-900',
-  teal: 'bg-teal-900',
-  cyan: 'bg-cyan-900',
-  lightBlue: 'bg-light-blue-900',
-  blue: 'bg-blue-900',
-  indigo: 'bg-indigo-900',
-  deepPurple: 'bg-deep-purple-900',
-  purple: 'bg-purple-900',
-  pink: 'bg-pink-900',
-  red: 'bg-red-900',
+  blueGray: "bg-blue-gray-900",
+  gray: "bg-gray-900",
+  brown: "bg-brown-900",
+  deepOrange: "bg-deep-orange-900",
+  orange: "bg-orange-900",
+  amber: "bg-amber-900",
+  yellow: "bg-yellow-900",
+  lime: "bg-lime-900",
+  lightGreen: "bg-light-green-900",
+  green: "bg-green-900",
+  teal: "bg-teal-900",
+  cyan: "bg-cyan-900",
+  lightBlue: "bg-light-blue-900",
+  blue: "bg-blue-900",
+  indigo: "bg-indigo-900",
+  deepPurple: "bg-deep-purple-900",
+  purple: "bg-purple-900",
+  pink: "bg-pink-900",
+  red: "bg-red-900",
 };
 
 const textColors = {
-  blueGray: 'text-blue-gray-700',
-  gray: 'text-gray-700',
-  brown: 'text-brown-700',
-  deepOrange: 'text-deep-orange-700',
-  orange: 'text-orange-700',
-  amber: 'text-amber-700',
-  yellow: 'text-yellow-700',
-  lime: 'text-lime-700',
-  lightGreen: 'text-light-green-700',
-  green: 'text-green-700',
-  teal: 'text-teal-700',
-  cyan: 'text-cyan-700',
-  lightBlue: 'text-light-blue-700',
-  blue: 'text-blue-700',
-  indigo: 'text-indigo-700',
-  deepPurple: 'text-deep-purple-700',
-  purple: 'text-purple-700',
-  pink: 'text-pink-700',
-  red: 'text-red-700',
+  blueGray: "text-blue-gray-700",
+  gray: "text-gray-700",
+  brown: "text-brown-700",
+  deepOrange: "text-deep-orange-700",
+  orange: "text-orange-700",
+  amber: "text-amber-700",
+  yellow: "text-yellow-700",
+  lime: "text-lime-700",
+  lightGreen: "text-light-green-700",
+  green: "text-green-700",
+  teal: "text-teal-700",
+  cyan: "text-cyan-700",
+  lightBlue: "text-light-blue-700",
+  blue: "text-blue-700",
+  indigo: "text-indigo-700",
+  deepPurple: "text-deep-purple-700",
+  purple: "text-purple-700",
+  pink: "text-pink-700",
+  red: "text-red-700",
 };
 
 export default function ClosingAlertsCode({ copyText, onCopy }) {
-  const [color, setColor] = useState('lightBlue');
-  const [type] = useState('react');
+  const [color, setColor] = useState("lightBlue");
+  const [type] = useState("react");
   const [showLabel, setShowLabel] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [modalText, setModalText] = useState();
@@ -83,15 +83,15 @@ export default function ClosingAlertsCode({ copyText, onCopy }) {
 
     const parentClassName = event.target.parentNode;
 
-    parentClassName.classList.add('opacity-0');
+    parentClassName.classList.add("opacity-0");
 
     Array.from(parentClassName.classList).map((el) =>
-      el.includes('duration') ? (delay = el.split('-')[1]) : null,
+      el.includes("duration") ? (delay = el.split("-")[1]) : null
     );
 
     setTimeout(
       () => setShowLabel(false),
-      delay ? parseInt(delay, 10) + 100 : 250,
+      delay ? parseInt(delay, 10) + 100 : 250
     );
   };
 
@@ -162,7 +162,7 @@ export default function ClosingLabel() {
                 Label
               </span>
               <span
-                className={`${spanBgColors[color]} text-white text-sm leading-none rounded-full w-4 h-4 cursor-pointer p-1 pt-0.5`}
+                className={`${spanBgColors[color]} text-white text-sm leading-none rounded-full w-4 h-4 cursor-pointer grid place-items-center p-0`}
                 onClick={closeLabel}
               >
                 &times;
