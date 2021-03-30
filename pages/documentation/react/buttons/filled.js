60f81a729cb7bbe6ed3f75f5889883632a959304
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:41:31 -0700

    Update documentation filled buttons component

--- pages/documentation/react/buttons/filled.js
+++ pages/documentation/react/buttons/filled.js
@@ -1,184 +1,159 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import ButtonCode from 'components/Documentation/JavaScript/React/ButtonCode';
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
-import Icon from 'components/Icon/Icon';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import SmallHeading from "components/Documentation/SmallHeading.js";
+import ButtonCode from "components/Documentation/JavaScript/React/ButtonCode";
+import Icon from "components/Icon/Icon";
 
 export default function Buttons() {
   const [copy, setCopy] = useState(null);
 
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
+      <Header title="React Buttons" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Buttons"
+          description="Use Material Tailwind buttons for actions in forms, dialogues, and more with support for multiple sizes, states, and more."
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/alerts"
-        />
-        <title>React Buttons | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Buttons"
-                description="Beautiful buttons using react."
-              />
-              <SmallHeading title="Button" />
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="regular"
-                rounded={false}
-                buttonChildren="Button"
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                Button
-              </ButtonCode>
+        <SmallHeading title="Button" />
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="regular"
+          rounded={false}
+          buttonChildren="Button"
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          Button
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Rounded Button" />
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="regular"
-                rounded={true}
-                buttonChildren="Rounded Button"
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                Rounded Button
-              </ButtonCode>
+        <SmallHeading title="Rounded Button" />
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="regular"
+          rounded={true}
+          buttonChildren="Rounded Button"
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          Rounded Button
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Small Button" />
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="sm"
-                rounded={false}
-                buttonChildren="Small Button"
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                Small Button
-              </ButtonCode>
+        <SmallHeading title="Small Button" />
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="sm"
+          rounded={false}
+          buttonChildren="Small Button"
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          Small Button
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Regular Button" />
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="regular"
-                rounded={false}
-                buttonChildren="Regular Button"
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                Regular Button
-              </ButtonCode>
+        <SmallHeading title="Regular Button" />
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="regular"
+          rounded={false}
+          buttonChildren="Regular Button"
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          Regular Button
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="Large Button" />
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="lg"
-                rounded={false}
-                buttonChildren="Large Button"
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                Large Button
-              </ButtonCode>
+        <SmallHeading title="Large Button" />
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="lg"
+          rounded={false}
+          buttonChildren="Large Button"
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          Large Button
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="With Icon Before" />
+        <SmallHeading title="With Icon Before" />
 
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="regular"
-                rounded={false}
-                hasIcon={true}
-                buttonChildren={`<Icon name="favorite" size="sm" /> With Icon Before`}
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                <Icon name="favorite" size="sm" />
-                With Icon Before
-              </ButtonCode>
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="regular"
+          rounded={false}
+          hasIcon={true}
+          buttonChildren={`<Icon name="favorite" size="sm" /> With Icon Before`}
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          <Icon name="favorite" size="sm" />
+          With Icon Before
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="With Icon After" />
+        <SmallHeading title="With Icon After" />
 
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="regular"
-                rounded={false}
-                hasIcon={true}
-                buttonChildren={`With Icon After <Icon name="favorite" size="sm" />`}
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                With Icon After
-                <Icon name="favorite" size="sm" />
-              </ButtonCode>
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="regular"
+          rounded={false}
+          hasIcon={true}
+          buttonChildren={`With Icon After <Icon name="favorite" size="sm" />`}
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          With Icon After
+          <Icon name="favorite" size="sm" />
+        </ButtonCode>
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
+        <div className="my-36" />
 
-              <SmallHeading title="With Icon Only" />
+        <SmallHeading title="With Icon Only" />
 
-              <ButtonCode
-                copyText={copy === 'Buttons' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Buttons' : '')}
-                buttonType="filled"
-                size="regular"
-                rounded={true}
-                hasIcon={true}
-                buttonChildren={`<Icon name="favorite" size="sm" />`}
-                iconOnly
-                rippleEffect='ripple="light"'
-                ripple="light"
-              >
-                <Icon name="favorite" size="sm" />
-              </ButtonCode>
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <ButtonCode
+          copyText={copy === "Buttons" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Buttons" : "")}
+          buttonType="filled"
+          size="regular"
+          rounded={true}
+          hasIcon={true}
+          buttonChildren={`<Icon name="favorite" size="sm" />`}
+          iconOnly
+          rippleEffect='ripple="light"'
+          ripple="light"
+        >
+          <Icon name="favorite" size="sm" />
+        </ButtonCode>
+      </Container>
     </>
   );
 }
