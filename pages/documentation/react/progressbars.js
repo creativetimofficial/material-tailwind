3bcb2b0c44b251ef3dbe65af8fb82d6cae3a6215
sajadevo <nawabisajadahmad@gmail.com>
2021-03-30 01:32:24 -0700

    Update documentation progressbars component

--- pages/documentation/react/progressbars.js
+++ pages/documentation/react/progressbars.js
@@ -1,65 +1,40 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Sidebar from 'components/Documentation/Sidebar.js';
-import routes from 'routes.js';
-import Navbar from 'components/Documentation/Navbar.js';
-import Footer from 'components/Documentation/Footer.js';
-import ProgressCode from 'components/Documentation/JavaScript/React/ProgressCode.js';
-import Heading from 'components/Documentation/Heading.js';
-import SmallHeading from 'components/Documentation/SmallHeading.js';
+import React, { useState } from "react";
+import Header from "components/Documentation/Header";
+import Container from "components/Documentation/Container";
+import Navbar from "components/Documentation/Navbar";
+import Heading from "components/Documentation/Heading.js";
+import SmallHeading from "components/Documentation/SmallHeading.js";
+import ProgressCode from "components/Documentation/JavaScript/React/ProgressCode.js";
 
 export default function Progressbars() {
   const [copy, setCopy] = useState(null);
   return (
     <>
-      <Head>
-        {/* Canonical SEO */}
-        <link
-          rel="canonical"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/small"
+      <Header title="React Progressbars" />
+      <Navbar />
+      <Container>
+        <Heading
+          title="React Progressbars"
+          description="Material Tailwind progressbars components can be used to show a user how far along he/she is in a process."
         />
-        {/* Open Graph data */}
-        <meta
-          property="og:url"
-          content="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/modals/small"
+
+        <SmallHeading title="Simple Progressbar" />
+        <ProgressCode
+          copyText={copy === "Modal" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Modal" : "")}
+          value="50"
         />
-        <title>React Progressbars | Tailwind Starter Kit by Creative Tim</title>
-      </Head>
-      <Navbar fixed />
-      <div className="container max-w-7xl mx-auto mt-4 pt-12">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-3/12 lg:w-2/12 pr-4 tex-left">
-            <Sidebar routes={routes} />
-          </div>
-          <div className="w-full sm:w-9/12 lg:w-8/12 px-4 sm:pr-10 lg:pr-4">
-            <div className="my-8">
-              <Heading
-                title="React Progressbars"
-                description="Different progressbars using react."
-              />
 
-              <SmallHeading title="Simple Progressbar" />
-              <ProgressCode
-                copyText={copy === 'Modal' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Modal' : '')}
-                value="50"
-              />
+        <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
 
-              <hr className="mt-20 mb-20 border-b-1 border-gray-300" />
-
-              <SmallHeading title="Progressbar with percentage text" />
-              <ProgressCode
-                copyText={copy === 'Modal' ? 'Copied' : 'Copy'}
-                onCopy={(text) => setCopy(text ? 'Modal' : '')}
-                value="75"
-                percentage
-              />
-            </div>
-          </div>
-          <div className="w-full lg:w-2/12 px-4 hidden lg:block"></div>
-        </div>
-      </div>
-      <Footer />
+        <SmallHeading title="Progressbar with Percentage" />
+        <ProgressCode
+          copyText={copy === "Modal" ? "Copied" : "Copy"}
+          onCopy={(text) => setCopy(text ? "Modal" : "")}
+          value="75"
+          percentage
+        />
+      </Container>
     </>
   );
 }
