652244343ffddca15fff356f073c76ff79d7e2a8
sajadevo <nawabisajadahmad@gmail.com>
2021-03-31 03:28:29 -0700

    Update the quick-start page

--- pages/documentation/quick-start.js
+++ pages/documentation/quick-start.js
@@ -6,6 +6,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import Heading from "components/Documentation/Heading";
 import SmallHeading from "components/Documentation/SmallHeading";
+import Button from "components/Button/Button";
 
 export default function QuickStart() {
   return (
@@ -27,13 +28,66 @@ export default function QuickStart() {
             {`npm i -E @material-tailwind/react`}
           </SyntaxHighlighter>
         </div>
+
         <SmallHeading
           title="Yarn"
           description={<>Install Material Tailwind via Yarn</>}
         />
-        <SyntaxHighlighter language="html" style={a11yDark}>
-          {`npm i -E @material-tailwind/react`}
-        </SyntaxHighlighter>
+        <div className="mb-12">
+          <SyntaxHighlighter language="html" style={a11yDark}>
+            {`npm i -E @material-tailwind/react`}
+          </SyntaxHighlighter>
+        </div>
+
+        <SmallHeading
+          title="Import Style Sheets"
+          description={
+            <>
+              Import the{" "}
+              <code className="text-light-blue-500">tailwind.css</code> and{" "}
+              <code className="text-light-blue-500">material-icons.css</code>{" "}
+              into your <code className="text-light-blue-500">app.js</code>{" "}
+              file. Make sure to import the{" "}
+              <code className="text-light-blue-500">tailwind.css</code> style
+              sheet after all other style sheets.
+            </>
+          }
+        />
+        <div className="mb-12">
+          <SyntaxHighlighter language="jsx" style={a11yDark}>
+            {`import "material-design-icons/iconfont/material-icons.css";
+import "material-tailwind/react/tailwind.css";`}
+          </SyntaxHighlighter>
+        </div>
+
+        <SmallHeading
+          title="Example"
+          description={<>Import the components you need</>}
+        />
+        <div className="border border-solid border-gray-300 rounded-lg my-4 p-4 pt-6 relative">
+          <div className="pb-4">
+            <Button color="lightBlue" ripple="light">
+              Button
+            </Button>
+          </div>
+
+          <SyntaxHighlighter language="jsx" style={a11yDark}>
+            {`import React from "react";
+import Button from "@material-tailwind/react/Button";
+
+export default function Button() {
+  return (
+    <Button
+      color="lightBlue"
+      ripple="light"
+    >
+      Button
+    </Button>
+  )
+}`}
+            ;
+          </SyntaxHighlighter>
+        </div>
       </Container>
     </>
   );
