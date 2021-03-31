da2058107729a89b15caec57f1fdbd49360c73c9
sajadevo <nawabisajadahmad@gmail.com>
2021-03-31 04:21:47 -0700

    Update the quick-start page

--- pages/documentation/quick-start.js
+++ pages/documentation/quick-start.js
@@ -75,7 +75,7 @@ import "@material-tailwind/react/tailwind.css";`}
             {`import React from "react";
 import Button from "@material-tailwind/react/Button";
 
-export default function Button() {
+export default function Example() {
   return (
     <Button
       color="lightBlue"
