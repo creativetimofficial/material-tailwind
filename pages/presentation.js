aa95196131e329da36860817d637a1d6e54c2291
sajadevo <nawabisajadahmad@gmail.com>
2021-03-31 04:22:01 -0700

    Update the presentation page

--- pages/presentation.js
+++ pages/presentation.js
@@ -190,7 +190,7 @@ import "@material-tailwind/react/tailwind.css";`}
                   {`import React from "react";
 import Button from "@material-tailwind/react/Button";
 
-export default function Button() {
+export default function Example() {
   return (
     <Button
       color="lightBlue"
