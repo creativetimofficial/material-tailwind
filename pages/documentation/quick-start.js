aff7d249036c4d7955194f7f8e99e1aaad29561f
sajadevo <nawabisajadahmad@gmail.com>
2021-03-31 01:37:23 -0700

    Update quick start page

--- pages/documentation/quick-start.js
+++ pages/documentation/quick-start.js
@@ -17,31 +17,22 @@ export default function QuickStart() {
           title="Quick Start"
           description="To get started without any installation, just copy one of the bellow links to our compiled version of Tailwindcss and add it into your project."
         />
+        <Heading title="Installation" />
         <SmallHeading
-          title="All CSS unminified (7.2MB)"
-          description={
-            <>
-              To get all the tailwindcss styles on which these components are
-              presented, include the following code inside your html file:
-            </>
-          }
+          title="NPM"
+          description={<>Install Material Tailwind via NPM</>}
         />
         <div className="mb-12">
           <SyntaxHighlighter language="html" style={a11yDark}>
-            {`<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/tailwind.css">`}
+            {`npm i -E @material-tailwind/react`}
           </SyntaxHighlighter>
         </div>
         <SmallHeading
-          title="Only used CSS minified (26 KB)"
-          description={
-            <>
-              To only get the tailwindcss styles for the presented components,
-              but minified, include the following code inside your html file:
-            </>
-          }
+          title="Yarn"
+          description={<>Install Material Tailwind via Yarn</>}
         />
         <SyntaxHighlighter language="html" style={a11yDark}>
-          {`<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css">`}
+          {`npm i -E @material-tailwind/react`}
         </SyntaxHighlighter>
       </Container>
     </>
