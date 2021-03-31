475223946cdb1befbf2774bd17cf1a9f0f5fafa8
EINazare <emanuelioannazare@gmail.com>
2021-03-31 15:37:02 +0300

    fix: solve preffix for build

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ module.exports = withFonts(
   withCSS(
     withImages(
       withSass({
+        assetPrefix:
+          process.env.NODE_ENV === "production"
+            ? "/material-tailwind"
+            : "",
         webpack(config, options) {
           config.module.rules.push({
             test: /\.(eot|ttf|woff|woff2)$/,
