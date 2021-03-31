657f3bf37ebee9390a9c9c72c0ab9b6063674f66
EINazare <emanuelioannazare@gmail.com>
2021-03-31 14:22:36 +0300

    fix: solve license links

--- pages/components.js
+++ pages/components.js
@@ -957,7 +957,7 @@ export default function Components() {
                 <a
                   className="text-gray-700 hover:text-gray-900 font-medium block pb-2 text-sm"
                   target="_blank"
-                  href="https://github.com/creativetimofficial/material-tailwind/blob/master/LICENSE.md?ref=material-tailwind"
+                  href="https://github.com/creativetimofficial/material-tailwind/blob/main/LICENSE.md?ref=material-tailwind"
                 >
                   MIT License
                 </a>
