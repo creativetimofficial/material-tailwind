b9ec4fa5096a10d59f8bf64fb93aed2aa67a0614
sajadevo <nawabisajadahmad@gmail.com>
2021-04-01 23:09:19 -0700

    Update Login template

--- pages/templates/login.js
+++ pages/templates/login.js
@@ -1,6 +1,7 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import LoginPage from "./Login/LoginPage";
+import LoginNavbar from "./Login/LoginNavbar";
+import LoginFooter from "./Login/LoginFooter";
+import LoginContainer from "./Login/LoginContainer";
 import Card from "components/Card/Card";
 import CardBody from "components/Card/CardBody";
 import CardFooter from "components/Card/CardFooter";
@@ -10,66 +11,55 @@ import Button from "components/Button/Button";
 
 export default function Login() {
   return (
-    <div
-      className="w-screen h-screen relative bg-position flex flex-col justify-between"
-      style={{
-        background: `linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.7)), url(${require("../../assets/img/background-1920x1280.jpg")})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}
-    >
-      <Navbar />
+    <LoginPage>
+      <LoginNavbar />
+      <LoginContainer>
+        <Card>
+          <div className="bg-gradient-to-tr from-light-blue-500 to-light-blue-700 -mt-12 rounded-lg text-white px-12 py-8 mb-8 text-center shadow-xl">
+            <H5>Login</H5>
+          </div>
 
-      <div className="grid place-items-center">
-        <div className="top-1/2 max-w-sm w-10/12">
-          <Card>
-            <div className="bg-gradient-to-tr from-light-blue-500 to-light-blue-700 -mt-12 rounded-lg text-white px-12 py-8 mb-8 text-center shadow-xl">
-              <H5>Login</H5>
+          <CardBody>
+            <div className="mb-8 px-4">
+              <InputIcon
+                type="text"
+                color="lightBlue"
+                placeholder="First Name"
+                iconName="account_circle"
+              />
             </div>
-
-            <CardBody>
-              <div className="mb-8">
-                <InputIcon
-                  type="text"
-                  color="lightBlue"
-                  placeholder="First Name"
-                  iconName="account_circle"
-                />
-              </div>
-              <div className="mb-8">
-                <InputIcon
-                  type="email"
-                  color="lightBlue"
-                  placeholder="Email Address"
-                  iconName="email"
-                />
-              </div>
-              <div className="mb-8">
-                <InputIcon
-                  type="password"
-                  color="lightBlue"
-                  placeholder="password"
-                  iconName="lock"
-                />
-              </div>
-            </CardBody>
-            <CardFooter>
-              <div className="flex justify-center">
-                <Button
-                  color="lightBlue"
-                  buttonType="link"
-                  size="lg"
-                  ripple="dark"
-                >
-                  Get Started
-                </Button>
-              </div>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
-
-      <Footer />
-    </div>
+            <div className="mb-8 px-4">
+              <InputIcon
+                type="email"
+                color="lightBlue"
+                placeholder="Email Address"
+                iconName="email"
+              />
+            </div>
+            <div className="mb-8 px-4">
+              <InputIcon
+                type="password"
+                color="lightBlue"
+                placeholder="password"
+                iconName="lock"
+              />
+            </div>
+          </CardBody>
+          <CardFooter>
+            <div className="flex justify-center">
+              <Button
+                color="lightBlue"
+                buttonType="link"
+                size="lg"
+                ripple="dark"
+              >
+                Get Started
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
+      </LoginContainer>
+      <LoginFooter />
+    </LoginPage>
   );
 }
