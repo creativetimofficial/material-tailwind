4e713b623102b2985fe6059d17e820dad9296f7c
sajadevo <nawabisajadahmad@gmail.com>
2021-04-02 10:13:10 -0700

    Update Login Navbar component

--- pages/templates/Login/LoginNavbar.js
+++ pages/templates/Login/LoginNavbar.js
@@ -10,7 +10,7 @@ export default function LoginFooter() {
   const rippleLight = (e) => ripple.create(e, "light");
 
   return (
-    <nav className="sticky top-0 z-40 lg:z-40 w-full bg-transparent flex items-center justify-between px-2 py-4">
+    <nav className="w-full bg-transparent flex items-center justify-between px-2 py-4">
       <div className="container max-w-7xl px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <Link
