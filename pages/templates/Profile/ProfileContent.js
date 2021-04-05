896298509752c44faee3442ed9bf3837e70d27b1
sajadevo <nawabisajadahmad@gmail.com>
2021-04-05 01:59:45 -0700

    Update Profile Content component

--- pages/templates/Profile/ProfileContent.js
+++ pages/templates/Profile/ProfileContent.js
@@ -4,7 +4,7 @@ export default function ProfileContent({ children }) {
   return (
     <section className="relative py-16 bg-gray-50">
       <div className="container max-w-7xl px-4 mx-auto">
-        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-xl -mt-64">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-2xl rounded-xl -mt-64">
           <div className="px-6">{children}</div>
         </div>
       </div>
