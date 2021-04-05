11af7be3a2e18e3562d7430b23a292f760340375
sajadevo <nawabisajadahmad@gmail.com>
2021-04-05 01:59:57 -0700

    Update Profile Content Body component

--- pages/templates/Profile/ProfileContentBody.js
+++ pages/templates/Profile/ProfileContentBody.js
@@ -1,23 +1,21 @@
 import React from "react";
+import H3 from "components/Typography/Heading3";
+import Icon from "components/Icon/Icon";
 
 export default function ProfileContentBody() {
   return (
-    <div className="text-center mt-8">
-      <h3 className="text-4xl font-semibold leading-normal text-gray-900 mb-2">
-        Jenna Stones
-      </h3>
-      <div className="text-sm mt-0 mb-2 text-gray-700 font-bold uppercase flex items-center justify-center">
-        <i className="material-icons mr-2 text-xl text-gray-700">place</i> Los
-        Angeles, California
+    <div className="text-center my-8">
+      <H3 color="gray">Jenna Stones</H3>
+      <div className="mt-0 mb-2 text-gray-700 font-medium flex items-center justify-center gap-2">
+        <Icon name="place" size="xl" />
+        Los Angeles, California
       </div>
-      <div className="mb-2 text-gray-700 mt-10 flex items-center justify-center">
-        <i className="material-icons mr-2 text-xl text-gray-700">work</i>
+      <div className="mb-2 text-gray-700 mt-10 flex items-center justify-center gap-2">
+        <Icon name="work" size="xl" />
         Solution Manager - Creative Tim Officer
       </div>
-      <div className="mb-2 text-gray-700 flex items-center justify-center">
-        <i className="material-icons mr-2 text-xl text-gray-700">
-          account_balance
-        </i>
+      <div className="mb-2 text-gray-700 flex items-center justify-center gap-2">
+        <Icon name="account_balance" size="xl" />
         University of Computer Science
       </div>
     </div>
