3a46c2128719874cdbe5be6519a4651aa27ac2a4
sajadevo <nawabisajadahmad@gmail.com>
2021-04-06 04:23:11 -0700

    Update Login Page Page

--- pages/templates/Login/LoginContainer.js
+++ pages/templates/Login/LoginContainer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function LoginContainer({ children }) {
   return (
-    <div className="grid place-items-center">
-      <div className="top-1/2 max-w-sm w-10/12">{children}</div>
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="max-w-sm w-96">{children}</div>
     </div>
   );
 }
