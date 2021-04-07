e2f1ed2606df439442861fcd3bec41bf953fae41
EINazare <emanuelioannazare@gmail.com>
2021-04-07 17:13:28 +0300

    fix: solve analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,28 +6,10 @@ export default class CustomDocument extends Document {
     return (
       <Html lang="en-US" style={{ scrollBehavior: 'smooth !important' }}>
         <Head>
-          {/* Anti-flicker snippet (recommended)  */}
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `.async-hide { opacity: 0 !important} `,
-            }}
-          ></style>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-K9BGS8K':true});`,
-            }}
-          ></script>
-          {/* Analytics-Optimize Snippet */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-46172202-22', 'auto', {allowLinker: true});ga('set', 'anonymizeIp', true);ga('require', 'GTM-K9BGS8K');ga('require', 'displayfeatures');ga('require', 'linker');ga('linker:autoLink', ["2checkout.com","avangate.com"]);`,
-            }}
-          ></script>
-          {/* end Analytics-Optimize Snippet */}
           {/* Google Tag Manager */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NKDMSK6');`,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KB2WKJS');`,
             }}
           ></script>
           {/* End Google Tag Manager */}
@@ -37,7 +19,7 @@ export default class CustomDocument extends Document {
           <noscript>
             <iframe
               title="google-tag-manage"
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KB2WKJS"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
