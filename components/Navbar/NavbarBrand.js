3ac260f59e11bbb103a269b542c9c35fe7f18b1e
sajadevo <nawabisajadahmad@gmail.com>
2021-04-13 03:29:48 -0700

    Add the rest props to navbar brand component

--- components/Navbar/NavbarBrand.js
+++ components/Navbar/NavbarBrand.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function NavbarBrand({ children }) {
-  return (
-    <a
-      className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white`}
-      href="#pablo"
-    >
-      {children}
-    </a>
-  );
+export default function NavbarBrand({ children, ...rest }) {
+    return (
+        <a
+            {...rest}
+            className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white`}
+        >
+            {children}
+        </a>
+    );
 }
 
 NavbarBrand.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
