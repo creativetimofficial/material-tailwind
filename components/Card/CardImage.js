2edeaea7186f15da36ee205f05bc9c167d6b6c1e
sajadevo <nawabisajadahmad@gmail.com>
2021-04-13 03:00:36 -0700

    Fix margin top for card image component

--- components/Card/CardImage.js
+++ components/Card/CardImage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function CardImage({ src, ...rest }) {
-  return (
-    <img {...rest} className="w-full rounded-lg -mt-8 shadow-lg" src={src} />
-  );
+    return (
+        <img
+            {...rest}
+            className="w-full rounded-lg -mt-9 shadow-lg"
+            src={src}
+        />
+    );
 }
 
 CardImage.propTypes = {
-  src: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
 };
