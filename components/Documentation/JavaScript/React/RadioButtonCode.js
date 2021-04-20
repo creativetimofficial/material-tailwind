87e2db2cd7a066ed7e13bc546157abd7ae43c73e
sajadevo <nawabisajadahmad@gmail.com>
2021-04-20 13:29:34 -0700

    Add radio button code

---
+++ components/Documentation/JavaScript/React/RadioButtonCode.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import DocsSnippet from 'components/Documentation/DocsSnippet.js';
+import Radio from 'components/Radio/Radio';
+
+export default function RadioButtonCode({ copyText, onCopy }) {
+    const [color, setColor] = useState('lightBlue');
+    const [type, setType] = useState('react');
+
+    const codeToShow = `import React from "react";
+import Radio from "@material-tailwind/react/Radio"
+
+export default function RadioButton() {
+  return (
+    <Radio
+      color="${color}"
+      text="Option 1"
+      id="option-1"
+      name="option"
+    />
+    <Radio
+      color="${color}"
+      text="Option 2"
+      id="option-2"
+      name="option"
+    />
+  )
+}`;
+
+    return (
+        <>
+            <DocsSnippet
+                copyText={copyText}
+                onCopy={onCopy}
+                activeColor={color}
+                activeFramework={type}
+                codeToShow={codeToShow}
+                onColorClick={(color) => setColor(color)}
+                onFrameworkClick={(type) => setType(type)}
+            >
+                <div className="flex justify-center py-5">
+                    <div className="mr-10">
+                        <Radio
+                            color={color}
+                            text="Option 1"
+                            id="option-1"
+                            name="option"
+                        />
+                    </div>
+                    <div>
+                        <Radio
+                            color={color}
+                            text="Option 2"
+                            id="option-2"
+                            name="option"
+                        />
+                    </div>
+                </div>
+            </DocsSnippet>
+        </>
+    );
+}
