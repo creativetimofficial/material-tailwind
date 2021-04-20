c1cc07f2047ac8d5a19f25670688ab163d209a6d
sajadevo <nawabisajadahmad@gmail.com>
2021-04-20 01:09:00 -0700

    Remove dropdown outline while focusing on it

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -336,7 +336,7 @@ export default function Dropdown({
             <Tippy
                 content={
                     <div
-                        className={`bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 transition-all duration-500`}
+                        className={`bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 overflow-hidden transition-all duration-500`}
                         style={{ minWidth: '10rem' }}
                     >
                         {children}
