19c82c1b268ba3f832038f24af433d0994f506af
sajadevo <nawabisajadahmad@gmail.com>
2021-04-23 04:40:45 -0700

    Update menu code component

--- components/Documentation/JavaScript/React/Menus/MenuCode.js
+++ components/Documentation/JavaScript/React/Menus/MenuCode.js
@@ -12,14 +12,14 @@ import NavItem from 'components/Nav/NavItem';
 import Icon from 'components/Icon/Icon';
 
 export default function MenuCode({ copyText, onCopy }) {
-  const [color, setColor] = useState('lightBlue');
-  const [type] = useState('react');
-  const [showModal, setShowModal] = useState(false);
-  const [modalText, setModalText] = useState();
-  const [openMenu, setOpenMenu] = useState(false);
-  const onFrameworkClick = Frameworks(type, setShowModal, setModalText);
+    const [color, setColor] = useState('lightBlue');
+    const [type] = useState('react');
+    const [showModal, setShowModal] = useState(false);
+    const [modalText, setModalText] = useState();
+    const [openMenu, setOpenMenu] = useState(false);
+    const onFrameworkClick = Frameworks(type, setShowModal, setModalText);
 
-  const codeToShow = `import React, { useState } from "react";
+    const codeToShow = `import React, { useState } from "react";
 import Navbar from "@material-tailwind/react/Navbar";
 import NavbarContainer from "@material-tailwind/react/NavbarContainer";
 import NavbarWrapper from "@material-tailwind/react/NavbarWrapper";
@@ -38,7 +38,7 @@ export default function Menu() {
       <NavbarContainer>
         <NavbarWrapper>
           <NavbarBrand>Menu</NavbarBrand>
-          <NavbarToggler onClick={() => setOpenMenu(!openMenu)} />
+          <NavbarToggler color="white" onClick={() => setOpenMenu(!openMenu)} ripple="light" />
         </NavbarWrapper>
 
         <NavbarCollapse open={openMenu}>
@@ -66,79 +66,87 @@ export default function Menu() {
   );
 }`;
 
-  return (
-    <>
-      {showModal ? (
+    return (
         <>
-          <div
-            className="block overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            onClick={() => setShowModal(false)}
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-sm">
-              {/*content*/}
-              <div className="border-0 rounded shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                  <h5 className="text-2xl font-normal mt-0 mb-0 ">
-                    Component not supported
-                  </h5>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-4">
-                    At the moment we do not support the choosen framework (
-                    {modalText}) for this component.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-      <DocsSnippet
-        copyText={copyText}
-        onCopy={onCopy}
-        activeColor={color}
-        activeFramework={type}
-        codeToShow={codeToShow}
-        onColorClick={(color) => setColor(color)}
-        onFrameworkClick={onFrameworkClick}
-      >
-        <Navbar color={color}>
-          <NavbarContainer>
-            <NavbarWrapper>
-              <NavbarBrand>Menu</NavbarBrand>
-              <NavbarToggler onClick={() => setOpenMenu(!openMenu)} />
-            </NavbarWrapper>
+            {showModal ? (
+                <>
+                    <div
+                        className="block overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        onClick={() => setShowModal(false)}
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-sm">
+                            {/*content*/}
+                            <div className="border-0 rounded shadow-sm relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                                    <h5 className="text-2xl font-normal mt-0 mb-0 ">
+                                        Component not supported
+                                    </h5>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="text-base leading-relaxed text-gray-600 font-normal mt-0 mb-4">
+                                        At the moment we do not support the
+                                        choosen framework ({modalText}) for this
+                                        component.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+            <DocsSnippet
+                copyText={copyText}
+                onCopy={onCopy}
+                activeColor={color}
+                activeFramework={type}
+                codeToShow={codeToShow}
+                onColorClick={(color) => setColor(color)}
+                onFrameworkClick={onFrameworkClick}
+            >
+                <Navbar color={color}>
+                    <NavbarContainer>
+                        <NavbarWrapper>
+                            <NavbarBrand>Menu</NavbarBrand>
+                            <NavbarToggler
+                                onClick={() => setOpenMenu(!openMenu)}
+                                ripple="light"
+                            />
+                        </NavbarWrapper>
 
-            <NavbarCollapse open={openMenu}>
-              <Nav>
-                <NavItem active="light" href="#navbar" ripple="light">
-                  <Icon name="language" size="xl" />
-                  Discover
-                </NavItem>
-                <NavItem href="#navbar" ripple="light">
-                  <Icon name="account_circle" size="xl" />
-                  Profile
-                </NavItem>
-                <NavItem href="#navbar" ripple="light">
-                  <Icon name="settings" size="xl" />
-                  Settings
-                </NavItem>
-              </Nav>
-            </NavbarCollapse>
-          </NavbarContainer>
-        </Navbar>
-      </DocsSnippet>
-    </>
-  );
+                        <NavbarCollapse open={openMenu}>
+                            <Nav>
+                                <NavItem
+                                    active="light"
+                                    href="#navbar"
+                                    ripple="light"
+                                >
+                                    <Icon name="language" size="xl" />
+                                    Discover
+                                </NavItem>
+                                <NavItem href="#navbar" ripple="light">
+                                    <Icon name="account_circle" size="xl" />
+                                    Profile
+                                </NavItem>
+                                <NavItem href="#navbar" ripple="light">
+                                    <Icon name="settings" size="xl" />
+                                    Settings
+                                </NavItem>
+                            </Nav>
+                        </NavbarCollapse>
+                    </NavbarContainer>
+                </Navbar>
+            </DocsSnippet>
+        </>
+    );
 }
