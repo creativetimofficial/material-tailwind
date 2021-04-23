238bb100edfda6b989b292d47351b59d6ec1bc54
sajadevo <nawabisajadahmad@gmail.com>
2021-04-23 04:29:52 -0700

    Update tab component shadow

--- components/Tab/Tab.js
+++ components/Tab/Tab.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Tab({ children }) {
-  return (
-    <div className="relative flex flex-col items-center justify-center bg-white rounded-lg shadow w-full p-4 my-8">
-      {children}
-    </div>
-  );
+    return (
+        <div className="relative flex flex-col items-center justify-center bg-white rounded-lg shadow-md w-full p-4 my-8">
+            {children}
+        </div>
+    );
 }
 
 Tab.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
