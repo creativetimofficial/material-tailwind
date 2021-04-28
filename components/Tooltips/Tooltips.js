bbf3f22d4d44707be1af95e829d1c95eb7a9721b
sajadevo <nawabisajadahmad@gmail.com>
2021-04-28 03:31:51 -0700

    Remove tipp.js arrow and animation styles sheet from tooltips component

--- components/Tooltips/Tooltips.js
+++ components/Tooltips/Tooltips.js
@@ -2,26 +2,24 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Tippy from '@tippyjs/react';
 import { roundArrow } from 'tippy.js';
-import 'tippy.js/dist/svg-arrow.css';
-import 'tippy.js/animations/shift-away.css';
 
 const Tooltip = forwardRef(({ children, placement }, ref) => {
-  return (
-    <Tippy
-      content={children}
-      placement={placement}
-      reference={ref}
-      animation="shift-away"
-      arrow={roundArrow}
-      className="arrow-dark"
-      interactive
-    />
-  );
+    return (
+        <Tippy
+            content={children}
+            placement={placement}
+            reference={ref}
+            animation="shift-away"
+            arrow={roundArrow}
+            className="arrow-dark"
+            interactive
+        />
+    );
 });
 
 Tooltip.propTypes = {
-  children: PropTypes.node.isRequired,
-  placement: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+    placement: PropTypes.string.isRequired,
 };
 
 export default Tooltip;
