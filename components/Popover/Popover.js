bfda0e6aeefc39fb0a2064cf98214287c987664a
sajadevo <nawabisajadahmad@gmail.com>
2021-04-28 03:31:14 -0700

    Remove tipp.js arrow and animation styles sheet from popover component

--- components/Popover/Popover.js
+++ components/Popover/Popover.js
@@ -2,27 +2,25 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Tippy from '@tippyjs/react';
 import { roundArrow } from 'tippy.js';
-import 'tippy.js/dist/svg-arrow.css';
-import 'tippy.js/animations/shift-away.css';
 
 const Popover = forwardRef(({ children, placement }, ref) => {
-  return (
-    <Tippy
-      content={children}
-      placement={placement}
-      reference={ref}
-      trigger="click"
-      animation="shift-away"
-      arrow={roundArrow}
-      className="arrow-light"
-      interactive
-    />
-  );
+    return (
+        <Tippy
+            content={children}
+            placement={placement}
+            reference={ref}
+            trigger="click"
+            animation="shift-away"
+            arrow={roundArrow}
+            className="arrow-light"
+            interactive
+        />
+    );
 });
 
 Popover.propTypes = {
-  children: PropTypes.node.isRequired,
-  placement: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+    placement: PropTypes.string.isRequired,
 };
 
 export default Popover;
