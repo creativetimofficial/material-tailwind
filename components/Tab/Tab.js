a44744e0f58b2f23faa2a3218a3c037ac12f102d
sajadevo <nawabisajadahmad@gmail.com>
2021-05-07 04:26:32 -0700

    Add className prop for tab components

--- components/Tab/Tab.js
+++ components/Tab/Tab.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Tab({ children }) {
+export default function Tab({ children, className }) {
     return (
-        <div className="relative flex flex-col items-center justify-center bg-white rounded-lg shadow-md w-full p-4 my-8">
+        <div
+            className={`relative flex flex-col items-center justify-center bg-white rounded-lg shadow-md w-full p-4 my-8 ${className}`}
+        >
             {children}
         </div>
     );
--- components/Tab/TabPane.js
+++ components/Tab/TabPane.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function TabPane({ children, active }) {
-  return <div className={active ? 'block' : 'hidden'}>{children}</div>;
+export default function TabPane({ children, active, className }) {
+    return (
+        <div className={`${active ? 'block' : 'hidden'} ${className}`}>
+            {children}
+        </div>
+    );
 }
 
 TabPane.propTypes = {
-  children: PropTypes.node.isRequired,
-  active: PropTypes.bool.isRequired,
+    children: PropTypes.node.isRequired,
+    active: PropTypes.bool.isRequired,
 };
