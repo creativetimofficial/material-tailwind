dac78430cd378b0dd9a40cc1ae1bb619ff1da6b7
sajadevo <nawabisajadahmad@gmail.com>
2021-05-12 02:34:39 -0700

    Add the material icons link to the head and remove it from the index.css

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,10 @@ export default function MyApp({ Component, pageProps }) {
                 />
                 <meta name="theme-color" content="#03a9f4" />
                 <link
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    rel="stylesheet"
+                />
+                <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
                     integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
