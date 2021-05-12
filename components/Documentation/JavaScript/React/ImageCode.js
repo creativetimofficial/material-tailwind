faa1fb8ac052b66f1df5b75f4675a13cbf9d07e0
sajadevo <nawabisajadahmad@gmail.com>
2021-05-12 03:16:33 -0700

    Update image documentatio code

--- components/Documentation/JavaScript/React/ImageCode.js
+++ components/Documentation/JavaScript/React/ImageCode.js
@@ -3,20 +3,20 @@ import DocsSnippet from 'components/Documentation/DocsSnippet.js';
 import Image from 'components/Image/Image';
 
 export default function ButtonsCode({
-  copyText,
-  onCopy,
-  src,
-  rounded,
-  raised,
-  alt,
+    copyText,
+    onCopy,
+    src,
+    rounded,
+    raised,
+    alt,
 }) {
-  const codeToShow = `import React from "react";
+    const codeToShow = `import React from "react";
 import Image from "@material-tailwind/react/Image";
 
 export default function Image() {
   return (
     <Image
-      src="${src}"
+      src="image-link-here"
       rounded={${rounded}}
       raised={${raised}}
       alt="${alt}"
@@ -24,15 +24,24 @@ export default function Image() {
   )
 }`;
 
-  return (
-    <>
-      <DocsSnippet copyText={copyText} onCopy={onCopy} codeToShow={codeToShow}>
-        <div className="flex justify-center w-full">
-          <div className="w-72">
-            <Image src={src} rounded={rounded} raised={raised} alt={alt} />
-          </div>
-        </div>
-      </DocsSnippet>
-    </>
-  );
+    return (
+        <>
+            <DocsSnippet
+                copyText={copyText}
+                onCopy={onCopy}
+                codeToShow={codeToShow}
+            >
+                <div className="flex justify-center w-full">
+                    <div className="w-72">
+                        <Image
+                            src={src}
+                            rounded={rounded}
+                            raised={raised}
+                            alt={alt}
+                        />
+                    </div>
+                </div>
+            </DocsSnippet>
+        </>
+    );
 }
