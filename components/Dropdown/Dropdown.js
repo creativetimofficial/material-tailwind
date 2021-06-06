4f9f5bdb679584c4712f61b81dd6a3fe7002d8c1
Velten Heyn <github@valoric.de>
2021-06-06 07:40:46 +0200

    Add class to tippy dropdown content
    To be able to switch of minWidth

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -278,6 +278,7 @@ export default function Dropdown({
     block,
     ripple,
     className,
+    contentClassName,
     ...rest
 }) {
     const [dropdownShow, setDropdownShow] = React.useState(false);
@@ -380,8 +381,7 @@ export default function Dropdown({
             <Tippy
                 content={
                     <div
-                        className={`bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 p-2 overflow-hidden transition-all duration-500`}
-                        style={{ minWidth: '10rem' }}
+                        className={`bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 p-2 overflow-hidden transition-all duration-500 min-w-10 ${contentClassName}`}
                     >
                         {children}
                     </div>
