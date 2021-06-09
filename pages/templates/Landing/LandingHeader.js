b1c4983f46eaf525580dddd10a7d30a85e6cd514
sajadevo <nawabisajadahmad@gmail.com>
2021-06-09 02:24:05 -0700

    Remove the templates from the repo

--- pages/templates/Landing/LandingHeader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LandingHeader({ children }) {
-  return (
-    <div className="relative pt-16 pb-32 flex content-center items-center justify-center h-screen">
-      {children}
-    </div>
-  );
-}
--- pages/templates/Landing/LandingTeamSection.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LandingTeamSection({ children }) {
-  return (
-    <section className="pt-20 pb-48">
-      <div className="container max-w-7xl mx-auto px-4">{children}</div>
-    </section>
-  );
-}
