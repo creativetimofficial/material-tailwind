de3747869743c59bcaeb7a8f6e2991c36ecce9ca
sajadevo <nawabisajadahmad@gmail.com>
2022-03-19 00:32:04 +0430

    Update the button component

--- packages/material-tailwind-react/src/components/Button/index.js
+++ packages/material-tailwind-react/src/components/Button/index.js
@@ -2,29 +2,8 @@ import { forwardRef, useContext } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 import Ripple from "material-ripple-effects";
+import validColors from "utils/validColors";
 import { MaterialTailwindTheme } from "context/theme";
-
-const validColors = {
-  "blue-grey": "blue-grey",
-  grey: "grey",
-  brown: "brown",
-  "deep-orange": "deep-orange",
-  orange: "orange",
-  amber: "amber",
-  yellow: "yellow",
-  lime: "lime",
-  "light-green": "light-green",
-  green: "green",
-  teal: "teal",
-  cyan: "cyan",
-  "light-blue": "light-blue",
-  blue: "blue",
-  indigo: "indigo",
-  "deep-purple": "deep-purple",
-  purple: "purple",
-  pink: "pink",
-  red: "red",
-};
 
 export const Button = forwardRef(
   ({ variant, size, color, fullWidth, ripple, className, children, ...rest }, ref) => {
