ac596c745274ef0e9a66bc6cf74e5f9361d6247d
sajadevo <nawabisajadahmad@gmail.com>
2022-03-21 23:01:50 +0430

    Add the react-sections to the purge content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ const withMT = require("./packages/material-tailwind-react/src/utils/withMT");
 module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./react-sections/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {}
