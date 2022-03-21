97aa34f6982c43711fa0bf515ffae6e7b01e461a
sajadevo <nawabisajadahmad@gmail.com>
2022-03-21 23:44:20 +0430

    Add rhe icon buttons section

---
+++ pages/react.js
@@ -0,0 +1,27 @@
+import Alerts from "/react-sections/Alerts";
+import Avatars from "/react-sections/Avatars";
+import Buttons from "/react-sections/Buttons";
+import Chips from "/react-sections/Chips";
+import IconButtons from "/react-sections/IconButtons";
+
+export default function React() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center p-12 mb-12">
+        React Version Components
+      </h1>
+
+      <div className="px-48">
+        <Alerts />
+        <hr className="mb-24" />
+        <Avatars />
+        <hr className="my-24" />
+        <Buttons />
+        <hr className="my-24" />
+        <Chips />
+        <hr className="my-24" />
+        <IconButtons />
+      </div>
+    </div>
+  );
+}
