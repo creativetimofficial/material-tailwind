83a391cdcd066cf45fd44e5c6024f47ae1bf890a
sajadevo <nawabisajadahmad@gmail.com>
2022-03-22 16:15:48 +0430

    Update the buttons section

--- react-sections/Buttons.js
+++ react-sections/Buttons.js
@@ -16,36 +16,78 @@ export default function Buttons() {
 
       <h2 className="text-2xl font-semibold mb-4">Button Sizes</h2>
       <div className="flex items-end gap-4 mb-12">
-        <Button size="sm">Small</Button>
-        <Button>Medium</Button>
-        <Button size="lg">Large</Button>
+        <Button variant="gradient" size="sm">
+          Small
+        </Button>
+        <Button variant="gradient">Medium</Button>
+        <Button variant="gradient" size="lg">
+          Large
+        </Button>
       </div>
 
       <h2 className="text-2xl font-semibold mb-4">Button Colors</h2>
       <div className="flex flex-wrap items-end gap-4 mb-12">
-        <Button color="blue-grey">blue grey</Button>
-        <Button color="grey">grey</Button>
-        <Button color="brown">brown</Button>
-        <Button color="deep-orange">deep orange</Button>
-        <Button color="orange">orange</Button>
-        <Button color="amber">amber</Button>
-        <Button color="yellow">yellow</Button>
-        <Button color="lime">lime</Button>
-        <Button color="light-green">light green</Button>
-        <Button color="green">green</Button>
-        <Button color="teal">teal</Button>
-        <Button color="cyan">cyan</Button>
-        <Button>light blue</Button>
-        <Button color="blue">blue</Button>
-        <Button color="indigo">indigo</Button>
-        <Button color="deep-purple">deep purple</Button>
-        <Button color="purple">purple</Button>
-        <Button color="pink">pink</Button>
-        <Button color="red">red</Button>
+        <Button variant="gradient" color="blue-grey">
+          blue grey
+        </Button>
+        <Button variant="gradient" color="grey">
+          grey
+        </Button>
+        <Button variant="gradient" color="brown">
+          brown
+        </Button>
+        <Button variant="gradient" color="deep-orange">
+          deep orange
+        </Button>
+        <Button variant="gradient" color="orange">
+          orange
+        </Button>
+        <Button variant="gradient" color="amber">
+          amber
+        </Button>
+        <Button variant="gradient" color="yellow">
+          yellow
+        </Button>
+        <Button variant="gradient" color="lime">
+          lime
+        </Button>
+        <Button variant="gradient" color="light-green">
+          light green
+        </Button>
+        <Button variant="gradient" color="green">
+          green
+        </Button>
+        <Button variant="gradient" color="teal">
+          teal
+        </Button>
+        <Button variant="gradient" color="cyan">
+          cyan
+        </Button>
+        <Button variant="gradient">light blue</Button>
+        <Button variant="gradient" color="blue">
+          blue
+        </Button>
+        <Button variant="gradient" color="indigo">
+          indigo
+        </Button>
+        <Button variant="gradient" color="deep-purple">
+          deep purple
+        </Button>
+        <Button variant="gradient" color="purple">
+          purple
+        </Button>
+        <Button variant="gradient" color="pink">
+          pink
+        </Button>
+        <Button variant="gradient" color="red">
+          red
+        </Button>
       </div>
 
       <h2 className="text-2xl font-semibold mb-4">Button with Full Width</h2>
-      <Button fullWidth>Full Width</Button>
+      <Button variant="gradient" fullWidth>
+        Full Width
+      </Button>
     </div>
   );
 }
