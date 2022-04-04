67f620c2010a969375970864fb9cfa646b12ced3
sajadevo <nawabisajadahmad@gmail.com>
2022-04-04 17:30:46 +0430

    Update the theme context

--- packages/material-tailwind-react/src/context/theme.js
+++ packages/material-tailwind-react/src/context/theme.js
@@ -1,19 +1,23 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import PropTypes from "prop-types";
 import theme from "theme/index";
+import combineMerge from "utils/combineMerge";
 
 const merge = require("deepmerge");
 
 export const MaterialTailwindTheme = createContext(theme);
+
 MaterialTailwindTheme.displayName = "MaterialTailwindThemeProvider";
 
 export function ThemeProvider({ value, children }) {
+  const mergedValue = merge(theme, value, { arrayMerge: combineMerge });
+
   return (
-    <MaterialTailwindTheme.Provider value={merge(theme, value)}>
-      {children}
-    </MaterialTailwindTheme.Provider>
+    <MaterialTailwindTheme.Provider value={mergedValue}>{children}</MaterialTailwindTheme.Provider>
   );
 }
+
+export const useTheme = () => useContext(MaterialTailwindTheme);
 
 ThemeProvider.defaultProps = {
   value: theme,
