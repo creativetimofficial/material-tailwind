05055e89e86cff1834cf23b5384cf2f75e1d23c4
sajadevo <nawabisajadahmad@gmail.com>
2022-05-03 00:55:08 +0430

    Add icon fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          ></link>
+          <script src="https://kit.fontawesome.com/42d5adcbca.js"></script>
         </Head>
-        <body>
+        <body className="bg-grey-100 antialiased">
           <Main />
           <NextScript />
         </body>
