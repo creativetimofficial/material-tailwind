5616e34d0b584fdbeab1dfadc0db518f0f7672fc
sajadevo <nawabisajadahmad@gmail.com>
2022-05-06 19:12:44 +0430

    Update chip component

--- packages/material-tailwind-react/src/components/Chip/index.tsx
+++ packages/material-tailwind-react/src/components/Chip/index.tsx
@@ -102,40 +102,44 @@ export const Chip = React.forwardRef<HTMLDivElement, ChipProps>(
     // 7. return
     return (
       <NewAnimatePresence>
-        <motion.div
-          {...rest}
-          ref={ref}
-          className={classes}
-          initial="unmount"
-          exit="unmount"
-          animate={show ? "mount" : "unmount"}
-          variants={appliedAnimation}
-        >
-          {icon && iconTemplate}
-          <div className={`${icon ? "ml-4" : ""} ${dismissible ? "mr-5" : ""} mt-px`}>{value}</div>
-          {dismissible && (
-            <div className={chipCloseButtonClasses}>
-              <div
-                role="button"
-                onClick={dismissible.onClose}
-                onMouseDown={(e) => !dismissible.action && rippleEffect.create(e, "light")}
-                className={`w-5 h-5 ${dismissible.action ? "" : "p-1"}`}
-              >
-                {dismissible.action || (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={3}
-                  >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                )}
-              </div>
+        {show && (
+          <motion.div
+            {...rest}
+            ref={ref}
+            className={classes}
+            initial="unmount"
+            exit="unmount"
+            animate={show ? "mount" : "unmount"}
+            variants={appliedAnimation}
+          >
+            {icon && iconTemplate}
+            <div className={`${icon ? "ml-4" : ""} ${dismissible ? "mr-5" : ""} mt-px`}>
+              {value}
             </div>
-          )}
-        </motion.div>
+            {dismissible && (
+              <div className={chipCloseButtonClasses}>
+                <div
+                  role="button"
+                  onClick={dismissible.onClose}
+                  onMouseDown={(e) => !dismissible.action && rippleEffect.create(e, "light")}
+                  className={`w-5 h-5 ${dismissible.action ? "" : "p-1"}`}
+                >
+                  {dismissible.action || (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={3}
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  )}
+                </div>
+              </div>
+            )}
+          </motion.div>
+        )}
       </NewAnimatePresence>
     );
   },
