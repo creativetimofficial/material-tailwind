c6263ff68c048a971996cccc96c4bb4e5fef4936
Stefan <stefan@creative-tim.com>
2022-05-12 18:06:22 +0300

    feat: add styling for breadcrumbs

---
+++ packages/material-tailwind-html/theme/components/breadcrumbs/index.js
@@ -0,0 +1,22 @@
+const breadcrumbs = (theme) => ({
+  ".breadcrumbs": {
+    display: "flex",
+    "flex-wrap": "wrap",
+    padding: "0.5rem 1rem",
+    "margin-bottom": "1rem",
+    "list-style": "none",
+    "background-color": theme("colors.light.500"),
+    "border-radius": "0.375rem",
+
+    ".breadcrumb-item": {
+      "font-size": ".875rem",
+      color: theme("colors.dark.500"),
+
+      "+ .breadcrumb-item": {
+        "padding-left": "0.5rem"
+      }
+    }
+  }
+});
+
+module.exports.breadcrumbs = breadcrumbs;
