e3c1e4d9e1f0c1a2c4f176fb15f5446266f4a8ca
Stefan <stefan@creative-tim.com>
2022-05-12 13:16:13 +0300

    refactor: update sidebar name

--- routes/html.routes.ts
+++ routes/html.routes.ts
@@ -2,7 +2,7 @@ export const routes = [
   {
     name: "guide",
     icon: "fas fa-book",
-    pages: ["quick-start", "what-is-tailwind-css", "license"]
+    pages: ["quick-start", "what-is-tailwind-CSS", "license"]
   },
   {
     name: "components",
