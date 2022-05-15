9fb270167861ce8b49f2d103355d07077720a697
sajadevo <nawabisajadahmad@gmail.com>
2022-05-15 21:07:11 +0430

    Landing Page: Add canonical and meta tags.

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,15 @@ export default function Presentation() {
   return (
     <>
       <Head>
-        <title>Presentation | Material Tailwind</title>
+        <title>
+          Material Tailwind - Easy-to-use Tailwind CSS components library with
+          Material Design
+        </title>
+        <link rel="canonical" href="https://www.material-tailwind.com" />
+        <meta
+          name="description"
+          content="Material Tailwind is a components library that features multiple React & HTML components, all written with Tailwind CSS classes and Material Design guidelines."
+        />
       </Head>
       <Navbar shadow />
       <main className="relative">
