c5a15433e21dca2f572ce12bc0d35494623ddcf6
sajadevo <nawabisajadahmad@gmail.com>
2022-06-24 16:53:49 +0430

    docs: update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,17 +23,12 @@ function MyApp({ Component, pageProps }) {
       </Script>
       <Script
         strategy="afterInteractive"
-        src="https://unpkg.com/material-ripple-effects/ripple.js"
-      />
-      <Script
-        strategy="afterInteractive"
         src="https://unpkg.com/@popperjs/core@2.9.1/dist/umd/popper.min.js"
       />
       <Script
         strategy="afterInteractive"
         src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"
       />
-      <Script strategy="afterInteractive" src="/material-tailwind-html.js" />
     </Fragment>
   );
 }
