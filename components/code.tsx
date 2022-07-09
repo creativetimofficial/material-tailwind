ba874d4cda73743c86884e8c0f1debfdf6434435
sajadevo <nawabisajadahmad@gmail.com>
2022-07-10 01:15:22 +0430

    docs: update code component

--- components/code.tsx
+++ components/code.tsx
@@ -1,18 +1,17 @@
 // prop-types
 interface Props {
   html: boolean;
-  small: boolean;
   children: any;
 }
 
-export default function Code({ html = false, small = false, children }: Props) {
+export default function Code({ html = false, children }: Props) {
   return (
     <code
       className={`rounded-md px-1 font-['Fira_Code'] font-normal ${
         html
           ? "bg-pink-500/[0.15] text-pink-500"
           : "bg-blue-500/[0.15] text-blue-500"
-      } ${small ? "text-sm" : "text-[15px]"}`}
+      } text-sm`}
     >
       {children}
     </code>
