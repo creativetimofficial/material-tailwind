446a391080b683df312cdddd94b02b46bbc03856
Stefan <stefan@creative-tim.com>
2022-07-18 23:36:42 +0300

    refactor: testing navtabs

--- pages/blocks.tsx
+++ pages/blocks.tsx
@@ -57,10 +57,71 @@ export default function Presentation() {
                 <i className="material-icons text-white">chevron_left</i>
               </button>
             </div>
-            <div className="w-1/4 my-10">
-              <div className="input-group input-group-outline">
-                <label className="form-label">Please select date</label>
-                <input className="form-control datepicker" type="text"/>
+            <div className="flex">
+              <div className="w-1/4 my-10">
+                <div className="input-group input-group-outline">
+                  <label className="form-label">Please select date</label>
+                  <input className="form-control datepicker" type="text"/>
+                </div>
+              </div>
+              <div className="w-1/4 my-10 ml-10">
+                <div className="input-group input-group-outline">
+                  <label className="form-label">Please select time</label>
+                  <input className="form-control timepicker" type="text"/>
+                </div>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="nav-tabs w-7/12">
+                <nav>
+                  <ul role="tablist" className="tabs p-1" tab-panel="">
+                    <li className="nav-item">
+                      <a className="nav-link active mb-0 px-0 py-1" aria-selected="true" aria-controls="html" role="tab">
+                        {`HTML`}
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link mb-0 px-0 py-1" aria-selected="false" aria-controls="react" role="tab">
+                        {`React`}
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link mb-0 px-0 py-1" aria-selected="false" aria-controls="vue" role="tab">
+                        {`Vue`}
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link mb-0 px-0 py-1" aria-selected="false" aria-controls="angular" role="tab">
+                        {`Angular`}
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link mb-0 px-0 py-1" aria-selected="false" aria-controls="svelte" role="tab">
+                        {`Svelte`}
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+                <div className="tabs-content p-5">
+                  <div className="active tab-panel" id="html" role="tabpanel">
+                    Because it's about motivating the doers. Because I'm here to follow my
+                    dreams and inspire people.
+                  </div>
+                  <div className="tab-panel" id="react" role="tabpanel">
+                    The reading of all good books is like a conversation with the finest
+                    minds of past centuries.
+                  </div>
+                  <div className="tab-panel" id="vue" role="tabpanel">
+                    Comparing yourself to others is the thief of joy.
+                  </div>
+                  <div className="tab-panel" id="angular" role="tabpanel">
+                    It's better to choose, commit, and get started instead of waiting for
+                    the best possible option.
+                  </div>
+                  <div className="tab-panel" id="svelte" role="tabpanel">
+                    If you think you can learn a lot from reading books, try writing one.
+                  </div>
+                </div>
               </div>
             </div>
           </div>
