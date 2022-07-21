9b73d35ca99ef714ff81ecb037e3eb81753185f9
Stefan <stefan@creative-tim.com>
2022-07-22 01:06:40 +0300

    refactor: include slider styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 import "/styles/globals.css";
 import "/styles/choices.css";
 import "/styles/flatpickr.css";
+import "/styles/nouislider.css";
 
 function MyApp({ Component, pageProps }) {
   return (
