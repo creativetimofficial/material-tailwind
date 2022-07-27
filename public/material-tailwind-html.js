2a977c47b439ab9a30fdced11aa2b7bbca536f4b
Stefan <stefan@creative-tim.com>
2022-07-27 04:27:57 +0300

    refactor: add form validation script

--- public/material-tailwind-html.js
+++ public/material-tailwind-html.js
@@ -2,6 +2,78 @@ import flatpickr from "flatpickr";
 import noUiSlider from 'nouislider';
 
 export default function init() {
+
+  "use strict";
+  (function () {
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirm_password = document.getElementById('confirm_password');
+    
+    function showError(input, message) {
+      const formControl = input.parentElement;
+      formControl.className = 'input-group input-group-outline mb-3 error is-filled';
+      const small = formControl.querySelector('small');
+      small.innerText = message;
+    }
+    
+    function showSucces(input) {
+      const formControl = input.parentElement;
+      formControl.className = 'input-group input-group-outline mb-3 success is-filled';
+    }
+    
+    function checkEmail(input) {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(re.test(input.value.trim())) {
+        showSucces(input)
+      } else {
+        showError(input,'Email is not invalid');
+      }
+    }
+    
+    function checkRequired(inputArr) {
+      inputArr.forEach(function(input){
+        if(input.value.trim() === '') {
+          showError(input,`${getFieldName(input)} is required`)
+        } else {
+          showSucces(input);
+        }
+      });
+    }
+    
+    function checkLength(input, min ,max) {
+      if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+      } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be les than ${max} characters`);
+      } else {
+        showSucces(input);
+      }
+    }
+    
+    function getFieldName(input) {
+      return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    }
+    
+    function checkPasswordMatch(input1, input2) {
+      if(input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+      }
+    }
+    
+    form.addEventListener('submit',function(e) {
+      e.preventDefault();
+  
+      checkRequired([username, email, password, confirm_password]);
+      checkLength(username,3,15);
+      checkLength(password,6,25);
+      checkEmail(email);
+      checkPasswordMatch(password, confirm_password);
+    });
+    
+  })();
+
   "use strict";
   (function () {
 
