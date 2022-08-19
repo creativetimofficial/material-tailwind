03d1e00b6b9668e9676dd2777f5e6c3f5ff7e7bc
Stefan <stefan@creative-tim.com>
2022-08-19 09:58:37 +0300

    feat: add new section - team 3

---
+++ components/pro/team/team-3.tsx
@@ -0,0 +1,158 @@
+export default function SectionTeam3() {
+  return (
+    <div className="bg-white">
+      <div className="container mx-auto px-8 py-20">
+        <div className="flex">
+          <div className="w-5/12">
+            <div className="text-center lg:text-left pt-5">
+              <p className="text-base leading-relaxed text-pink-500 mb-2 font-semibold">Our team</p>
+              <h1 className="tracking-normal font-sans text-5xl font-semibold text-blue-gray-900 mb-8 leading-tight lg:text-6xl">Leadership team</h1>
+              <p className="text-xl font-normal leading-relaxed text-gray-700 lg:pr-20">We're a cross-disciplinary team that loves to create great experiences for our customers.</p>
+              <div className="mt-12 flex justify-center gap-3 lg:justify-start">
+                <button className="button button-outlined button-pink button-lg" type="button">About us</button>
+                <button className="button button-pink button-lg" type="button">Open positions</button>
+              </div>
+            </div>
+          </div>
+          <div className="w-7/12">
+            <div className="grid grid-cols-1 items-start gap-8 md:grid-cols-2 lg:grid-cols-3">
+              <div className="card card-plain">
+                <div className="card-body">
+                  <a href="#">
+                    <img className="w-1/2 rounded-full" src="https://images.unsplash.com/photo-1519699047748-de8e457a634e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="card image"/>
+                  </a>
+                  <h5 className="tracking-normal font-sans text-xl font-semibold leading-snug mt-4 mb-0">Olivia Rhye</h5>
+                  <p className="text-xl font-normal leading-relaxed text-pink-500">Founder & CEO</p>
+                  <p>Former co-founder of Opendoor. Early staff at Spotify and Clearbit.</p>
+                  <div className="flex mt-3">
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-twitter text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-linkedin text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-dribbble text-xl" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card card-plain">
+                <div className="card-body">
+                  <a href="#">
+                    <img className="w-1/2 rounded-full" src="https://images.unsplash.com/photo-1618077360395-f3068be8e001?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="card image"/>
+                  </a>
+                  <h5 className="tracking-normal font-sans text-xl font-semibold leading-snug mt-4 mb-0">Pheonix Baker</h5>
+                  <p className="text-xl font-normal leading-relaxed text-pink-500">Engineering Manager</p>
+                  <p>Lead engineering teams at Figma, Pitch, and Protocol Labs.</p>
+                  <div className="flex mt-3">
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-twitter text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-linkedin text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-dribbble text-xl" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card card-plain">
+                <div className="card-body">
+                  <a href="#">
+                    <img className="w-1/2 rounded-full" src="https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="card image"/>
+                  </a>
+                  <h5 className="tracking-normal font-sans text-xl font-semibold leading-snug mt-4 mb-0">Lana Steiner</h5>
+                  <p className="text-xl font-normal leading-relaxed text-pink-500">Product Manager</p>
+                  <p>Former PM for Linear, Lambda School, and On Deck.</p>
+                  <div className="flex mt-3">
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-twitter text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-linkedin text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-dribbble text-xl" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card card-plain">
+                <div className="card-body">
+                  <a href="#">
+                    <img className="w-1/2 rounded-full" src="https://images.unsplash.com/photo-1577744168855-0391d2ed2b3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="card image"/>
+                  </a>
+                  <h5 className="tracking-normal font-sans text-xl font-semibold leading-snug mt-4 mb-0">Demi Wilkinson</h5>
+                  <p className="text-xl font-normal leading-relaxed text-pink-500">Frontend Developer</p>
+                  <p>Former frontend dev for Linear, Coinbase, and Postscript.</p>
+                  <div className="flex mt-3">
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-twitter text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-linkedin text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-dribbble text-xl" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card card-plain">
+                <div className="card-body">
+                  <a href="#">
+                    <img className="w-1/2 rounded-full" src="https://images.unsplash.com/photo-1447194047554-cfe888edc98c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="card image"/>
+                  </a>
+                  <h5 className="tracking-normal font-sans text-xl font-semibold leading-snug mt-4 mb-0">Candice Wu</h5>
+                  <p className="text-xl font-normal leading-relaxed text-pink-500">Backend Developer</p>
+                  <p>Lead backend end at Clarbit. Former Clarbit and Loom.</p>
+                  <div className="flex mt-3">
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-twitter text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-linkedin text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-dribbble text-xl" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="card card-plain">
+                <div className="card-body">
+                  <a href="#">
+                    <img className="w-1/2 rounded-full" src="https://images.unsplash.com/photo-1552162864-987ac51d1177?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3217&q=80" alt="card image"/>
+                  </a>
+                  <h5 className="tracking-normal font-sans text-xl font-semibold leading-snug mt-4 mb-0">Natali Craig</h5>
+                  <p className="text-xl font-normal leading-relaxed text-pink-500">Product Designer</p>
+                  <p>Founding design team at Figma. Former Pleo, Stripe, and Tile.</p>
+                  <div className="flex mt-3">
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-twitter text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-linkedin text-xl" aria-hidden="true"></i>
+                    </button>
+                    <button className="button button-link button-text button-secondary">
+                      <i className="fab fa-dribbble text-xl" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+
+        
+      </div>
+    </div>
+  )
+}
