4188dd3b29241bdc46a03cb39127cbbc5c66732b
Stefan <stefan@creative-tim.com>
2022-08-24 15:22:19 +0300

    refactor: responsive fix

--- components/pro/footers/footer-1.tsx
+++ components/pro/footers/footer-1.tsx
@@ -1,11 +1,11 @@
 export default function SectionFooter1() {
   return (
     <div className="my-20">
-      <div className="container mx-auto">
-        <div className="flex items-center">
-          <div className="w-full md:w-8/12">
+      <div className="container mx-auto p-8 md:p-0">
+        <div className="lg:flex items-center">
+          <div className="w-full lg:w-8/12">
             <h5 className="font-bold text-xl mb-6">Material Tailwind</h5>
-            <ul className="flex gap-8 list-unstyled">
+            <ul className="block md:flex gap-8 list-unstyled">
               <li>
                 <a
                   href="#"
@@ -68,23 +68,21 @@ export default function SectionFooter1() {
               </li>
             </ul>
           </div>
-          
-
-          <div className="w-full md:mb-2 md:w-4/12 ">
+          <div className="w-full md:mb-2 lg:w-4/12 mt-10 lg:mt-0">
             <p className="text-blue-gray-900 font-semibold mb-4">Stay up to date</p>
 
             <div className="flex">
-              <div className="input-group input-group-outline w-full lg:w-8/12 mr-4">
+              <div className="input-group input-group-outline w-1/2 lg:w-8/12 mr-4">
                 <label className="form-label">Enter your email</label>
                 <input type="text" className="form-control" />
               </div>
-              <button className="button button-pink w-full lg:w-4/12" type="button">
+              <button className="button button-pink lg:w-4/12" type="button">
                 Subscribe
               </button>
             </div>
           </div>
         </div>
-        <hr className="horizontal dark my-12" />
+        <hr className="horizontal dark my-10 md:my-12" />
         <div className="flex flex-wrap items-center justify-center gap-2 md:justify-between">
           <p className="py-1 font-normal text-grey-500">
             Copyright © {/* */}2077{/* */} Material Tailwind by{/* */}{" "}
@@ -97,8 +95,8 @@ export default function SectionFooter1() {
               Creative Tim.
             </a>
           </p>
-          <div className="w-full px-4 text-center md:w-4/12 md:px-0 md:text-right">
-            <ul className="flex gap-8 list-unstyled justify-end">
+          <div className="w-full md:px-4 text-center md:w-4/12 md:px-0 md:text-right">
+            <ul className="flex gap-8 list-unstyled md:justify-end">
               <li>
                 <a
                   href="#"
