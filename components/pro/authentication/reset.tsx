0f9da2a884ce8748f5e38f2caec70d76193c7134
Stefan <stefan@creative-tim.com>
2022-08-25 17:21:05 +0300

    Update reset.tsx

--- components/pro/authentication/reset.tsx
+++ components/pro/authentication/reset.tsx
@@ -1,11 +1,11 @@
 export default function SectionReset() {
   return (
-    <section className="block">
-      <div className="pb-56 pt-12 m-4 min-h-50-screen items-start rounded-xl p-0 relative overflow-hidden flex bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1497996541515-6549b145cd90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80')]">
+    <section className="block my-12">
+      <div className="py-56 m-4 min-h-50-screen items-start rounded-xl p-0 relative overflow-hidden flex bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1497996541515-6549b145cd90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80')]">
         <span className="absolute top-0 left-0 w-full h-full bg-center bg-cover opacity-60 bg-gradient-to-tl from-grey-900 to-slate-800" />
       </div>
-      <div className="container mx-auto -mt-12">
-          <div className="card w-1/3 mx-auto">
+      <div className="container mx-auto -mt-24">
+          <div className="card relative z-10 w-1/3 mx-auto">
             <div className="card-header mx-4 -mt-6">
               <div className="shadow-pink pe-1 rounded-lg bg-pink-500 py-5">
                 <h4 className="mt-2 mb-0 text-center font-bold text-white">Reset Password</h4>
@@ -22,7 +22,6 @@ export default function SectionReset() {
 
             </div>
           </div>
-
         </div>
     </section>
   )
