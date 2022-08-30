4d455a20c4f75f15c476aa7882112ef223cf57d8
Stefan <stefan@creative-tim.com>
2022-08-30 12:25:47 +0300

    refactor: add script for the pricing 1

--- public/material-tailwind-html.js
+++ public/material-tailwind-html.js
@@ -5,6 +5,39 @@ import noUiSlider from 'nouislider';
 export default function init() {
 
   "use strict";
+  (function() {
+    if(document.querySelector(".tabs")){
+      var nav_pills = document.querySelector(".tabs");
+    
+      var plans = document.querySelectorAll("[" + nav_pills.getAttribute("aria-controls") + "]");
+    
+      var links = nav_pills.querySelectorAll("li a.nav-link");
+    
+      links.forEach((link) => {
+        link.addEventListener("click", function () {
+    
+          var selected_link = plans[0].querySelector("[" + link.getAttribute("aria-controls") + "]");
+    
+          if (!selected_link.hasAttribute("data-active")) {
+            let active_link = nav_pills.querySelector("li a.nav-link.active");
+    
+            plans.forEach(plan => {              
+              let active_plan = plan.querySelector("[" + active_link.getAttribute("aria-controls") + "]");
+              let selected_plan = plan.querySelector("[" + link.getAttribute("aria-controls") + "]");
+
+              active_plan.classList.add("hidden");
+              selected_plan.classList.remove("hidden");
+              
+              active_plan.removeAttribute("data-active");
+              selected_plan.setAttribute("data-active", "true");
+            });
+          }
+        });
+      });
+      
+    }
+  })();
+  "use strict";
   (function () {
     const form = document.getElementById('form');
     const username = document.getElementById('username');
