e1d28bea42d8c1018b163a9db0529129e69179ad
Stefan <stefan@creative-tim.com>
2022-09-06 23:14:58 +0300

    refactor: update features 3 texts and colors

--- components/pro/features/features-3.tsx
+++ components/pro/features/features-3.tsx
@@ -4,9 +4,12 @@ export default function SectionFeatures3() {
       <div className="container mx-auto">
         <div className="mb-20 grid lg:grid-cols-2">
           <div className="lg:pr-10">
-            <div className="h-full min-h-[20rem] w-full rounded-3xl bg-gradient-to-tr from-pink-500 to-pink-700" />
+            <img 
+                src="https://images.unsplash.com/photo-1601933513793-1252ce25d644?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+                className="w-full rounded-lg shadow-lg"
+              />
           </div>
-          <div className="grid items-center py-12 px-2 lg:ml-8 lg:py-20">
+          <div className="grid items-center py-12 px-2 lg:ml-8 lg:py-16">
             <div className="mb-6 grid h-12 w-12 place-items-center rounded-full bg-pink-100/80 p-2.5 text-pink-500 ring-8 ring-pink-100/40">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -22,10 +25,10 @@ export default function SectionFeatures3() {
                 />
               </svg>
             </div>
-            <h3 className="tracking-normal font-sans text-3xl font-semibold leading-snug text-blue-gray-900 mb-2">
+            <h3 className="tracking-normal font-sans text-3xl font-semibold leading-snug text-blue-grey-900 mb-2">
               See our projects
             </h3>
-            <p className="text-xl leading-relaxed mb-10 text-gray-700">
+            <p className="text-xl leading-relaxed mb-10 text-blue-grey-700">
               People are so scared to lose that they don't even try. Like, one thing people can't say is that I'm not trying, and I'm not trying my hardest.
             </p>
             <div className="flex gap-2">
@@ -54,12 +57,12 @@ export default function SectionFeatures3() {
                 </svg>
                 demo
               </button>
-              <button className="button button-pink button-lg" type="button">read more</button>
+              <button className="button button-pink button-lg" type="button" data-ripple-light="true">read more</button>
             </div>
           </div>
         </div>
         <div className="mb-20 grid lg:grid-cols-2">
-          <div className="grid items-center py-12 px-2 lg:mr-16 lg:py-20">
+          <div className="grid items-center py-12 px-2 lg:mr-16 lg:py-16">
             <div className="mb-6 grid h-12 w-12 place-items-center rounded-full bg-pink-100/80 p-2.5 text-pink-500 ring-8 ring-pink-100/40">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -75,10 +78,10 @@ export default function SectionFeatures3() {
                 />
               </svg>
             </div>
-            <h3 className="tracking-normal font-sans text-3xl font-semibold leading-snug text-blue-gray-900 mb-2">
+            <h3 className="tracking-normal font-sans text-3xl font-semibold leading-snug text-blue-grey-900 mb-2">
               Read More About Us
             </h3>
-            <p className="text-xl leading-relaxed mb-10 text-gray-700">
+            <p className="text-xl leading-relaxed mb-10 text-blue-grey-700">
               Pain is what we go through as we become older. We get insulted by others, lose trust for those others. We get back stabbed by friends. It becomes harder for us to give others a hand.
             </p>
             <div className="flex gap-2">
@@ -104,11 +107,14 @@ export default function SectionFeatures3() {
                 </svg>
                 demo
               </button>
-              <button className="button button-pink button-lg" type="button">read more</button>
+              <button className="button button-pink button-lg" type="button" data-ripple-light="true">read more</button>
             </div>
           </div>
           <div className="row-start-1 lg:row-auto lg:pl-10">
-            <div className="h-full min-h-[20rem] w-full rounded-3xl bg-gradient-to-tr from-pink-500 to-pink-700" />
+            <img 
+              src="https://images.unsplash.com/photo-1534120247760-c44c3e4a62f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+              className="w-full rounded-lg shadow-lg"
+            />
           </div>
         </div>
       </div>
