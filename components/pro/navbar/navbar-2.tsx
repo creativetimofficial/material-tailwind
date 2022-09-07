4edb7904d5f40fa5644d020df343b31b25437278
Stefan <stefan@creative-tim.com>
2022-09-07 22:02:07 +0300

    refactor: update navbar 2

--- components/pro/navbar/navbar-2.tsx
+++ components/pro/navbar/navbar-2.tsx
@@ -1,71 +1,60 @@
 export default function SectionNavbar2() {
   return (
-    <nav className="block py-4 shadow-md w-full max-w-full rounded-none px-4 bg-blue-500 text-white">
-      <div className="container mx-auto flex items-center justify-between">
-        <a href="#" className="antialiased text-sm leading-normal text-white mr-4 cursor-pointer py-1.5 font-bold">Material Tailwind</a>
-        <div className="hidden lg:block">
-          <ul className="mb-4 mt-2 flex flex-col gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
-            <a href="#">
-              <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z"></path><path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z"></path></svg>
-                Pages
-              </li>
-            </a>
-            <a href="#">
-              <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
-                Account
-              </li>
-            </a>
-            <a href="#">
-              <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path></svg>
-                Blocks
-              </li>
-            </a>
-            <a href="#">
-              <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd"></path></svg>
-                Docs
-              </li>
-            </a>
-          </ul>
+    <nav className="navbar w-full bg-blue-500 rounded-none my-5 py-4">
+      <div className="container mx-auto lg:flex">
+        <div className="flex items-center justify-between">
+          <a className="nav-brand mr-auto ml-4 !text-white" href="#">Material Tailwind</a>
+          <button
+            data-trigger="true"
+            className="navbar-trigger ml-auto mr-0 mb-0 block lg:hidden"
+            type="button"
+            aria-controls="navigation"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-trigger-icon">
+              <span bar1="" className="navbar-trigger-bar mt-1 !bg-white">
+                <span className="hidden origin-[10%_10%] rotate-45 !bg-white"></span>
+              </span>
+              <span bar2="" className="bar2 navbar-trigger-bar mt-2 !bg-white"></span>
+              <span bar3="" className="bar3 navbar-trigger-bar mt-2 !bg-white">
+                <span className="mt-[0.4375rem] hidden origin-[10%_90%] -rotate-45 !bg-white"></span>
+              </span>
+            </span>
+          </button>
         </div>
-        <button className="button button-light text-dark-500 button-sm hidden lg:inline-block" type="button">Buy Now</button>
-        <button className="relative middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 ml-auto inline-block lg:hidden" type="button">
-          <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </span>
-        </button>
-      </div>
-      <div className="block w-full basis-full overflow-hidden h-0 opacity-0">
-        <ul className="mb-4 mt-2 flex flex-col gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
-          <a href="#">
-            <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z"></path><path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z"></path></svg>
-              Pages
-            </li>
-          </a>
-          <a href="#">
-            <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
-              Account
-            </li>
-          </a>
-          <a href="#">
-            <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path></svg>
-              Blocks
-            </li>
-          </a>
-          <a href="#">
-            <li className="antialiased text-sm leading-normal text-white flex items-center gap-1.5 p-1 font-normal">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd"></path></svg>
-              Docs
-            </li>
-          </a>
-        </ul>
-        <button className="middle none font-sans font-bold center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg bg-white text-blue-gray-900 shadow-md shadow-blue-gray-500/10 hover:shadow-lg hover:shadow-blue-gray-500/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full mb-2" type="button">Buy Now</button>
+
+          <div className="collapse navbar-collapse transition-all duration-500 overflow-hidden !basis-auto">
+            <ul className="navbar-nav mx-auto">
+              <li>
+                <a className="nav-link !text-white" aria-current="page" href="#">
+                  <i className="material-icons mr-2 text-base opacity-60">article</i>
+                  <span>Documantation</span>
+                </a>
+              </li>
+              <li>
+                <a className="nav-link !text-white" href="#">
+                  <i className="material-icons mr-2 text-base opacity-60">apps</i>
+                  <span>Components</span>
+                </a>
+              </li>
+              <li className="flex">
+                <a className="nav-link !text-white" href="#">
+                  <i className="material-icons mr-2 text-base opacity-60"
+                    >view_carousel</i
+                  >
+                  <span>Templates</span>
+                </a>
+              </li>
+              <li>
+                <a className="nav-link !text-white" href="#">
+                  <i className="fab fa-github mr-2 text-base opacity-60"></i>
+                  <span>Github</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className="button button-light text-blue-grey-900 button-sm hidden lg:inline-block" type="button">Buy Now</button>
       </div>
     </nav>
   )
