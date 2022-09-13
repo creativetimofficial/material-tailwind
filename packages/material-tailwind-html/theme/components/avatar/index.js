7a69258128a4f9dc2461eafc13b799e9af97cdaa
Stefan <stefan@creative-tim.com>
2022-09-13 18:30:06 +0300

    refactor: update avatar styling

--- packages/material-tailwind-html/theme/components/avatar/index.js
+++ packages/material-tailwind-html/theme/components/avatar/index.js
@@ -1,59 +1,56 @@
 const avatar = (theme) => ({
   ".avatar": {
     color: theme("colors.white"),
-    display: "inline-flex",
-    "align-items": "center",
-    "justify-content": "center",
-    "font-size": "1rem",
-    "border-radius": "50rem",
-    height: "2.5rem",
-    width: "2.5rem",
+    // display: "inline-flex",
+    height: theme("height.10"),
+    width: theme("width.10"),
     transition: "all .2s ease-in-out",
   
     "img": {
-      height: "100%",
-      width: "100%",
-      "border-radius": ".75rem",
+      height: theme("height.full"),
+      width: theme("width.full"),
+      "border-radius": theme("borderRadius.xl"),
     },
   
     "&.avatar-circular img": {
-      "border-radius": "50%"
+      "border-radius": theme("borderRadius.full")
     },
   
     "&.avatar-xs": {
-      height: "1.25rem",
-      width: "1.25rem",
+      height: theme("height.5"),
+      width: theme("width.5"),
     },
     "&.avatar-sm": {
-      height: "1.75rem",
-      width: "1.75rem",
+      height: theme("height.7"),
+      width: theme("width.7"),
     },
     "&.avatar-md": {
-      height: "2.5rem",
-      width: "2.5rem",
+      height: theme("height.10"),
+      width: theme("width.10"),
     },
     "&.avatar-lg": {
-      height: "3rem",
-      width: "3rem",
+      height: theme("height.12"),
+      width: theme("width.12"),
     },
     "&.avatar-xl": {
-      height: "4rem",
-      width: "4rem",
+      height: theme("height.16"),
+      width: theme("width.16"),
     }, 
     "&.avatar-xxl": {
-      height: "5rem",
-      width: "5rem",
+      height: theme("height.20"),
+      width: theme("width.20"),
     }
   },
 
   ".avatar-group": {
     ".avatar": {
       position: "relative",
-      "z-index": "2",
-      border: "2px solid " + theme("colors.white"),
+      "z-index": theme("zIndex.20"),
+      border: theme("spacing.1") + " solid " + theme("colors.white"),
+      "border-radius": theme("borderRadius.full"),
 
       "&:hover": {
-        "z-index": "3"
+        "z-index": theme("zIndex.30")
       },
 
       "+ .avatar": {
