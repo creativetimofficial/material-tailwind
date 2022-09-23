becb048c67546689cacf7122907815dfb5cbc23d
Stefan <stefan@creative-tim.com>
2022-09-23 23:15:54 +0300

    refactor: update form control

--- packages/material-tailwind-html/theme/components/input/index.js
+++ packages/material-tailwind-html/theme/components/input/index.js
@@ -1,17 +1,17 @@
 const input = (theme) => ({
   ".form-control": {
     display: "block",
-    width: "100%",
-    padding: "0.5rem 0",
-    "font-size": ".875rem",
-    "font-weight": "400",
-    "line-height": "1.15rem",
+    width: theme("width.full"),
+    padding: "theme('spacing.2') 0",
+    fontSize: theme("fontSize.sm"),
+    fontWeight: theme("fontWeight.normal"),
+    lineHeight: "1.15rem",
     color: "#495057",
-    "background-color": "transparent",
-    "background-clip": "padding-box",
+    backgroundColor: theme("colors.transparent"),
+    backgroundClip: "padding-box",
     border: `1px solid theme("colors.blue-grey.100")`,
     appearance: "none",
-    "border-radius": "0.375rem",
+    borderRadius: theme("borderRadius.md"),
     transition: ".2s ease",
   
     "&:focus-visible": {
@@ -22,76 +22,76 @@ const input = (theme) => ({
     position: "absolute"
   },
   ".form-label,label": {
-    "font-size": ".875rem",
-    "font-weight": "400",
-    "margin-bottom": "0.5rem",
+    fontSize: theme("fontSize.sm"),
+    fontWeight: theme("fontWeight.normal"),
+    marginBottom: theme("spacing.2"),
     color: theme("colors.blue-grey.500"),
-    "margin-left": "0",
-    "transition": "all .2s ease"
+    marginLeft: theme("spacing.0"),
+    transition: "all .2s ease"
   },
   ".input-group": {
     position: "relative",
     display: "flex",
-    "flex-wrap": "wrap",
-    "align-items": "stretch",
-    width: "100%",
+    flexWrap: "wrap",
+    alignItems: "stretch",
+    width: theme("width.full"),
     transition: ".2s ease",
     border: "none",
 
     "&.input-group-outline": {
       ".form-label": {
         display: "flex",
-        "line-height": "3.925",
+        lineHeight: "3.925",
         top: "-0.375rem",
-        "margin-bottom": "0",
+        marginBottom: theme("spacing.0"),
 
         "&:before,&:after": {
           content: '""',
-          "border-top": "1px solid",
-          "border-top-color": theme("colors.blue-grey.100"),
-          "pointer-events": "none",
-          "margin-top": "0.375rem",
-          "box-sizing": "border-box",
+          borderTop: "1px solid",
+          borderTopColor: theme("colors.blue-grey.100"),
+          pointerEvents: "none",
+          marginTop: theme("spacing.[1.5]"),
+          boxSizing: "border-box",
           display: "block",
-          height: "0.5rem",
-          width: "0.625rem",
-          "border-width": "1px 0 0",
-          "border-color": "transparent"
+          height: theme("spacing.2"),
+          width: theme("spacing.[2.5]"),
+          borderWidth: "1px 0 0",
+          borderColor: theme("colors.transparent")
         },
 
         "&:before": {
-          "margin-right": "4px",
-          "border-left": "1px solid transparent",
-          "border-radius": "6px 0"
+          marginRight: "4px",
+          borderLeft: "1px solid transparent",
+          borderRadius: "6px 0"
         },
 
         "&:after": {
-          "flex-grow": "1",
-          "margin-left": "4px",
-          "border-right": "1px solid transparent",
-          "border-radius": "0 6px"
+          flexGrow: "1",
+          marginLeft: "4px",
+          borderRight: "1px solid transparent",
+          borderRadius: "0 6px"
         }
       },
 
       ".form-control": {
-        "background": "none",
+        background: "none",
         border: `1px solid theme("colors.blue-grey.100")`,
-        "border-radius": "0.375rem",
-        "border-top-left-radius": "0.375rem",
-        "border-bottom-left-radius": "0.375rem",
-        padding: "0.625rem 0.75rem",
-        "line-height": "1.25rem",
-        "z-index": "20"
+        borderRadius: theme("borderRadius.md"),
+        borderTopLeftRadius: theme("borderRadius.md"),
+        borderBottomLeftRadius: theme("borderRadius.md"),
+        padding: "theme('spacing.[2.5]') theme('spacing.3')",
+        lineHeight: theme("spacing.5"),
+        zIndex: theme("zIndex.20")
       }
     },
 
     "&.input-group-dynamic,&.input-group-static,": {
       ".form-control": {
-        "border-radius": "0",
+        borderRadius: "0",
         border: "0",
-        "background": "no-repeat bottom,50% calc(100% - 1px)",
-        "background-image": `linear-gradient(0deg,theme("colors.pink.500") 2px,rgba(156,39,176,0) 0),linear-gradient(0deg,theme("colors.blue-grey.100") 1px,hsla(0,0%,82%,0) 0)`,
-        "background-size": "0 100%,100% 100%",
+        background: "no-repeat bottom,50% calc(100% - 1px)",
+        backgroundImage: `linear-gradient(0deg,theme("colors.pink.500") 2px,rgba(156,39,176,0) 0),linear-gradient(0deg,theme("colors.blue-grey.100") 1px,hsla(0,0%,82%,0) 0)`,
+        backgroundSize: "0 100%,100% 100%",
         transition: ".2s ease",
       },
       ".form-label": {
@@ -101,23 +101,23 @@ const input = (theme) => ({
 
     "&.input-group-static": {
       "label": {
-        "margin-bottom": "0"
+        marginBottom: "0"
       },
     },
 
     "&.focused,&.is-filled": {
       ".form-label": {
-        width: "100%",
-        height: "100%",
-        "font-size": ".6875rem",
+        width: theme("width.full"),
+        height: theme("height.full"),
+        fontSize: ".6875rem",
         color: theme("colors.pink.500"),
         display: "flex",
-        "line-height": "1.25",
+        lineHeight: "1.25",
 
         "&:before": {
-          opacity: "1",
-          "border-top-color": theme("colors.pink.500"),
-          "box-shadow": `inset 0 1px theme("colors.pink.500")`
+          opacity: theme("opacity.100"),
+          borderTopColor: theme("colors.pink.500"),
+          boxShadow: `inset 0 1px theme("colors.pink.500")`
         },
 
         "&:after": {
