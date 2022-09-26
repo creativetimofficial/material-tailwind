fcc6158852a50dc67673f93002560fd2c81f1686
Stefan <stefan@creative-tim.com>
2022-09-26 17:59:49 +0300

    refactor: update radio component - theme()

--- packages/material-tailwind-html/theme/components/radio/index.js
+++ packages/material-tailwind-html/theme/components/radio/index.js
@@ -4,7 +4,7 @@ const radio = (theme) => ({
     height: "1.23rem"
   },
   ".radio + .form-check-input": {
-    "border-radius": "50%"
+    borderRadius: "50%"
   },
   ".radio + .form-check-input:after": {
     transition: "opacity .25s ease-in-out",
@@ -12,20 +12,20 @@ const radio = (theme) => ({
     position: "absolute",
     width: ".8rem",
     height: ".8rem",
-    "border-radius": "50%",
-    "background-image": `linear-gradient(195deg, theme("colors.pink.400"), theme("colors.pink.600"))`,
-    opacity: "0",
-    left: "0px",
-    right: "0",
-    top: "0px",
-    bottom: "0",
+    borderRadius: "50%",
+    backgroundImage: `linear-gradient(195deg, theme("colors.pink.400"), theme("colors.pink.600"))`,
+    opacity: theme('opacity.0'),
+    left: theme('spacing.0'),
+    right: theme('spacing.0'),
+    top: theme('spacing.0'),
+    bottom: theme('spacing.0'),
     margin: "auto"
   },
   ".radio:checked + .form-check-input": {
-    "border-color": theme("colors.pink.500")
+    borderColor: theme("colors.pink.500")
   },
   ".radio:checked + .form-check-input:after": {
-    opacity: "1"
+    opacity: theme('opacity.100')
   }
 });
 
