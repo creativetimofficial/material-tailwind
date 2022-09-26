2def8cbbdb1eeb970184b9f1cf7cbd381bf37e43
Stefan <stefan@creative-tim.com>
2022-09-26 17:59:56 +0300

    refactor: update spinners component - theme()

--- packages/material-tailwind-html/theme/components/spinners/index.js
+++ packages/material-tailwind-html/theme/components/spinners/index.js
@@ -1,23 +1,23 @@
 const spinners = (theme) => ({
   ".spinner-border": {
     display: "inline-block",
-    width: "2rem",
-    height: "2rem",
-    "vertical-align": "-0.125em",
+    width: theme("width.8"),
+    height: theme("height.8"),
+    verticalAlign: "-" + theme("spacing.[0.5]]"),
     border: "0.25em solid currentColor",
-    "border-right-color": "transparent",
-    "border-radius": "50%",
+    borderRightColor: theme("colors.white"),
+    borderRadius: "50%",
     animation: ".75s linear infinite b",
 
     ".sr-only": {
       border: "0",
       clip: "rect(0,0,0,0)",
-      height: "1px",
-      margin: "-1px",
+      height: theme("height.px"),
+      margin: "-" + theme("spacing.px"),
       overflow: "hidden",
-      padding: "0",
+      padding: theme("spacing.0"),
       position: "absolute",
-      width: "1px"
+      width: theme("width.px")
     }
   },
   "@keyframes b": {
