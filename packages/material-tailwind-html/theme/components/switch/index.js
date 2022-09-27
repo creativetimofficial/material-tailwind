450688d4a4ba04a437dc9748b0721d5321928d33
Stefan <stefan@creative-tim.com>
2022-09-27 16:59:47 +0300

    rerfactor: update switch component

--- packages/material-tailwind-html/theme/components/switch/index.js
+++ packages/material-tailwind-html/theme/components/switch/index.js
@@ -4,10 +4,10 @@ const toggle = (theme) => ({
       position: "relative",
       height: ".9375rem",
       width: "1.875rem",
-      "background-color": theme("colors.blue-grey.100"),
-      "background-image": "none",
-      "background-position": "0",
-      "border-radius": "1.875rem",
+      backgroundColor: theme("colors.blue-grey.100"),
+      backgroundImage: "none",
+      backgroundPosition: "0",
+      borderRadius: "1.875rem",
       transition: "background-color .25s ease,border-color .25s ease,background-position .15s ease-in-out,opacity .15s ease-out,box-shadow .15s ease-in-out",
 
       "&:after": {
@@ -15,26 +15,26 @@ const toggle = (theme) => ({
         top: "-3px",
         left: "-5px",
         content: '""',
-        width: "1.25rem",
-        height: "1.25rem",
+        width: theme("width.5"),
+        height: theme("height.5"),
         border: `1px solid theme("colors.blue-grey.100")`,
-        "border-radius": "50%",
-        "background-color": theme("colors.white"),
+        borderRadius: "50%",
+        backgroundColor: theme("colors.white"),
         transform: "translateX(1px)",
-        transition: "transform .25s ease-in-out,background-color .25s ease-in-out",
-        "box-shadow": "0 4px 6px -1px rgb(0 0 0 / 10%), 0 2px 4px -1px rgb(0 0 0 / 6%)",
+        transition: "transform .25s ease-in-out, background-color .25s ease-in-out",
+        boxShadow: "0 4px 6px -1px rgb(0 0 0 / 10%), 0 2px 4px -1px rgb(0 0 0 / 6%)",
        
       },
 
       "&[checked]": {
-        "border-color": theme("colors.grey.800"),
-        "background-color": theme("colors.grey.800"),
-        "background-position": "100%",
-        "background-image": "none",
+        borderColor: theme("colors.grey.800"),
+        backgroundColor: theme("colors.grey.800"),
+        backgroundPosition: "100%",
+        backgroundImage: "none",
 
         "&:after": {
           transform: "translateX(21px)",
-          "border-color": theme("colors.grey.800")
+          borderColor: theme("colors.grey.800")
         }
       }
     }
