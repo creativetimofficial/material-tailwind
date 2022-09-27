5de216f46a2915c0b13edcabb72048039f21441c
sajadevo <nawabisajadahmad@gmail.com>
2022-09-27 13:43:42 +0430

    update rewriters

--- next.config.js
+++ next.config.js
@@ -16,14 +16,13 @@ const nextConfig = {
     ];
   },
   async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/blocks/:path*",
-          destination: "https://material-taillwind-pro-bxhm.vercel.app/:path*",
-        },
-      ],
-    };
+    return [
+      {
+        source: "/blocks/:path*",
+        destination: "https://material-taillwind-pro-bxhm.vercel.app/:path*",
+        basePath: false,
+      },
+    ];
   },
 };
 
