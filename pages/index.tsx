1f57aad92bacaa5e8f81ec4f25f864e921517e67
sajadevo <nawabisajadahmad@gmail.com>
2022-11-22 15:53:43 +0430

    remove black friday banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@ import React from "react";
 
 // next.js components
 import Head from "next/head";
-import Link from "next/link";
 
 // @material-tailwind/react components
-import { Card, Alert, Button } from "@material-tailwind/react";
+import { Card } from "@material-tailwind/react";
 
 // page components
 import Navbar from "components/layout/navbar";
@@ -24,8 +23,6 @@ import SectionTestimonials from "components/sections/testimonials";
 import SectionCommunity from "components/sections/community";
 
 export default function Presentation() {
-  const [show, setShow] = React.useState(true);
-
   return (
     <>
       <Head>
@@ -43,30 +40,6 @@ export default function Presentation() {
           content="tailwind css, material design, react, next, react.js, next.js, tailwind template, tailwind css theme, tailwindcss components"
         />
       </Head>
-      {show && (
-        <div className="fixed top-28 left-0 z-[99] w-screen bg-gradient-to-r from-red-400 to-red-600 pt-2">
-          <div className="container mx-auto">
-            <Alert
-              show={show}
-              dismissible={{
-                onClose: () => setShow(false),
-              }}
-              className="bg-trasparent rounded-none"
-              icon={<div className="-mt-1 text-2xl">😍</div>}
-            >
-              <div className="-mt-2 flex items-center justify-between">
-                Black Friday Exclusive - Get access to our Premium Components
-                and save up to 50% OFF
-                <Link href="/blocks#pricing">
-                  <Button className="flex bg-blue-gray-900 shadow-blue-gray-900/10 hover:shadow-blue-gray-900/25">
-                    get offer 🔥
-                  </Button>
-                </Link>
-              </div>
-            </Alert>
-          </div>
-        </div>
-      )}
       <Navbar shadow />
       <main className="relative">
         <Header />
