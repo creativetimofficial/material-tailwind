8a7adc2b4f04c9e93f778c81430e6c15090e3a44
sajadevo <nawabisajadahmad@gmail.com>
2022-12-19 19:02:41 +0430

    add test file

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@material-tailwind/react";
+
+const AddressPage = () => {
+  return (
+    <Typography
+      as="div"
+      variant="medium whitespace-pre-line"
+      className="normal-case"
+    >
+      <address>The Address Page</address>
+    </Typography>
+  );
+};
+
+export default AddressPage;
