a626eefff4bb1460cb310cd7d409854bfea2a674
sajadevo <nawabisajadahmad@gmail.com>
2023-01-03 11:17:45 +0430

    docs: update to next 13

--- components/cards/framework.tsx
+++ components/cards/framework.tsx
@@ -13,15 +13,14 @@ interface Props {
 const Framework = forwardRef(
   ({ children, route, center = false }: Props, ref: any) => {
     return (
-      <Link href={route}>
-        <a
-          ref={ref}
-          className={`grid w-full min-w-[7rem] transform cursor-pointer ${
-            center ? "place-items-center" : ""
-          } border-blue-gray-50 hover:border-blue-gray-100 hover:bg-blue-gray-50 rounded-xl border bg-white px-3 py-2 transition-all hover:scale-105 hover:bg-opacity-25`}
-        >
-          {children}
-        </a>
+      <Link
+        ref={ref}
+        href={route}
+        className={`grid w-full min-w-[7rem] transform cursor-pointer ${
+          center ? "place-items-center" : ""
+        } rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25`}
+      >
+        {children}
       </Link>
     );
   }
