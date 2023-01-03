bed2f97df41b5345cc692f91d1cba84a22e30236
sajadevo <nawabisajadahmad@gmail.com>
2023-01-03 11:13:21 +0430

    react: fix the height and overflow issue for AccordionBody

--- packages/material-tailwind-react/src/components/Accordion/AccordionBody.tsx
+++ packages/material-tailwind-react/src/components/Accordion/AccordionBody.tsx
@@ -45,7 +45,7 @@ export const AccordionBody = React.forwardRef<HTMLDivElement, AccordionBodyProps
         transition: { duration: 0.2, times: [0.4, 0, 0.2, 1] },
       },
       mount: {
-        height: "100%",
+        height: "auto",
         transition: { duration: 0.2, times: [0.4, 0, 0.2, 1] },
       },
     };
