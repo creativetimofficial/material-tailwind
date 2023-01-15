0cdbd4cecdc36cb32de36567d6886f1cd7ad6c78
Stefan <stefan@creative-tim.com>
2023-01-15 18:42:49 +0200

    refactor: update avatar component styling

--- packages/material-tailwind-html/theme/components/avatar/index.js
+++ packages/material-tailwind-html/theme/components/avatar/index.js
@@ -1,44 +1,44 @@
 const avatar = (theme) => ({
+
   ".avatar": {
     color: theme("colors.white"),
     display: "inline-flex",
-    height: theme("height.10"),
-    width: theme("width.10"),
+    height: theme("height.12"),
+    width: theme("width.12"),
     transition: "all .2s ease-in-out",
-  
-    "img": {
-      height: theme("height.full"),
-      width: theme("width.full"),
-      borderRadius: theme("borderRadius.xl"),
-    },
-  
-    "&.avatar-circular img": {
+    borderRadius: theme("borderRadius.lg"),
+
+    "&.avatar-circular": {
       borderRadius: theme("borderRadius.full")
     },
   
     "&.avatar-xs": {
-      height: theme("height.5"),
-      width: theme("width.5"),
+      height: theme("height.6"),
+      width: theme("width.6"),
+      borderRadius: theme("borderRadius.md"),
     },
     "&.avatar-sm": {
-      height: theme("height.7"),
-      width: theme("width.7"),
+      height: theme("height.9"),
+      width: theme("width.9"),
+      borderRadius: theme("borderRadius.md"),
     },
     "&.avatar-md": {
-      height: theme("height.10"),
-      width: theme("width.10"),
-    },
-    "&.avatar-lg": {
       height: theme("height.12"),
       width: theme("width.12"),
     },
-    "&.avatar-xl": {
+    "&.avatar-lg": {
       height: theme("height.16"),
       width: theme("width.16"),
-    }, 
-    "&.avatar-xxl": {
+    },
+    "&.avatar-xl": {
       height: theme("height.20"),
       width: theme("width.20"),
+      borderRadius: theme("borderRadius.xl"),
+    }, 
+    "&.avatar-xxl": {
+      height: theme("height.24"),
+      width: theme("width.24"),
+      borderRadius: theme("borderRadius.2xl"),
     }
   },
 
