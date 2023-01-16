2756f61ce60ff298204f4e9492d1cede5ae6d381
sajadevo <nawabisajadahmad@gmail.com>
2023-01-16 14:36:45 +0430

    update code preview

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   productionBrowserSourceMaps: true,
   async redirects() {
     return [
