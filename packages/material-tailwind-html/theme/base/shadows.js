e915f9e45ef5a53b14f1bcea6ed7a7edff8ab0b5
Stefan <stefan@creative-tim.com>
2023-01-20 17:20:03 +0200

    refactor: remove custom shadows

--- packages/material-tailwind-html/theme/base/shadows.js
+++ packages/material-tailwind-html/theme/base/shadows.js
@@ -6,9 +6,7 @@ const shadows = {
   xl: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
   "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.25)",
   inner: "inset 0 2px 4px 0 rgb(0 0 0 / 0.05)",
-  none: "0 0 rgb(0, 0 / 0, 0)",
-  card: "0 4px 6px -1px rgb(0 0 0 / 10%), 0 2px 4px -1px rgb(0 0 0 / 6%)",
-  dialog: "0 27px 24px 0 rgb(0 0 0 / 20%), 0 40px 77px 0 rgb(0 0 0 / 22%)"
+  none: "0 0 rgb(0, 0 / 0, 0)"
 };
 
 module.exports = shadows;
