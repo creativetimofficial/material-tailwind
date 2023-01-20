2acc731c97fc1d5a2c39ed834ab33a66440b6b3d
Stefan <stefan@creative-tim.com>
2023-01-20 13:02:10 +0200

    Update material-tailwind-html.js

--- public/material-tailwind-html.js
+++ public/material-tailwind-html.js
@@ -473,113 +473,108 @@ export default function init() {
   })();
   ("use strict");
   (function () {
+    var total = document.querySelectorAll("[data-tabs]");
+
     var getEventTarget = function getEventTarget(e) {
       e = e || window.event;
       return e.target || e.srcElement;
     };
-    var total = document.querySelectorAll(".tabs");
+    
     total.forEach(function (item, i) {
       var moving_div = document.createElement("div");
-      var first_li = item.querySelector("li:first-child .nav-link");
+      var first_li = item.querySelector("li:first-child [data-tab-target]");
       var tab = first_li.cloneNode();
       tab.innerHTML = "-";
-      tab.classList.add("bg-white");
-      tab.classList.add("text-white");
-      tab.style.animation = ".5s ease";
-      moving_div.classList.add("moving-tab", "absolute");
+      tab.classList.remove("bg-inherit", "text-slate-700");
+      tab.classList.add("bg-white", "text-white");
+      tab.style.animation = ".2s ease";
+  
+      moving_div.classList.add("z-10", "absolute", "text-slate-700", "rounded-lg", "bg-inherit", "flex-auto", "text-center", "bg-none", "border-0", "block", "shadow");
+      moving_div.setAttribute("moving-tab", "");
+      moving_div.setAttribute("data-tab-target", "");
       moving_div.appendChild(tab);
       item.appendChild(moving_div);
+  
       var list_length = item.getElementsByTagName("li").length;
-      moving_div.style.boxShadow = "0 1px 5px 1px #ddd";
+  
       moving_div.style.padding = "0px";
-      moving_div.style.width =
-        item.querySelector("li:nth-child(1)").offsetWidth + "px";
+      moving_div.style.width = item.querySelector("li:nth-child(1)").offsetWidth + "px";
       moving_div.style.transform = "translate3d(0px, 0px, 0px)";
       moving_div.style.transition = ".5s ease";
+  
       item.onmouseover = function (event) {
-        var target = getEventTarget(event);
-        var li = target.closest("li");
+        let target = getEventTarget(event);
+        let li = target.closest("li");
         if (li) {
-          var nodes = Array.from(li.closest("ul").children);
-          var index = nodes.indexOf(li) + 1;
-          item.querySelector("li:nth-child(" + index + ") .nav-link").onclick =
-            function () {
-              item.querySelectorAll("li").forEach(function (list_item) {
-                list_item.firstElementChild.classList.remove("active");
-                list_item.firstElementChild.setAttribute(
-                  "aria-selected",
-                  "false"
-                );
-              });
-              li.firstElementChild.classList.add("active");
-              li.firstElementChild.setAttribute("aria-selected", "true");
-              moving_div = item.querySelector(".moving-tab");
-              var sum = 0;
-              if (item.classList.contains("flex-col")) {
-                for (var j = 1; j <= nodes.indexOf(li); j++) {
-                  sum += item.querySelector(
-                    "li:nth-child(" + j + ")"
-                  ).offsetHeight;
-                }
-                moving_div.style.transform =
-                  "translate3d(0px," + sum + "px, 0px)";
-                moving_div.style.height = item.querySelector(
-                  "li:nth-child(" + j + ")"
-                ).offsetHeight;
-              } else {
-                for (var j = 1; j <= nodes.indexOf(li); j++) {
-                  sum += item.querySelector(
-                    "li:nth-child(" + j + ")"
-                  ).offsetWidth;
-                }
-                moving_div.style.transform =
-                  "translate3d(" + sum + "px, 0px, 0px)";
-                moving_div.style.width =
-                  item.querySelector("li:nth-child(" + index + ")")
-                    .offsetWidth + "px";
+          let nodes = Array.from(li.closest("ul").children);
+          let index = nodes.indexOf(li) + 1;
+          item.querySelector("li:nth-child(" + index + ") [data-tab-target]").onclick = function () {
+            item.querySelectorAll("li").forEach(function (list_item) {
+              list_item.firstElementChild.removeAttribute("active");
+              list_item.firstElementChild.setAttribute("aria-selected", "false");
+            });
+            li.firstElementChild.setAttribute("active", "");
+            li.firstElementChild.setAttribute("aria-selected", "true");
+            moving_div = item.querySelector("[moving-tab]");
+            let sum = 0;
+            if (item.classList.contains("flex-col")) {
+              for (var j = 1; j <= nodes.indexOf(li); j++) {
+                sum += item.querySelector("li:nth-child(" + j + ")").offsetHeight;
               }
-            };
+              moving_div.style.transform = "translate3d(0px," + sum + "px, 0px)";
+              moving_div.style.height = item.querySelector("li:nth-child(" + j + ")").offsetHeight;
+            } else {
+              for (var j = 1; j <= nodes.indexOf(li); j++) {
+                sum += item.querySelector("li:nth-child(" + j + ")").offsetWidth;
+              }
+              moving_div.style.transform = "translate3d(" + sum + "px, 0px, 0px)";
+              moving_div.style.width = item.querySelector("li:nth-child(" + index + ")").offsetWidth + "px";
+            }
+          };
         }
       };
     });
+
     window.addEventListener("resize", function (event) {
       total.forEach(function (item, i) {
-        item.querySelector(".moving-tab").remove();
+        item.querySelector("[moving-tab]").remove();
         var moving_div = document.createElement("div");
-        var tab = item.querySelector(".nav-link.active").cloneNode();
+        var tab = item.querySelector("[data-tab-target][active]").cloneNode();
         tab.innerHTML = "-";
         tab.classList.remove("bg-inherit");
-        tab.style.animation = ".5s ease";
-        moving_div.classList.add("moving-tab", "absolute");
+        tab.classList.add("bg-white", "text-white");
+        tab.style.animation = ".2s ease";
+  
+        moving_div.classList.add("z-10", "absolute", "text-slate-700", "rounded-lg", "bg-inherit", "flex-auto", "text-center", "bg-none", "border-0", "block", "shadow");
+        moving_div.setAttribute("moving-tab", "");
+        moving_div.setAttribute("data-tab-target", "");
         moving_div.appendChild(tab);
+  
         item.appendChild(moving_div);
-        moving_div.style.boxShadow = "0 1px 5px 1px #ddd";
+  
         moving_div.style.padding = "0px";
         moving_div.style.transition = ".5s ease";
-        var li = item.querySelector(".nav-link.active").parentElement;
+  
+        let li = item.querySelector("[data-tab-target][active]").parentElement;
+  
         if (li) {
-          var nodes = Array.from(li.closest("ul").children);
-          var index = nodes.indexOf(li) + 1;
-          var sum = 0;
+          let nodes = Array.from(li.closest("ul").children);
+          let index = nodes.indexOf(li) + 1;
+  
+          let sum = 0;
           if (item.classList.contains("flex-col")) {
             for (var j = 1; j <= nodes.indexOf(li); j++) {
               sum += item.querySelector("li:nth-child(" + j + ")").offsetHeight;
             }
             moving_div.style.transform = "translate3d(0px," + sum + "px, 0px)";
-            moving_div.style.width =
-              item.querySelector("li:nth-child(" + index + ")").offsetWidth +
-              "px";
-            moving_div.style.height = item.querySelector(
-              "li:nth-child(" + j + ")"
-            ).offsetHeight;
+            moving_div.style.width = item.querySelector("li:nth-child(" + index + ")").offsetWidth + "px";
+            moving_div.style.height = item.querySelector("li:nth-child(" + j + ")").offsetHeight;
           } else {
             for (var j = 1; j <= nodes.indexOf(li); j++) {
               sum += item.querySelector("li:nth-child(" + j + ")").offsetWidth;
             }
             moving_div.style.transform = "translate3d(" + sum + "px, 0px, 0px)";
-            moving_div.style.width =
-              item.querySelector("li:nth-child(" + index + ")").offsetWidth +
-              "px";
+            moving_div.style.width = item.querySelector("li:nth-child(" + index + ")").offsetWidth + "px";
           }
         }
       });
@@ -597,30 +592,33 @@ export default function init() {
         });
       }
     });
-    var total = document.querySelectorAll("[tab-panel]");
+    var total = document.querySelectorAll("[data-tab-content]");
     if (total[0]) {
       total.forEach(function (nav_pills) {
-        var links = nav_pills.querySelectorAll("li a.nav-link");
+        var links = nav_pills.querySelectorAll("li a[data-tab-target]");
         links.forEach(function (link) {
           link.addEventListener("click", function () {
             var clicked_tab = document.querySelector(
               "#" + link.getAttribute("aria-controls")
             );
-            if (!clicked_tab.classList.contains("active")) {
+            if (!clicked_tab.classList.contains("block","opacity-100")) {
               var active_link = clicked_tab
-                .closest(".nav-tabs")
-                .querySelector("li a.nav-link.active");
+                .closest("[data-tab-content]")
+                .parentElement
+                .querySelector("li a[data-tab-target][active]");
               var active_panel = document.querySelector(
                 "#" + active_link.getAttribute("aria-controls")
               );
-              active_panel.classList.remove("active");
-              clicked_tab.classList.add("active");
+              active_panel.classList.remove("block", "opacity-100");
+              clicked_tab.classList.add("block","opacity-100");
             }
           });
         });
       });
     }
   })();
+
+  
   ("use strict");
   (function () {
     var toggles = document.querySelectorAll('[data-attribute="toggle"]');
