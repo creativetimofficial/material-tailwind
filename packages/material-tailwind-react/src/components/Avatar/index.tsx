12ed90f341f8e9da4f92de723f1e7efa1c90ce20
sajadevo <nawabisajadahmad@gmail.com>
2023-02-22 16:41:40 +0430

    react: add new options and variant for avatar

--- packages/material-tailwind-react/src/components/Avatar/index.tsx
+++ packages/material-tailwind-react/src/components/Avatar/index.tsx
@@ -11,32 +11,46 @@ import objectsToString from "../../utils/objectsToString";
 import { useTheme } from "../../context/theme";
 
 // types
-import type { variant, size, className } from "../../types/components/avatar";
-import { propTypesVariant, propTypesSize, propTypesClassName } from "../../types/components/avatar";
+import type { variant, size, className, withBorder, color } from "../../types/components/avatar";
+import {
+  propTypesVariant,
+  propTypesSize,
+  propTypesClassName,
+  propTypesColor,
+  propTypesWithBorder,
+} from "../../types/components/avatar";
 
 export interface AvatarProps extends React.ComponentProps<"img"> {
   variant?: variant;
   size?: size;
   className?: className;
+  withBorder?: withBorder;
+  color?: color;
 }
 
 export const Avatar = React.forwardRef<HTMLImageElement, AvatarProps>(
-  ({ variant, size, className, ...rest }, ref) => {
+  ({ variant, size, className, color, withBorder, ...rest }, ref) => {
     // 1. init
     const { avatar } = useTheme();
     const { valid, defaultProps, styles } = avatar;
-    const { base, variants, sizes } = styles;
+    const { base, variants, sizes, borderColor } = styles;
 
     // 2. set default props
     variant = variant ?? defaultProps.variant;
     size = size ?? defaultProps.size;
     className = className ?? defaultProps.className;
+    withBorder = withBorder ?? defaultProps.withBorder;
+    color = color ?? defaultProps.color;
 
     // 3. set styles
     const avatarVariant = objectsToString(variants[findMatch(valid.variants, variant, "rounded")]);
     const avatarSize = objectsToString(sizes[findMatch(valid.sizes, size, "md")]);
+    const avatarBorderColor = objectsToString(borderColor[findMatch(valid.colors, color, "blue")]);
     const classes = twMerge(
-      classnames(objectsToString(base), avatarVariant, avatarSize),
+      classnames(objectsToString(base.initial), avatarVariant, avatarSize, {
+        [objectsToString(base.withBorder)]: withBorder,
+        [avatarBorderColor]: withBorder,
+      }),
       className,
     );
 
@@ -49,6 +63,8 @@ Avatar.propTypes = {
   variant: PropTypes.oneOf(propTypesVariant),
   size: PropTypes.oneOf(propTypesSize),
   className: propTypesClassName,
+  withBorder: propTypesWithBorder,
+  color: propTypesColor,
 };
 
 Avatar.displayName = "MaterialTailwind.Avatar";
