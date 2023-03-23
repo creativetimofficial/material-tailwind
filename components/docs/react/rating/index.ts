8396ab77401f5aa9f49a9ef58a4aca4f08a8e6e3
sajadevo <nawabisajadahmad@gmail.com>
2023-03-23 14:50:57 +0430

    docs-react: update rating examples

--- components/docs/react/rating/index.ts
+++ components/docs/react/rating/index.ts
@@ -3,5 +3,4 @@ export * from "./rating-with-text";
 export * from "./rating-colors";
 export * from "./default-rating";
 export * from "./custom-rating-icon";
-export * from "./rating-with-card";
 export * from "./readonly-rating";
