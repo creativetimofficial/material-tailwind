fec1aee544d17abb031b5183a57386b67fe78a6d
dragosct10 <dragos@creative-tim.com>
2023-06-30 18:22:52 +0300

    refactor: header

--- components/layout/figma/header.tsx
+++ components/layout/figma/header.tsx
@@ -2,110 +2,44 @@ import { useState, useEffect } from "react";
 
 // next.js components
 import Image from "next/image";
-import Link from "next/link";
-
-// react-copy-to-clipboard components
-import { CopyToClipboard } from "react-copy-to-clipboard";
 
 // @material-tailwind/react components
 import {
-  Tabs,
-  TabsHeader,
-  Tab,
   Typography,
   Button,
-  Tooltip,
+  Chip,
 } from "@material-tailwind/react";
 
 export default function Header() {
-  const [version, setVersion] = useState("npm i @material-tailwind/react");
-  const [copied, setCopied] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setCopied(false);
-    }, 3000);
-  }, [copied]);
-
   return (
     <div className="h-screen min-h-screen bg-[url('/img/header-figma.jpg')] bg-cover bg-center bg-no-repeat">
-      <div className="relative z-50 h-fit py-20 lg:py-32">
+      <div className="relative z-50 h-fit pt-32">
         <div className="container mx-auto px-4">
-          <div className="flex flex-wrap items-center justify-between">
-            <div className="mt-48 w-full mx-auto px-4 md:w-8/12 lg:mt-4 lg:w-5/12">
-              <div className="w-full max-w-full sm:my-auto md:w-5/6 md:flex-none lg:w-1/2">
-                <Tabs value="react" className="mb-6">
-                  <TabsHeader
-                    className="h-10 w-auto rounded-full border border-white/80 bg-white/80 shadow-2xl shadow-blue-gray-500/40 backdrop-blur-2xl backdrop-saturate-200"
-                    indicatorProps={{
-                      className: "rounded-full",
-                    }}
-                  >
-                    <Tab
-                      value="react"
-                      className="p-0 font-normal text-[#1A237E]"
-                      onClick={() =>
-                        setVersion("npm i @material-tailwind/react")
-                      }
-                    >
-                      <i className="fab fa-react" />
-                      &nbsp;React
-                    </Tab>
-                    <Tab
-                      value="html"
-                      className="p-0 font-normal text-[#1A237E]"
-                      onClick={() =>
-                        setVersion("npm i @material-tailwind/html")
-                      }
-                    >
-                      <i className="fab fa-html5" />
-                      &nbsp;HTML
-                    </Tab>
-                  </TabsHeader>
-                </Tabs>
+          <div className="flex flex-wrap items-center justify-between relative">
+            <Image className="absolute left-24" src="/img/logos/arrow-1.png" width="60" height="60" alt="Arrow-1 Logo" />
+            <div className="mt-46 mx-auto px-4 lg:mt-12 w-8/12 text-center">
+              <div className="border border-blue-gray-100 bg-white text-blue-gray-900 p-3 py-2.5 pr-5 rounded-lg inline-flex items-center font-semibold">
+                <Chip value="New" className="mr-4 rounded-md font-medium bg-slate-900" />
+                Figma Version 1.0.0
               </div>
               <Typography
                 variant="h1"
-                className="mb-2 font-black tracking-normal text-slate-950"
+                className="mb-5 mt-10 relative text-center text-6xl font-black leading-tight tracking-normal text-blue-gray-900"
               >
-                Material Tailwind Figma
+                Figma Design System <br /> for Material Tailwind Pro
+                <Image className="absolute top-8 right-10" src="/img/logos/arrow-3.png" width="60" height="60" alt="Arrow-3 Logo" />
               </Typography>
-              <Typography className="mb-6 text-lg font-light text-[#1A237E] lg:pr-12">
-                Material Tailwind is an easy to use components library for
-                Tailwind CSS and Material Design.
+              <Typography className="mb-10 relative text-xl text-center font-medium leading-normal text-blue-gray-500 lg:pr-12">
+                Interested in working with the designer files? No worries, we&apos;ve prepared a special Figma version of the product! Interested in working with the designer files? No worries, we&apos;ve prepared a special Figma version of the product!
+                <Image className="absolute top-24 left-24" src="/img/logos/arrow-2.png" width="60" height="60" alt="Arrow-2 Logo" />
               </Typography>
-              <div className="flex flex-col-reverse gap-2 lg:flex-row">
-                <Link
-                  href={
-                    version === "npm i @material-tailwind/react"
-                      ? "/docs/react/installation"
-                      : "/docs/html/installation"
-                  }
-                >
-                  <Button variant="gradient" className="h-full w-full">
-                    Get Started
-                  </Button>
-                </Link>
-                <div className="flex rounded-lg border border-white/80 bg-white/80 py-2.5 px-5 text-[#1A237E] shadow-2xl shadow-blue-gray-500/20 backdrop-blur-2xl backdrop-saturate-200">
-                  <CopyToClipboard
-                    text={version}
-                    onCopy={() => setCopied(true)}
-                  >
-                    <p className="mb-0 flex w-full items-center justify-between font-normal">
-                      {version}
-                      <Tooltip
-                        content={copied ? "Copied" : "Copy"}
-                        interactive={false}
-                      >
-                        <i
-                          className={`${
-                            copied ? "fas" : "far"
-                          } fa-copy ml-4 cursor-pointer text-sm`}
-                        />
-                      </Tooltip>
-                    </p>
-                  </CopyToClipboard>
-                </div>
+
+              <div>
+                <Button size="lg" variant="outlined" className="capitalize bg-white relative font-medium text-md gap-3 inline-flex items-center text-blue-gray-800 border-blue-gray-100 focus:ring-transparent">
+                  <Image src="/img/icon-figma.svg" width="30" height="30" alt="Figma Logo" className="absolute" />
+                  <span className="pl-8">Preview in Figma</span>
+                </Button>
+                <Button size="lg" variant="filled" className="ml-4 text-md font-medium capitalize bg-blue-gray-900 hover:shadow-blue-gray-500/40 shadow-blue-gray-500/20">Pricing & FAQ</Button>
               </div>
             </div>
           </div>
