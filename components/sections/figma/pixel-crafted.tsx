7c8fe0b4a606bf0f604831e8cabd4485e5a55c4d
dragosct10 <dragos@creative-tim.com>
2023-07-06 23:46:14 +0300

    feat: pixel responsive

--- components/sections/figma/pixel-crafted.tsx
+++ components/sections/figma/pixel-crafted.tsx
@@ -30,23 +30,23 @@ const cardContent = [
 export default function PixelCrafted() {
   return (
     <section className="pb-12 pt-20">
-      <div className="mx-auto mb-10 text-center w-8/12">
+      <div className="mx-auto mb-10 text-center lg:w-10/12 xl:w-8/12">
         <Typography
           variant="h1"
-          className="mb-5 mt-5 relative text-center text-5xl font-semibold leading-tight tracking-normal text-blue-gray-900"
+          className="mb-5 mt-5 relative text-center text-4xl md:text-5xl font-semibold leading-tight tracking-normal text-blue-gray-900"
         >
           Perfect Pixel crafted
         </Typography>
         <Typography
           variant="paragraph"
-          className="mb-5 mt-5 relative text-center text-xl font-normal leading-relaxed tracking-normal text-blue-gray-400"
+          className="mb-5 mt-5 relative text-center text-lg md:text-xl font-normal leading-relaxed tracking-normal text-blue-gray-400"
         >
           Material Tailwind Pro features all the React or HTML components examples you will need to build amazing web projects, all written with Tailwind CSS classes and Material Design guidelines. 
         </Typography>
       </div>
-      <div className="flex items-center mt-16">
+      <div className="flex flex-col lg:flex-row items-center mt-16">
         {cardContent.map((card, i) => (
-          <div className="relative flex-auto px-4" key={i}>
+          <div className="relative flex-auto mt-8 lg:mt-0 px-4" key={i}>
             <ContentCards 
               minHeight="min-h-[480px]" 
               title={card.title} 
