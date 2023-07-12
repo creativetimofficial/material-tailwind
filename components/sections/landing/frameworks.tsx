b25cc7a1ec9dab40a1778046a342a0771dc2b3c2
dragosct10 <dragos@creative-tim.com>
2023-07-12 21:08:46 +0300

    Update frameworks.tsx

--- components/sections/landing/frameworks.tsx
+++ components/sections/landing/frameworks.tsx
@@ -2,47 +2,56 @@ import { Card, CardBody, CardHeader, Typography } from '@material-tailwind/react
 
 type CardsProps = {
   children: React.ReactNode;
+  img: React.ReactNode;
 }
 
 const content = [
   {
     title: 'React',
+    img: '/img/react.svg',
     description: 'Open-source JavaScript framework and library developed by Facebook.',
   },
   {
     title: 'Html',
+    img: '/img/html.svg',
     description: 'The standard markup language for creating Web pages.',
   },
   {
     title: 'Next.js',
+    img: '/img/nextjs.svg',
     description: 'React framework that gives you building blocks to create web apps.',
   },
   {
     title: 'Create React App',
+    img: '/img/react-app.svg',
     description: 'Set up a modern web app by running one command.',
   },
   {
     title: 'Remix',
+    img: '/img/remix.svg',
     description: 'Full stack web framework that lets you focus on the user interface.',
   },
   {
     title: 'Vite',
+    img: '/img/vitejs.svg',
     description: 'Flexible programmatic APIs with full TypeScript typing.',
   },
   {
     title: 'Astro',
+    img: '/img/astro.svg',
     description: 'All-in-one web framework designed for speed.',
   },
   {
     title: 'Gatsby',
-    description: 'Combines functionality from React, GraphQL& Webpack into a single tool.',
+    img: '/img/gatsby.svg',
+    description: 'The Fastest Frontend for the Headless Web.',
   },
 ]
 
-const Cards: React.FC<CardsProps> = ({ children }) => {
+const Cards: React.FC<CardsProps> = ({ children, img }) => {
   return (
     <>
-      <Card color="white" className="mt-4 rounded-xl overflow-hidden">
+      <Card color="white" className="mt-4 lg:mt-8 rounded-xl overflow-hidden">
         <div className="bg-[url('/img/header-figma.jpg')] bg-cover bg-center bg-no-repeat h-full w-full absolute top-0"></div>
         <CardHeader
           color="transparent"
@@ -50,9 +59,9 @@ const Cards: React.FC<CardsProps> = ({ children }) => {
           shadow={false}
           className="flex items-center"
         >
-          <img src="/img/react.png" alt="react" className="w-10 h-10" />
+          {img}
         </CardHeader>
-        <CardBody className="relative z-10">
+        <CardBody className="relative z-10 pl-4 pr-2">
           {children}
         </CardBody>
       </Card>
@@ -62,7 +71,7 @@ const Cards: React.FC<CardsProps> = ({ children }) => {
 
 const Frameworks = () => {
   return (
-    <section className="pb-12">
+    <section>
       <div className="mb-10 w-12/12 -mt-20">
         <Typography
           variant="h1"
@@ -79,8 +88,8 @@ const Frameworks = () => {
 
         <div className="flex flex-wrap">
           {content.map((item, i) => (
-            <div className="lg:w-3/12 px-4 mt-4 lg:mt-0" key={i}>
-              <Cards>
+            <div className="w-full lg:w-3/12 lg:px-4 mt-4 lg:mt-0" key={i}>
+              <Cards img={<img src={item.img} className="w-8 h-8" />}>
                 <Typography className="font-semibold text-gray-900 text-xl">
                   {item.title}
                 </Typography>
