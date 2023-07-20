f685eccb22e6d49f35b0dfb8038ce78d677fad9b
sajadevo <nawabisajadahmad@gmail.com>
2023-07-20 23:32:06 +0430

    docs-react: update the table docs

--- pages/docs/[...slug].tsx
+++ pages/docs/[...slug].tsx
@@ -65,6 +65,7 @@ import * as NavbarExamples from "docs-content/react/navbar";
 import * as FooterExamples from "docs-content/react/footer";
 import * as ImgExamples from "docs-content/react/img";
 import * as VideoExamples from "docs-content/react/video";
+import * as TableExamples from "docs-content/react/table";
 
 import * as RatingExamples from "components/docs/react/rating";
 import * as SliderExamples from "components/docs/react/slider";
@@ -74,7 +75,6 @@ import * as PaginationExamples from "components/docs/react/pagination";
 import * as SpeedDialExamples from "components/docs/react/speed-dial";
 import * as StepperExamples from "components/docs/react/stepper";
 import * as SidebarExamples from "components/docs/react/sidebar";
-import * as TableExamples from "components/docs/react/table";
 
 import TransparentTabs from "components/docs/transparent-tabs";
 import DocsTitle from "components/docs-title";
