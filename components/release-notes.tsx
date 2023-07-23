07d1899397fc99e07660982fd72ec1a42a3fd044
Sayed Murtaza <mortazasadat37@gmail.com>
2023-07-23 17:11:58 +0430

    release-notes page

--- components/release-notes.tsx
+++ components/release-notes.tsx
@@ -6,25 +6,38 @@ import {
   TimelineIcon,
   TimelineBody,
   Typography,
+  Chip,
 } from "@material-tailwind/react";
 import { HomeIcon, BellIcon, CurrencyDollarIcon } from "@heroicons/react/24/solid";
 
 export default function ReleaseNotes() {
   return (
-    <div className="w-full  md:w-[40rem]">
+    <div className="ml-28">
       <Timeline>
         <TimelineItem>
+          <div className="absolute -left-28 -top-8 flex flex-col items-end">
+            <Chip
+              variant="outlined"
+              color="blue-gray"
+              value="v2.1.1"
+              className="mb-2 w-max rounded-full border-blue-gray-50 px-2 py-1 lowercase text-blue-gray-900"
+            />
+            <Typography variant="small" color="blue-gray" className="font-bold">
+              May 28, 2023
+            </Typography>
+          </div>
+
           <TimelineConnector />
 
           <TimelineHeader>
-            <TimelineIcon className=" border-2 border-blue-gray-100 !bg-gray-50  p-2 ">
-              <HomeIcon className="h-4 w-4 text-blue-gray-200" />
+            <TimelineIcon className=" border-2 border-blue-gray-100 !bg-gray-50  p-1.5 ">
+              <HomeIcon className="h-3.5 w-3.5 text-blue-gray-200" />
             </TimelineIcon>
             <Typography variant="h5" color="blue-gray-300">
               New Components & Section
             </Typography>
           </TimelineHeader>
-          <TimelineBody className="pb-8">
+          <TimelineBody className="w-auto pb-8">
             <Typography className="mb-4 font-medium text-blue-gray-500">
               Material Tailwind v2.1.1 - this release feature{" "}
               <span className="font-bold text-blue-gray-600 ">
@@ -81,18 +94,31 @@ export default function ReleaseNotes() {
           </TimelineBody>
         </TimelineItem>
         <TimelineItem>
+          <div className="absolute -left-28 -top-8 flex flex-col items-end">
+            <Chip
+              variant="outlined"
+              color="blue-gray"
+              value="v2.1.0"
+              className="mb-2 w-max rounded-full border-blue-gray-50 px-2 py-1 lowercase text-blue-gray-900"
+            />
+            <Typography variant="small" color="blue-gray" className="font-bold">
+              May 28, 2023
+            </Typography>
+          </div>
+
           <TimelineConnector />
+
           <TimelineHeader>
-            <TimelineIcon className=" border-2 border-blue-gray-100 !bg-gray-50  p-2 ">
-              <BellIcon className="h-4 w-4 text-blue-gray-200" />
+            <TimelineIcon className=" border-2 border-blue-gray-100 !bg-gray-50  p-1.5 ">
+              <HomeIcon className="h-3.5 w-3.5 text-blue-gray-200" />
             </TimelineIcon>
             <Typography variant="h5" color="blue-gray-300">
               New Components & Section
             </Typography>
           </TimelineHeader>
-          <TimelineBody className="pb-8">
+          <TimelineBody className="w-auto pb-8">
             <Typography className="mb-4 font-medium text-blue-gray-500">
-              Material Tailwind v2.1.1 - this release feature{" "}
+              Material Tailwind v2.1.0 - this release feature{" "}
               <span className="font-bold text-blue-gray-600 ">
                 5 new components and 5 sections.
               </span>
@@ -125,23 +151,23 @@ export default function ReleaseNotes() {
             <div>
               <div className="mb-1 flex items-center">
                 <div className="mr-2 h-2 w-2 rounded-full bg-blue-gray-100"></div>
-                <Typography className="font-semibold text-blue-gray-400">Blog</Typography>
+                <Typography className="font-medium text-blue-gray-400">Blog</Typography>
               </div>
               <div className="mb-1 flex items-center">
                 <div className="mr-2 h-2 w-2 rounded-full bg-blue-gray-100"></div>
-                <Typography className="font-semibold text-blue-gray-400">Testmonial</Typography>
+                <Typography className="font-medium text-blue-gray-400">Testmonial</Typography>
               </div>
               <div className="mb-1 flex items-center">
                 <div className="mr-2 h-2 w-2 rounded-full bg-blue-gray-100"></div>
-                <Typography className="font-semibold text-blue-gray-400">FAQ</Typography>
+                <Typography className="font-medium text-blue-gray-400">FAQ</Typography>
               </div>
               <div className="mb-1 flex items-center">
                 <div className="mr-2 h-2 w-2 rounded-full bg-blue-gray-100"></div>
-                <Typography className="font-semibold text-blue-gray-400">Content</Typography>
+                <Typography className="font-medium text-blue-gray-400">Content</Typography>
               </div>
               <div className="flex items-center">
                 <div className="mr-2 h-2 w-2 rounded-full bg-blue-gray-100"></div>
-                <Typography className="font-semibold text-blue-gray-400">Metrics</Typography>
+                <Typography className="font-medium text-blue-gray-400">Metrics</Typography>
               </div>
             </div>
           </TimelineBody>
