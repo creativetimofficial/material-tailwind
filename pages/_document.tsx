945a9754c8531f058f1c5d4be974db56c7cb4970
dragosct10 <dragos@creative-tim.com>
2023-08-18 16:41:21 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function MyDocument() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head>
+        <link rel="canonical" href="https://www.material-tailwind.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
