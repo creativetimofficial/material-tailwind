1a49655a28c9f801661c0ee3dcc8e9f2047bce04
lzadhito <lzadhito@gmail.com>
2023-08-25 21:37:55 +0700

    refactor(docs-content/react/button): explicit loading boolean prop

--- docs-content/react/button/button-loading.tsx
+++ docs-content/react/button/button-loading.tsx
@@ -3,14 +3,14 @@ import { Button } from "@material-tailwind/react";
 export function ButtonLoading() {
   return (
     <div className="flex items-center gap-4">
-      <Button loading>Loading</Button>
-      <Button variant="outlined" loading>
+      <Button loading={true}>Loading</Button>
+      <Button variant="outlined" loading={true}>
         Loading
       </Button>
-      <Button variant="text" loading>
+      <Button variant="text" loading={true}>
         Loading
       </Button>
-      <Button className="rounded-full" loading>
+      <Button className="rounded-full" loading={true}>
         Loading
       </Button>
     </div>
