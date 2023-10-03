197136234f9b52acab75aaddd8ac494e8a412eb4
Sajad Ahmad Nawabi <nawabisajadahmad@gmail.com>
2023-10-03 09:56:00 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,19 +16,19 @@ import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }) {  
   const router = useRouter()
 
-  useEffect(() => {
-    // This pageview only triggers the first time (it's important for Pixel to have real information)
-    fbq.pageview()
+  // useEffect(() => {
+  //   // This pageview only triggers the first time (it's important for Pixel to have real information)
+  //   fbq.pageview()
 
-    const handleRouteChange = () => {
-      fbq.pageview()
-    }
+  //   const handleRouteChange = () => {
+  //     fbq.pageview()
+  //   }
 
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+  //   router.events.on('routeChangeComplete', handleRouteChange)
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
 
   return (
     <Fragment>
