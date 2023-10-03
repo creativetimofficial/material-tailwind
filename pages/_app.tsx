ceb2e1bd34e58c70bcbb87d06b643d13186f7444
dragosct10 <dragos@creative-tim.com>
2023-10-03 12:26:18 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,33 +6,42 @@ import Script from "next/script";
 // @material-tailwind components
 import { ThemeProvider } from "@material-tailwind/react";
 
-import { useRouter } from 'next/router'
-
 // styles
 import "/styles/globals.css";
 import "@docsearch/css";
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+const pixelId = "111649226022273";
 
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('111649226022273') // facebookPixelId
-        ReactPixel.pageView()
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
-  
+function MyApp({ Component, pageProps }) {  
   return (
     <Fragment>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
+
+      <Script strategy="lazyOnload">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${pixelId}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KB2WKJS');
