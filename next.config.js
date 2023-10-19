ab25ee0eade24e192cc0b695438ea776a61c6f90
dragosct10 <dragos@creative-tim.com>
2023-10-19 15:54:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,56 +8,15 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "nepcha.com"],
   },
+  assetPrefix: isProd
+    ? `https://docs-test.material-tailwind.com`
+    : undefined,
   async redirects() {
     return [
       {
-        source: "/documentation/quick-start",
+        source: "/",
         destination: "/docs/react/installation",
         permanent: true,
-      },
-      {
-        source: "/components",
-        destination: "/docs/react/button",
-        permanent: true,
-      },
-      {
-        source: "/back-to-school",
-        destination: "/blocks",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/theme/:slug*",
-        destination: "/docs/react/:slug*",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/props/:slug*",
-        destination: "/docs/react/:slug*",
-        permanent: true,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/figma",
-        destination:
-          "https://material-taillwind-pro-ct-tailwind-team.vercel.app/figma",
-      },
-      {
-        source: "/blocks",
-        destination:
-          "https://material-taillwind-pro-ct-tailwind-team.vercel.app/blocks",
-      },
-      {
-        source: "/back-to-school",
-        destination:
-          "https://material-taillwind-pro-ct-tailwind-team.vercel.app/blocks",
-      },
-      {
-        source: "/blocks/:slug*",
-        destination:
-          "https://material-taillwind-pro-ct-tailwind-team.vercel.app/blocks/:slug*",
       },
     ];
   },
