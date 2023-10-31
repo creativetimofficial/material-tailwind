ce55efa01d25e2278aff0fbf72cd1bde4e1dc72a
Sajad Ahmad Nawabi <nawabisajadahmad@gmail.com>
2023-10-31 09:30:27 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,33 +15,33 @@ export default function MyDocument() {
         />
         <link
           rel="shortcut icon"
-          href="https://docs.material-tailwind.com/favicon.png/favicon.png"
+          href="https://docs.material-tailwind.com/favicon.png"
           type="image/png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="https://docs.material-tailwind.com/favicon.png/favicon.png"
+          href="https://docs.material-tailwind.com/favicon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="https://docs.material-tailwind.com/favicon.png/favicon.png"
+          href="https://docs.material-tailwind.com/favicon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="https://docs.material-tailwind.com/favicon.png/favicon.png"
+          href="https://docs.material-tailwind.com/favicon.png"
         />
         <link
           rel="manifest"
-          href="https://docs.material-tailwind.com/favicon.png/img/site.webmanifest"
+          href="https://docs.material-tailwind.com/img/site.webmanifest"
         />
         <link
           rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
+          href="https://docs.material-tailwind.com/img/safari-pinned-tab.svg"
           color="#020617"
         />
         <meta name="msapplication-TileColor" content="#020617" />
