c3477f330478145da4dfca2978ae5d26580229e4
Sayed Murtaza <mortazasadat37@gmail.com>
2023-12-20 13:46:08 +0430

    gallery update

--- docs-content/html/gallery/masonry-grid-gallery.tsx
+++ docs-content/html/gallery/masonry-grid-gallery.tsx
@@ -4,21 +4,21 @@ export function MasonryGridGallery() {
       <div className="grid gap-4">
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg "
+            className="h-auto max-w-full rounded-lg object-cover object-center"
             src="https://www.material-tailwind.com/image/product-11.jpeg"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
             src="https://www.material-tailwind.com/image/cart-illustration.svg"
             alt="gallery-photo"
           />
@@ -27,21 +27,21 @@ export function MasonryGridGallery() {
       <div className="grid gap-4">
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg "
+            className="h-auto max-w-full rounded-lg object-cover object-center"
             src="https://docs.material-tailwind.com/img/team-3.jpg"
             alt="gallery-photo"
           />
@@ -50,22 +50,22 @@ export function MasonryGridGallery() {
       <div className="grid gap-4">
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg "
+            className="h-auto max-w-full rounded-lg object-cover object-center"
             src="https://docs.material-tailwind.com/img/team-3.jpg"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
             alt="gallery-photo"
           />
         </div>
@@ -73,15 +73,15 @@ export function MasonryGridGallery() {
       <div className="grid gap-4">
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
             alt="gallery-photo"
           />
         </div>
         <div>
           <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+            className="h-auto max-w-full rounded-lg object-cover object-center"
+            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
             alt="gallery-photo"
           />
         </div>
