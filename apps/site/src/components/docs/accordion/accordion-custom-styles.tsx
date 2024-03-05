2acd16dc2b887596f9f08ef5f86daaa65a168697
Maisam Afshar <maisam.afshar@gmail.com>
2024-03-05 14:31:38 +0430

    update accordion custom styles

--- apps/site/src/components/docs/accordion/accordion-custom-styles.tsx
+++ apps/site/src/components/docs/accordion/accordion-custom-styles.tsx
@@ -6,13 +6,13 @@ export function AccordionCustomStyles() {
     <Accordion defaultValue="react">
       <Accordion.Item
         value="react"
-        className="mb-2 rounded-lg border border-surface p-2"
+        className="mb-2 rounded-lg border border-surface p-3"
       >
-        <Accordion.Trigger>
+        <Accordion.Trigger className="p-0">
           Material Tailwind React
           <NavArrowDown className="h-4 w-4 group-data-[open=true]:rotate-180" />
         </Accordion.Trigger>
-        <Accordion.Content>
+        <Accordion.Content className="pb-0 pt-3">
           Material Tailwind is an open-source crafted in Tailwind CSS. Get
           Material Tailwind and take advantage of its free components and
           features that will help you set up your web project quickly.
@@ -20,13 +20,13 @@ export function AccordionCustomStyles() {
       </Accordion.Item>
       <Accordion.Item
         value="html"
-        className="mb-2 rounded-lg border border-surface p-2"
+        className="mb-2 rounded-lg border border-surface p-3"
       >
-        <Accordion.Trigger>
+        <Accordion.Trigger className="p-0">
           Material Tailwind HTML
           <NavArrowDown className="h-4 w-4 group-data-[open=true]:rotate-180" />
         </Accordion.Trigger>
-        <Accordion.Content>
+        <Accordion.Content className="pb-0 pt-3">
           Material Tailwind is an open-source crafted in Tailwind CSS. Get
           Material Tailwind and take advantage of its free components and
           features that will help you set up your web project quickly.
@@ -34,13 +34,13 @@ export function AccordionCustomStyles() {
       </Accordion.Item>
       <Accordion.Item
         value="vue"
-        className="rounded-lg border border-surface p-2"
+        className="rounded-lg border border-surface p-3"
       >
-        <Accordion.Trigger>
+        <Accordion.Trigger className="p-0">
           Material Tailwind Vue
           <NavArrowDown className="h-4 w-4 group-data-[open=true]:rotate-180" />
         </Accordion.Trigger>
-        <Accordion.Content>
+        <Accordion.Content className="pb-0 pt-3">
           Material Tailwind is an open-source crafted in Tailwind CSS. Get
           Material Tailwind and take advantage of its free components and
           features that will help you set up your web project quickly.
