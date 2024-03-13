3c893231b38cda887e296d9b87e58b6a5584778b
Maisam Afshar <maisam.afshar@gmail.com>
2024-03-13 14:03:01 +0430

    add input-with-dropdown

--- apps/site/src/components/docs/input/index.ts
+++ apps/site/src/components/docs/input/index.ts
@@ -6,5 +6,6 @@ export * from "./input-with-icon";
 export * from "./input-with-helper-text";
 export * from "./input-with-label";
 export * from "./input-with-button";
+export * from "./input-with-dropdown";
 export * from "./input-disabled";
 export * from "./input-custom-styles";
