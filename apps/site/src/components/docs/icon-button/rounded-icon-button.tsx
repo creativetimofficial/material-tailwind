7ab99a5f69c5751e1ecdf95482d09a78d6dac1f5
Maisam Afshar <maisam.afshar@gmail.com>
2024-03-13 13:21:39 +0430

    update rounded-icon-button

--- apps/site/src/components/docs/icon-button/rounded-icon-button.tsx
+++ apps/site/src/components/docs/icon-button/rounded-icon-button.tsx
@@ -4,17 +4,17 @@ import { BrightStar } from "iconoir-react";
 export function RoundedIconButton() {
   return (
     <div className="flex gap-4">
-      <IconButton rounded variant="ghost">
-        <BrightStar className="h-4 w-4 stroke-2" />
+      <IconButton isCircular variant="ghost">
+        <BrightStar className="w-4 h-4 stroke-2" />
       </IconButton>
-      <IconButton rounded variant="outline">
-        <BrightStar className="h-4 w-4 stroke-2" />
+      <IconButton isCircular variant="outline">
+        <BrightStar className="w-4 h-4 stroke-2" />
       </IconButton>
-      <IconButton rounded variant="solid">
-        <BrightStar className="h-4 w-4 stroke-2" />
+      <IconButton isCircular variant="solid">
+        <BrightStar className="w-4 h-4 stroke-2" />
       </IconButton>
-      <IconButton rounded variant="gradient">
-        <BrightStar className="h-4 w-4 stroke-2" />
+      <IconButton isCircular variant="gradient">
+        <BrightStar className="w-4 h-4 stroke-2" />
       </IconButton>
     </div>
   );
