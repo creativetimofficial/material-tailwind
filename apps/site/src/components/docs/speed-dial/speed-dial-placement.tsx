e1766cb60c76e550ea6a9f42fdf744704b71b152
Maisam Afshar <maisam.afshar@gmail.com>
2024-03-18 20:56:51 +0430

    fix: speed-dial-placement opening direction

--- apps/site/src/components/docs/speed-dial/speed-dial-placement.tsx
+++ apps/site/src/components/docs/speed-dial/speed-dial-placement.tsx
@@ -3,9 +3,9 @@ import { Home, Plus, SelectFace3d, Settings } from "iconoir-react";
 
 export function SpeedDialPlacement() {
   return (
-    <div className="relative w-full h-80">
-      <div className="absolute bottom-0 right-0">
-        <Tooltip interactive offset={4} placement="top">
+    <div className="relative h-80 w-full">
+      <div className="absolute left-0 top-0">
+        <Tooltip interactive offset={4} placement="bottom">
           <Tooltip.Trigger as={IconButton} isCircular>
             <Plus className="h-[18px] w-[18px] transition-transform group-data-[open=true]:rotate-45" />
           </Tooltip.Trigger>
@@ -29,6 +29,42 @@ export function SpeedDialPlacement() {
               </Tooltip.Content>
             </Tooltip>
             <Tooltip placement="left">
+              <Tooltip.Trigger as={IconButton} isCircular color="secondary">
+                <SelectFace3d className="h-[18px] w-[18px]" />
+              </Tooltip.Trigger>
+              <Tooltip.Content>
+                Dashboard
+                <Tooltip.Arrow />
+              </Tooltip.Content>
+            </Tooltip>
+          </Tooltip.Content>
+        </Tooltip>
+      </div>
+      <div className="absolute right-0 top-0">
+        <Tooltip interactive offset={4} placement="left">
+          <Tooltip.Trigger as={IconButton} isCircular>
+            <Plus className="h-[18px] w-[18px] transition-transform group-data-[open=true]:rotate-45" />
+          </Tooltip.Trigger>
+          <Tooltip.Content className="flex gap-1 bg-transparent shadow-none">
+            <Tooltip placement="top">
+              <Tooltip.Trigger as={IconButton} isCircular color="secondary">
+                <Home className="h-[18px] w-[18px]" />
+              </Tooltip.Trigger>
+              <Tooltip.Content>
+                Home
+                <Tooltip.Arrow />
+              </Tooltip.Content>
+            </Tooltip>
+            <Tooltip placement="top">
+              <Tooltip.Trigger as={IconButton} isCircular color="secondary">
+                <Settings className="h-[18px] w-[18px]" />
+              </Tooltip.Trigger>
+              <Tooltip.Content>
+                Settings
+                <Tooltip.Arrow />
+              </Tooltip.Content>
+            </Tooltip>
+            <Tooltip placement="top">
               <Tooltip.Trigger as={IconButton} isCircular color="secondary">
                 <SelectFace3d className="h-[18px] w-[18px]" />
               </Tooltip.Trigger>
@@ -76,8 +112,8 @@ export function SpeedDialPlacement() {
           </Tooltip.Content>
         </Tooltip>
       </div>
-      <div className="absolute top-0 left-0">
-        <Tooltip interactive offset={4} placement="bottom">
+      <div className="absolute bottom-0 right-0">
+        <Tooltip interactive offset={4} placement="top">
           <Tooltip.Trigger as={IconButton} isCircular>
             <Plus className="h-[18px] w-[18px] transition-transform group-data-[open=true]:rotate-45" />
           </Tooltip.Trigger>
@@ -101,42 +137,6 @@ export function SpeedDialPlacement() {
               </Tooltip.Content>
             </Tooltip>
             <Tooltip placement="left">
-              <Tooltip.Trigger as={IconButton} isCircular color="secondary">
-                <SelectFace3d className="h-[18px] w-[18px]" />
-              </Tooltip.Trigger>
-              <Tooltip.Content>
-                Dashboard
-                <Tooltip.Arrow />
-              </Tooltip.Content>
-            </Tooltip>
-          </Tooltip.Content>
-        </Tooltip>
-      </div>
-      <div className="absolute top-0 right-0">
-        <Tooltip interactive offset={4} placement="bottom">
-          <Tooltip.Trigger as={IconButton} isCircular>
-            <Plus className="h-[18px] w-[18px] transition-transform group-data-[open=true]:rotate-45" />
-          </Tooltip.Trigger>
-          <Tooltip.Content className="flex flex-col gap-1 bg-transparent shadow-none">
-            <Tooltip placement="right">
-              <Tooltip.Trigger as={IconButton} isCircular color="secondary">
-                <Home className="h-[18px] w-[18px]" />
-              </Tooltip.Trigger>
-              <Tooltip.Content>
-                Home
-                <Tooltip.Arrow />
-              </Tooltip.Content>
-            </Tooltip>
-            <Tooltip placement="right">
-              <Tooltip.Trigger as={IconButton} isCircular color="secondary">
-                <Settings className="h-[18px] w-[18px]" />
-              </Tooltip.Trigger>
-              <Tooltip.Content>
-                Settings
-                <Tooltip.Arrow />
-              </Tooltip.Content>
-            </Tooltip>
-            <Tooltip placement="right">
               <Tooltip.Trigger as={IconButton} isCircular color="secondary">
                 <SelectFace3d className="h-[18px] w-[18px]" />
               </Tooltip.Trigger>
