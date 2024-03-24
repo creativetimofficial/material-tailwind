23c804ac33109aa3598855ade099ccc05a2b6d33
Maisam Afshar <maisam.afshar@gmail.com>
2024-03-24 15:29:13 +0430

    remove unused Button import

--- apps/site/src/components/docs/chip/chip-dismissible.tsx
+++ apps/site/src/components/docs/chip/chip-dismissible.tsx
@@ -1,11 +1,11 @@
-import { Chip, Button } from "@material-tailwind/react/dist";
+import { Chip } from "@material-tailwind/react/dist";
 import { ProfileCircle } from "iconoir-react";
 
 export function ChipDismissible() {
   return (
     <Chip>
       <Chip.Icon>
-        <ProfileCircle className="h-full w-full" />
+        <ProfileCircle className="w-full h-full" />
       </Chip.Icon>
       <Chip.Label>Material Tailwind</Chip.Label>
       <Chip.DismissTrigger />
