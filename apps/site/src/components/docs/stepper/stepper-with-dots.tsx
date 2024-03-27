7cb6c346607926c9ef51303c96104eac84a78049
Maisam Afshar <maisam.afshar@gmail.com>
2024-03-27 06:37:41 +0430

    update timeline with dots

--- apps/site/src/components/docs/stepper/stepper-with-dots.tsx
+++ apps/site/src/components/docs/stepper/stepper-with-dots.tsx
@@ -14,23 +14,23 @@ export function StepperWithDots() {
         <Timeline.Item disabled={step < 0} value={0} className="w-full">
           <Timeline.Header>
             <Timeline.Separator />
-            <Timeline.Icon className="h-4 w-4 " />
+            <Timeline.Icon className="w-4 h-4" />
           </Timeline.Header>
         </Timeline.Item>
         <Timeline.Item disabled={step < 1} value={1} className="w-full">
           <Timeline.Header>
             <Timeline.Separator />
-            <Timeline.Icon className="h-4 w-4 " />
+            <Timeline.Icon className="w-4 h-4" />
           </Timeline.Header>
         </Timeline.Item>
         <Timeline.Item disabled={step < 2} value={2}>
           <Timeline.Header>
-            <Timeline.Icon className="h-4 w-4 " />
+            <Timeline.Icon className="w-4 h-4" />
           </Timeline.Header>
         </Timeline.Item>
       </Timeline>
 
-      <div className="mt-14 flex w-full justify-between gap-4">
+      <div className="flex justify-between w-full gap-4 mt-14">
         <Button disabled={step === 0} onClick={() => setStep(step - 1)}>
           Previous
         </Button>
