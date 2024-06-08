285ec5ea8fa3b7f6f4c2e644333ca98cb18f9f12
sajadevo <nawabisajadahmad@gmail.com>
2024-06-08 19:29:16 +0430

    update button demo for react examples

--- apps/site/src/components/docs/button/button-demo.tsx
+++ apps/site/src/components/docs/button/button-demo.tsx
@@ -1,10 +1,5 @@
 import { Button } from "@material-tailwind/react";
-import Link from "next/link";
 
 export function ButtonDemo() {
-  return (
-    <Button as={Link} href="/home" size="lg" variant="solid" color="primary">
-      Button
-    </Button>
-  );
+  return <Button>Button</Button>;
 }
