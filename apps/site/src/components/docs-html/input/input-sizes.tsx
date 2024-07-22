934a15d6bfff9b96bf23b07358137a01a78f5406
sajadevo <nawabisajadahmad@gmail.com>
2024-07-22 15:04:30 +0430

    update html version input examples

--- apps/site/src/components/docs-html/input/input-sizes.tsx
+++ apps/site/src/components/docs-html/input/input-sizes.tsx
@@ -1,27 +1,21 @@
 export function InputSizes() {
   return (
-    <div className="w-72 space-y-4">
-      <div className="relative w-full rounded-md text-sm leading-none">
-        <input
-          placeholder="Input Small"
-          className="peer h-full w-full select-none rounded-[inherit] border border-surface bg-transparent px-1.5 py-[4.5px] leading-[inherit] text-black outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 dark:text-white"
-          type="text"
-        />
-      </div>
-      <div className="relative w-full rounded-md text-sm leading-none">
-        <input
-          placeholder="Input Medium"
-          className="peer h-full w-full select-none rounded-[inherit] border border-surface bg-transparent px-2.5 py-[8.5px] leading-[inherit] text-black outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 dark:text-white"
-          type="text"
-        />
-      </div>
-      <div className="relative w-full rounded-lg text-base leading-none">
-        <input
-          placeholder="Input Large"
-          className="peer h-full w-full select-none rounded-[inherit] border border-surface bg-transparent px-3.5 py-[12.25px] leading-[inherit] text-black outline-none ring-4 ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 dark:text-white"
-          type="text"
-        />
-      </div>
+    <div className="flex flex-col gap-4">
+      <input
+        placeholder="Input Small"
+        className="w-72 select-none rounded-md border border-surface bg-transparent px-1.5 py-[4.5px] text-sm leading-none text-black outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 dark:text-white"
+        type="text"
+      />
+      <input
+        placeholder="Input Medium"
+        className="w-72 select-none rounded-md border border-surface bg-transparent px-2.5 py-[8.5px] text-sm leading-none text-black outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 dark:text-white"
+        type="text"
+      />
+      <input
+        placeholder="Input Large"
+        className="w-72 select-none rounded-lg border border-surface bg-transparent px-3.5 py-[12.25px] text-base leading-none text-black outline-none ring-4 ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 dark:text-white"
+        type="text"
+      />
     </div>
   );
 }
