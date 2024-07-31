d068e12755e15694836c216adfe6fe924cdca631
sanam453 <sanam.hamza9@gmail.com>
2024-07-31 16:24:15 +0430

    Fixed add-product-dialog

--- docs-content/react/dialog/add-product-dialog.tsx
+++ docs-content/react/dialog/add-product-dialog.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import {
+  Input,
+  Option,
+  Select,
   Button,
   Dialog,
-  DialogHeader,
-  DialogBody,
-  DialogFooter,
-  Typography,
+  Textarea,
   IconButton,
+  Typography,
+  DialogBody,
+  DialogHeader,
+  DialogFooter,
 } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
@@ -22,10 +26,10 @@ export function AddProductDialog() {
       </Button>
       <Dialog size="sm" open={open} handler={handleOpen}>
         <DialogHeader className="relative m-0 block p-6">
-          <Typography variant="h5" color="blue-gray">
+          <Typography variant="h4" color="blue-gray">
             Manage Item
           </Typography>
-          <Typography className="font-normal text-gray-600">
+          <Typography className="mt-1 font-normal text-gray-600">
             Keep your records up-to-date and organized.
           </Typography>
           <IconButton
@@ -37,7 +41,117 @@ export function AddProductDialog() {
             <XMarkIcon className="h-4 w-4 stroke-2" />
           </IconButton>
         </DialogHeader>
-        <DialogBody className="px-6 pb-6 pt-2">Content Here</DialogBody>
+        <DialogBody className="space-y-4 px-6 pb-6 pt-2">
+          <div>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="mb-2 text-left font-medium"
+            >
+              Name
+            </Typography>
+            <Input
+              color="gray"
+              size="lg"
+              placeholder="eg. White Shoes"
+              name="name"
+              className="focus:border-t-gray-900"
+              containerProps={{
+                className: "!min-w-full",
+              }}
+              labelProps={{
+                className: "hidden",
+              }}
+            />
+          </div>
+          <div>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="mb-2 text-left font-medium"
+            >
+              Category
+            </Typography>
+            <Select
+              className="!w-full !border-[1.5px] !border-blue-gray-200/90 !border-t-blue-gray-200/90 bg-white text-gray-800 ring-4 ring-transparent placeholder:text-gray-600 focus:!border-primary focus:!border-t-blue-gray-900 group-hover:!border-primary"
+              placeholder="1"
+              animate={{
+                mount: { y: 0 },
+                unmount: { y: 25 },
+              }}
+              labelProps={{
+                className: "hidden",
+              }}
+            >
+              <Option>Clothing</Option>
+              <Option>Clothing</Option>
+              <Option>Clothing</Option>
+            </Select>
+          </div>
+          <div className="flex gap-4">
+            <div className="w-full">
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="mb-2 text-left font-medium"
+              >
+                Weight
+              </Typography>
+              <Input
+                color="gray"
+                size="lg"
+                placeholder="eg. <8.8oz | 250g"
+                name="weight"
+                className="focus:border-t-gray-900"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+                labelProps={{
+                  className: "hidden",
+                }}
+              />
+            </div>
+            <div className="w-full">
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="mb-2 text-left font-medium"
+              >
+                Size
+              </Typography>
+              <Input
+                color="gray"
+                size="lg"
+                placeholder="eg. US 8"
+                name="size"
+                className="focus:border-t-gray-900"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+                labelProps={{
+                  className: "hidden",
+                }}
+              />
+            </div>
+          </div>
+          <div>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="mb-2 text-left font-medium"
+            >
+              Description (Optional)
+            </Typography>
+            <Textarea
+              rows={7}
+              value="More details about product."
+              className="!w-full !border-[1.5px] !border-blue-gray-200/90 !border-t-blue-gray-200/90 bg-white text-gray-600 ring-4 ring-transparent placeholder:text-gray-600 focus:!border-primary focus:!border-t-blue-gray-900 group-hover:!border-primary"
+              labelProps={{
+                className: "hidden",
+              }}
+            />
+          </div>
+        </DialogBody>
         <DialogFooter>
           <Button className="ml-auto" onClick={handleOpen}>
             Add Product
