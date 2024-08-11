a2a78473c89f3c92540a4b7e7c6bc64ce5b551ec
sajadevo <nawabisajadahmad@gmail.com>
2024-08-11 21:56:55 +0430

    update swiper docs page for react

--- apps/site/src/components/docs/plugins/swiper/carousel-demo.tsx
+++ apps/site/src/components/docs/plugins/swiper/carousel-demo.tsx
@@ -54,12 +54,18 @@ export function CarouselDemo() {
         modules={[Navigation, Pagination]}
         className="relative rounded-lg [&_div.swiper-button-next]:text-background [&_div.swiper-button-prev]:text-background "
       >
-        {[...Array(5)].map((_, index) => (
+        {[
+          "https://images.unsplash.com/photo-1470813740244-df37b8c1edcb?w=1600&auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlfGVufDB8MHwwfHx8MA%3D%3D",
+          "https://plus.unsplash.com/premium_photo-1673603988651-99f79e4ae7d3?w=1600&auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8MHwwfHx8MA%3D%3D",
+          "https://images.unsplash.com/photo-1465189684280-6a8fa9b19a7a?w=1600&auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZXxlbnwwfDB8MHx8fDA%3D",
+          "https://images.unsplash.com/photo-1458668383970-8ddd3927deed?w=1600&auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJlfGVufDB8MHwwfHx8MA%3D%3D",
+          "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1600&auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfDB8MHx8fDA%3D",
+        ].map((img, index) => (
           <SwiperSlide key={index} className="select-none">
             <img
+              src={img}
               alt={`image-${index}`}
               className="h-[28rem] w-full object-cover"
-              src={`https://source.unsplash.com/random?nature%20${index}&orientation=landscape&q=80`}
             />
           </SwiperSlide>
         ))}
