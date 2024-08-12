9a5d457eaaf4a12ca014d7fa479028ca4a52ea33
dragosct10 <dragos@creative-tim.com>
2024-08-12 15:17:36 +0300

    Create input-number-with-label copy.tsx

---
+++ docs-content/html/input-number/input-number-with-label copy.tsx
@@ -0,0 +1,12 @@
+export function InputNumberWithLabel() {
+  return (
+    <div className="w-full max-w-sm min-w-[200px]">
+      <label className="block mb-1 text-sm text-neutral-900">
+          Your Name
+      </label>
+      <input
+        className="w-full h-10 bg-transparent text-neutral-700 text-sm border border-neutral-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-neutral-400 hover:border-neutral-400 shadow-sm focus:shadow-md"
+        placeholder="Enter your text" />
+    </div>
+  );
+}
