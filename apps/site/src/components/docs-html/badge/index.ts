10277a06dbbc93161607829f3846752090626b7a
Maisam Afshar <maisam.afshar@gmail.com>
2024-08-12 15:23:57 +0500

    add badge example for html version

---
+++ apps/site/src/components/docs-html/badge/index.ts
@@ -0,0 +1,7 @@
+export * from "./badge-demo";
+export * from "./badge-colors";
+export * from "./badge-placement";
+export * from "./badge-dot";
+export * from "./badge-overlap";
+export * from "./badge-with-border";
+export * from "./badge-custom-styles";
