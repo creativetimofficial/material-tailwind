66668f1c67faa9515dc489e705a9661330770953
dragosct10 <dragos@creative-tim.com>
2024-08-12 15:18:09 +0300

    Update docs-navbar.tsx

--- widgets/layout/docs-navbar.tsx
+++ widgets/layout/docs-navbar.tsx
@@ -95,6 +95,7 @@ import * as HTMLChipExamples from "docs-content/html/chip";
 import * as HTMLDialogExamples from "docs-content/html/dialog";
 import * as HTMLIconButtonExamples from "docs-content/html/icon-button";
 import * as HTMLInputExamples from "docs-content/html/input";
+import * as HTMLInputNumberExamples from "docs-content/html/input-number";
 import * as HTMLFormExamples from "docs-content/html/form";
 import * as HTMLListExamples from "docs-content/html/list";
 import * as HTMLMenuExamples from "docs-content/html/menu";
@@ -378,6 +379,7 @@ const components = {
   HTMLDialogExamples,
   HTMLIconButtonExamples,
   HTMLInputExamples,
+  HTMLInputNumberExamples,
   HTMLFormExamples,
   HTMLListExamples,
   HTMLMenuExamples,
