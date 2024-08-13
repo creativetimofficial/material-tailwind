a14bab09b8fc0ed3c119cf7dad328e28930ffae7
Maisam Afshar <maisam.afshar@gmail.com>
2024-08-13 16:45:53 +0500

    remove buttons from html stepper examples

--- apps/site/src/components/docs-html/stepper/stepper-with-dots.tsx
+++ apps/site/src/components/docs-html/stepper/stepper-with-dots.tsx
@@ -20,17 +20,6 @@ export function StepperWithDots() {
           </div>
         </div>
       </div>
-      <div className="mt-14 flex w-full justify-between gap-4">
-        <button
-          disabled={true}
-          className="inline-flex cursor-not-allowed select-none items-center justify-center rounded-md border border-primary bg-primary px-3.5 py-2.5 text-center align-middle font-sans text-sm font-medium leading-none text-primary-foreground opacity-50 shadow-none transition-all duration-300 ease-in hover:border-primary-light hover:bg-primary-light"
-        >
-          Previous
-        </button>
-        <button className="inline-flex select-none items-center justify-center rounded-md border border-primary bg-primary px-3.5 py-2.5 text-center align-middle font-sans text-sm font-medium leading-none text-primary-foreground transition-all duration-300 ease-in hover:border-primary-light hover:bg-primary-light">
-          Next
-        </button>
-      </div>
     </div>
   );
 }
