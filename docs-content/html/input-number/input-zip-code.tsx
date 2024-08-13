a973a8fe433c04e7adf624f581151d32ea6ce0a1
dragosct10 <dragos@creative-tim.com>
2024-08-13 15:00:58 +0300

    Update input-zip-code.tsx

--- docs-content/html/input-number/input-zip-code.tsx
+++ docs-content/html/input-number/input-zip-code.tsx
@@ -7,15 +7,17 @@ export function InputZipCode() {
       <input
         type="number"
         className="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-        placeholder="Enter your text" />
+        placeholder="ZIP code (020111)"
+        maxLength={5}       
+      />
 
-      <p class="flex items-center gap-1 mt-2 font-sans text-sm antialiased font-normal leading-normal text-gray-700">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 -mt-px">
+      <p className="flex items-center mt-2 text-xs text-slate-500">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2 text-slate-400">
           <path fill-rule="evenodd"
             d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
             clip-rule="evenodd"></path>
         </svg>
-        Use at least 8 characters, one uppercase, one lowercase and one number.
+        Your text help us to provide location-specific services.
       </p>    
     </div>
   );
