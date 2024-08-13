518ad82cf1e9a133a6be7ef44efc05cea9f9213a
dragosct10 <dragos@creative-tim.com>
2024-08-13 15:00:38 +0300

    Update index.ts

--- docs-content/html/input-number/index.ts
+++ docs-content/html/input-number/index.ts
@@ -1,2 +1,7 @@
 export * from "./input-number-with-select";
 export * from "./input-zip-code";
+export * from "./input-phone-number";
+export * from "./input-amount";
+export * from "./input-amount-with-icon";
+export * from "./input-counter-buttons";
+export * from "./input-currency";
