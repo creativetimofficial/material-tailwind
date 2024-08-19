9ed903ff86b1b8bc5c9324a9a2ea7d87ba079cdd
dragosct10 <dragos@creative-tim.com>
2024-08-19 12:11:09 +0300

    Update index.ts

--- docs-content/html/input-phone/index.ts
+++ docs-content/html/input-phone/index.ts
@@ -3,3 +3,5 @@ export * from "./input-phone-validation-text";
 export * from "./input-phone-icon";
 export * from "./input-phone-with-country-code";
 export * from "./input-phone-with-floating-label";
+export * from "./input-phone-number-with-select";
+export * from "./input-phone-with-authentication-form";
