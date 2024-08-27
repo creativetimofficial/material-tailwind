0141d7fffea2cc5674b2e5be494577ad98cc2e6d
sajadevo <nawabisajadahmad@gmail.com>
2024-08-27 15:39:39 +0430

    add script for alert dismissible component

--- apps/site/src/components/docs-html/alert/alert-dismissible.tsx
+++ apps/site/src/components/docs-html/alert/alert-dismissible.tsx
@@ -1,6 +1,24 @@
+"use client";
+
+import * as React from "react";
+
 export function AlertDismissible() {
+  React.useEffect(() => {
+    const alert = document.querySelector("#alert");
+    const alertDismissTrigger = document.querySelector(
+      "#alert-dismiss-trigger",
+    );
+
+    if (alert && alertDismissTrigger) {
+      alertDismissTrigger.addEventListener("click", () => {
+        alert.remove();
+      });
+    }
+  }, []);
+
   return (
     <div
+      id="alert"
       role="alert"
       className="relative flex w-full items-start rounded-md border border-primary bg-primary p-2 text-primary-foreground"
     >
@@ -8,7 +26,7 @@ export function AlertDismissible() {
         <svg
           width="1.5em"
           height="1.5em"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -18,27 +36,27 @@ export function AlertDismissible() {
           <path
             d="M12 7L12 13"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></path>
           <path
             d="M12 17.01L12.01 16.9989"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></path>
           <path
             d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></path>
         </svg>
       </span>
       <div className="m-1.5 w-full font-sans text-base leading-none">
         A simple alert for showing message.
       </div>
-      <button className="outline-none" data-dismissible-target="alert">
+      <button id="alert-dismiss-trigger" className="outline-none">
         <svg
           viewBox="0 0 24 24"
           fill="none"
@@ -49,8 +67,8 @@ export function AlertDismissible() {
           <path
             d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></path>
         </svg>
       </button>
