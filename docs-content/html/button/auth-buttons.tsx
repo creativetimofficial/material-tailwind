1bf128404ff254d0c8fd92af4ea3321a7517df08
dragosct10 <dragos@creative-tim.com>
2024-08-29 14:22:50 +0300

    Update auth-buttons.tsx

--- docs-content/html/button/auth-buttons.tsx
+++ docs-content/html/button/auth-buttons.tsx
@@ -1,40 +1,32 @@
 export function AuthButtons() {
   return (
     <div className="flex flex-col items-center gap-4">
-      <button
-        className="flex select-none items-center gap-3 rounded-lg bg-white py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-blue-gray-900 shadow-md shadow-blue-gray-500/10 transition-all hover:shadow-lg hover:shadow-blue-gray-500/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
+      <button className="rounded-md flex items-center border border-transparent py-2 px-4 text-center text-sm transition-all text-slate-600 hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
         <img
           src="https://docs.material-tailwind.com/icons/metamask.svg"
           alt="metamask"
-          className="h-6 w-6"
+          className="h-5 w-5 mr-2"
         />
         Connect Wallet
       </button>
-      <button
-        className="flex select-none items-center gap-3 rounded-lg border border-blue-gray-500 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-blue-gray-500 transition-all hover:opacity-75 focus:ring focus:ring-blue-gray-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
+
+      <button className="rounded-md flex items-center border border-slate-300 py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
         <img
           src="https://docs.material-tailwind.com/icons/google.svg"
           alt="metamask"
-          className="h-6 w-6"
+          className="h-5 w-5 mr-2"
         />
         Continue with Google
       </button>
-      <button
-        className="group relative flex select-none items-center gap-3 overflow-hidden rounded-lg bg-gradient-to-tr from-light-blue-600 to-light-blue-400 py-3.5 px-7 pr-[72px] text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-light-blue-500/20 transition-all hover:shadow-lg hover:shadow-light-blue-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-        type="button"
-      >
+
+      <button className="rounded-md flex items-center bg-blue-400 text-white border border-transparent py-2 px-4 text-center text-sm transition-all hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-500 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
         Sign in with Twitter
-        <span className="absolute right-0 grid h-full w-12 place-items-center bg-light-blue-600 transition-colors group-hover:bg-light-blue-700">
-          <img
-            src="https://docs.material-tailwind.com/icons/twitter.svg"
-            alt="metamask"
-            className="h-6 w-6"
-          />
-        </span>
+
+        <img
+          src="https://docs.material-tailwind.com/icons/twitter.svg"
+          alt="twitter"
+          className="h-5 w-5 ml-2"
+        />
       </button>
     </div>
   );
