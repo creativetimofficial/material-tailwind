9183b2dde28c14267dbe5950e72137ef2d57b607
dragosct10 <dragos@creative-tim.com>
2024-08-29 11:54:09 +0300

    Update button.tsx

--- docs-content/html/button/button.tsx
+++ docs-content/html/button/button.tsx
@@ -1,9 +1,6 @@
 export function ButtonDefault() {
   return (
-    <button
-      className="select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button"
-    >
+    <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
       Button
     </button>
   );
