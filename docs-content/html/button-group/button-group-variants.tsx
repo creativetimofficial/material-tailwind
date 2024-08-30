cfd4456b0b0f502b7251f372fd2cf7dbdb89f68e
dragosct10 <dragos@creative-tim.com>
2024-08-30 13:21:58 +0300

    Update button-group-variants.tsx

--- docs-content/html/button-group/button-group-variants.tsx
+++ docs-content/html/button-group/button-group-variants.tsx
@@ -2,7 +2,7 @@ export function ButtonGroupVariants() {
   return (
     <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
       <div className="flex w-max flex-col gap-4">
-        <div className="row flex divide-x divide-gray-800">
+        <div className="row flex divide-x divide-slate-700">
           <button className="rounded-md rounded-r-none bg-slate-800 py-2 px-4 border border-transparent border-r-0 text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
             One
           </button>
@@ -14,7 +14,7 @@ export function ButtonGroupVariants() {
           </button>
         </div>
 
-        <div className="row flex divide-x divide-gray-800">
+        <div className="row flex divide-x divide-slate-700">
           <button className="rounded-md rounded-r-none bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
             One
           </button>
@@ -38,21 +38,22 @@ export function ButtonGroupVariants() {
             Three
           </button>
         </div>
-        <div className="row flex divide-x divide-gray-800">
+
+        <div className="row flex">
           <button
-            className="select-none rounded-lg rounded-r-none border-r-0 px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md rounded-r-none py-2 px-4 text-center text-sm transition-all hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 focus:text-white focus:bg-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
           >
             One
           </button>
           <button
-            className="select-none rounded-lg rounded-l-none rounded-r-none border-r-0 px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-none border-l border-r border-slate-300 py-2 px-4 text-center text-sm transition-all hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
           >
             Two
           </button>
           <button
-            className="select-none rounded-lg rounded-l-none px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            className="rounded-md rounded-l-none py-2 px-4 text-center text-sm transition-all hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 focus:text-white focus:bg-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
           >
             Three
