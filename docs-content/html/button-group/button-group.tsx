3277667dd33a1704224709762b32eef4bb855736
dragosct10 <dragos@creative-tim.com>
2024-08-30 13:12:10 +0300

    Update button-group.tsx

--- docs-content/html/button-group/button-group.tsx
+++ docs-content/html/button-group/button-group.tsx
@@ -1,7 +1,7 @@
 export function ButtonGroupDefault() {
   return (
     <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-      <div className="row flex divide-x divide-slate-800">
+      <div className="row flex divide-x divide-slate-700">
         <button className="rounded-md rounded-r-none bg-slate-800 py-2 px-4 border border-transparent border-r-0 text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
           One
         </button>
