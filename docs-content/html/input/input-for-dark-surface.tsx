86b817f0f7e4e3ca51a16af9ac7b99e9ead045c1
dragosct10 <dragos@creative-tim.com>
2024-09-02 11:36:24 +0300

    Update input-for-dark-surface.tsx

--- docs-content/html/input/input-for-dark-surface.tsx
+++ docs-content/html/input/input-for-dark-surface.tsx
@@ -1,16 +1,9 @@
 export function InputForDarkSurface() {
   return (
-    <div className="grid h-full w-full place-items-center rounded-lg bg-gray-900">
-      <div className="w-72">
-        <div className="relative h-10 w-full min-w-[200px]">
-          <input
-            type="email"
-            className="peer h-full w-full rounded-[7px] border border-white border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal  !text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-white focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder=" "
-          />
-          <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight !text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-white before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-white after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-white peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-white peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-            Email Address
-          </label>
+    <div className="grid h-full w-full place-items-center rounded-lg bg-slate-800">
+      <div className="flex w-72 flex-col">
+        <div className="w-full max-w-sm min-w-[200px]">
+          <input className="w-full bg-transparent placeholder:text-slate-300 text-white text-sm border border-slate-400 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-50 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Type here..." />
         </div>
       </div>
     </div>
