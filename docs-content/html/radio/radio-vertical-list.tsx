1b08f3e1ede64a1daf3753786f580788c94282e6
dragosct10 <dragos@creative-tim.com>
2024-09-03 17:19:53 +0300

    Update radio-vertical-list.tsx

--- docs-content/html/radio/radio-vertical-list.tsx
+++ docs-content/html/radio/radio-vertical-list.tsx
@@ -1,119 +1,78 @@
 export function RadioVerticalList() {
   return (
-    <div className="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-      <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
+    <div className="relative flex flex-col rounded-xl bg-white shadow">
+      <nav className="flex min-w-[240px] flex-col gap-1 p-2">
         <div
           role="button"
-          className="flex w-full items-center rounded-lg p-0 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="flex w-full items-center rounded-lg p-0 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100"
         >
           <label
-            htmlFor="vertical-list-react"
+            htmlFor="react-vertical"
             className="flex w-full cursor-pointer items-center px-3 py-2"
           >
-            <div className="mr-3 grid place-items-center">
-              <div className="inline-flex items-center">
-                <label
-                  className="relative flex cursor-pointer items-center rounded-full p-0"
-                  htmlFor="vertical-list-react"
-                >
-                  <input
-                    name="vertical-list"
-                    id="vertical-list-react"
-                    type="radio"
-                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
-                  />
-                  <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-gray-900 opacity-0 transition-opacity peer-checked:opacity-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-3.5 w-3.5"
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                    >
-                      <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
-                    </svg>
-                  </span>
-                </label>
-              </div>
+            <div className="inline-flex items-center">
+              <label className="relative flex items-center cursor-pointer" htmlFor="react-vertical">
+                <input
+                  name="framework"
+                  type="radio"
+                  className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
+                  id="react-vertical"
+                  defaultChecked
+                />
+                <span className="absolute bg-slate-800 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+              </label>
+              <label className="ml-2 text-slate-600 cursor-pointer text-sm" htmlFor="react-vertical">
+                React.js
+              </label>
             </div>
-            <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-400 antialiased">
-              React.js
-            </p>
           </label>
         </div>
         <div
           role="button"
-          className="flex w-full items-center rounded-lg p-0 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="flex w-full items-center rounded-lg p-0 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100"
         >
           <label
-            htmlFor="vertical-list-vue"
+            htmlFor="vue-vertical"
             className="flex w-full cursor-pointer items-center px-3 py-2"
           >
-            <div className="mr-3 grid place-items-center">
-              <div className="inline-flex items-center">
-                <label
-                  className="relative flex cursor-pointer items-center rounded-full p-0"
-                  htmlFor="vertical-list-vue"
-                >
-                  <input
-                    name="vertical-list"
-                    id="vertical-list-vue"
-                    type="radio"
-                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
-                  />
-                  <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-gray-900 opacity-0 transition-opacity peer-checked:opacity-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-3.5 w-3.5"
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                    >
-                      <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
-                    </svg>
-                  </span>
-                </label>
-              </div>
+            <div className="inline-flex items-center">
+              <label className="relative flex items-center cursor-pointer" htmlFor="vue-vertical">
+                <input
+                  name="framework"
+                  type="radio"
+                  className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
+                  id="vue-vertical"
+                />
+                <span className="absolute bg-slate-800 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+              </label>
+              <label className="ml-2 text-slate-600 cursor-pointer text-sm" htmlFor="vue-vertical">
+                Vue.js
+              </label>
             </div>
-            <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-400 antialiased">
-              Vue.js
-            </p>
           </label>
         </div>
         <div
           role="button"
-          className="flex w-full items-center rounded-lg p-0 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="flex w-full items-center rounded-lg p-0 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100"
         >
           <label
-            htmlFor="vertical-list-svelte"
+            htmlFor="svelte-vertical"
             className="flex w-full cursor-pointer items-center px-3 py-2"
           >
-            <div className="mr-3 grid place-items-center">
-              <div className="inline-flex items-center">
-                <label
-                  className="relative flex cursor-pointer items-center rounded-full p-0"
-                  htmlFor="vertical-list-svelte"
-                >
-                  <input
-                    name="vertical-list"
-                    id="vertical-list-svelte"
-                    type="radio"
-                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
-                  />
-                  <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-gray-900 opacity-0 transition-opacity peer-checked:opacity-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-3.5 w-3.5"
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                    >
-                      <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
-                    </svg>
-                  </span>
-                </label>
-              </div>
+            <div className="inline-flex items-center">
+              <label className="relative flex items-center cursor-pointer" htmlFor="svelte-vertical">
+                <input
+                  name="framework"
+                  type="radio"
+                  className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
+                  id="svelte-vertical"
+                />
+                <span className="absolute bg-slate-800 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+              </label>
+              <label className="ml-2 text-slate-600 cursor-pointer text-sm" htmlFor="svelte-vertical">
+                Svelte.js
+              </label>
             </div>
-            <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-400 antialiased">
-              Svelte.js
-            </p>
           </label>
         </div>
       </nav>
