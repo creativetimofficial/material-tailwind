877e289f3960d0e933041124c23a3b2cd3b20a51
dragosct10 <dragos@creative-tim.com>
2024-09-03 12:29:28 +0300

    Update chip-sizes.tsx

--- docs-content/html/chip/chip-sizes.tsx
+++ docs-content/html/chip/chip-sizes.tsx
@@ -1,14 +1,14 @@
 export function ChipSizes() {
   return (
-    <div className="flex items-end gap-2">
-      <div className="relative grid select-none items-center whitespace-nowrap rounded-md bg-gray-900 px-2 py-1 font-sans text-xs font-bold uppercase text-white">
-        <span className="">chip small</span>
+    <div className="flex items-center gap-3">
+      <div className="rounded-md bg-slate-800 py-1 px-2.5 border border-transparent text-xs text-white transition-all shadow-sm">
+          Small Chip
       </div>
-      <div className="relative grid select-none items-center whitespace-nowrap rounded-lg bg-gray-900 py-1.5 px-3 font-sans text-xs font-bold uppercase text-white">
-        <span className="">chip medium</span>
+      <div className="rounded-md bg-slate-800 py-1 px-2.5 border border-transparent text-sm text-white transition-all shadow-sm">
+          Medium Chip
       </div>
-      <div className="relative grid select-none items-center whitespace-nowrap rounded-lg bg-gray-900 px-4 py-2 font-sans text-xs font-bold uppercase text-white">
-        <span className="">chip large</span>
+      <div className="rounded-md bg-slate-800 py-1.5 px-3 border border-transparent text-sm text-white transition-all shadow-sm">
+          Large Chip
       </div>
     </div>
   );
