a900c6105beb6db7004ad50f1bdddfe340510cf0
dragosct10 <dragos@creative-tim.com>
2024-09-03 13:20:31 +0300

    Update checkbox-ripple-effect.tsx

--- docs-content/html/checkbox/checkbox-ripple-effect.tsx
+++ docs-content/html/checkbox/checkbox-ripple-effect.tsx
@@ -40,7 +40,7 @@ export function CheckboxRippleEffect() {
         <label className="flex items-center cursor-pointer relative" htmlFor="ripple-off">
           <input type="checkbox"
             className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800"
-            id="check-ripple-off" />
+            id="ripple-off" />
           <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
               stroke="currentColor" stroke-width="1">
