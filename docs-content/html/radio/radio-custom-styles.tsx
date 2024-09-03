25e9780cfafba8249f6ef1fbbe76f9dc71072859
dragosct10 <dragos@creative-tim.com>
2024-09-03 17:19:46 +0300

    Update radio-custom-styles.tsx

--- docs-content/html/radio/radio-custom-styles.tsx
+++ docs-content/html/radio/radio-custom-styles.tsx
@@ -1,75 +1,36 @@
 export function RadioCustomStyles() {
   return (
-    <div className="flex gap-10">
+    <div className="flex flex-col gap-10">
+      {/* HTML Radio Button */}
       <div className="inline-flex items-center">
-        <label
-          className="relative flex cursor-pointer items-center rounded-full p-3"
-          htmlFor="custom-style1"
-        >
+        <label className="relative flex items-center cursor-pointer" htmlFor="html-custom">
           <input
-            name="type"
+            name="framework-custom"
             type="radio"
-            className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-gray-900/10 bg-gray-900/5 p-0 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
-            id="custom-style1"
+            className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-200 bg-slate-100 checked:border-slate-300 transition-all"
+            id="html-custom"
+            defaultChecked
           />
-          <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-gray-900 opacity-0 transition-opacity peer-checked:opacity-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-full w-full scale-105"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </span>
+          <span className="absolute bg-slate-800 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
         </label>
-        <label
-          className="mt-px cursor-pointer select-none font-light text-gray-700"
-          htmlFor="custom-style1"
-        >
-          <p className="block font-sans text-base font-normal leading-relaxed text-blue-gray-400 antialiased">
-            React
-          </p>
+        <label className="ml-2 text-slate-600 cursor-pointer text-sm" htmlFor="html-custom">
+          HTML
         </label>
       </div>
+
+      {/* React Radio Button */}
       <div className="inline-flex items-center">
-        <label
-          className="relative flex cursor-pointer items-center rounded-full p-3"
-          htmlFor="custom-style2"
-        >
+        <label className="relative flex items-center cursor-pointer" htmlFor="react-custom">
           <input
-            name="type"
+            name="framework-custom"
             type="radio"
-            className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-gray-900/10 bg-gray-900/5 p-0 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:before:bg-gray-900 hover:before:opacity-0"
-            id="custom-style2"
-            checked
+            className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-200 bg-slate-100 checked:border-slate-300 transition-all"
+            id="react-custom"
           />
-          <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-gray-900 opacity-0 transition-opacity peer-checked:opacity-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-full w-full scale-105"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </span>
+          <span className="absolute bg-slate-800 w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
         </label>
-        <label
-          className="mt-px cursor-pointer select-none font-light text-gray-700"
-          htmlFor="custom-style2"
-        >
-          <p className="block font-sans text-base font-normal leading-relaxed text-blue-gray-400 antialiased">
-            React
-          </p>
+        <label className="ml-2 text-slate-600 cursor-pointer text-sm" htmlFor="react-custom">
+          React
         </label>
       </div>
     </div>
