83540769f8cc238f7dd7d3bba592413ad283cb48
dragosct10 <dragos@creative-tim.com>
2024-09-04 12:32:32 +0300

    Update tabs-with-content.tsx

--- docs-content/html/tabs/tabs-with-content.tsx
+++ docs-content/html/tabs/tabs-with-content.tsx
@@ -2,61 +2,43 @@ export function TabsWithContent() {
   return (
     <div className="w-full">
       <div className="relative right-0">
-        <ul
-          className="relative flex list-none flex-wrap rounded-xl bg-blue-gray-50/60 p-1"
-          data-tabs="tabs"
-          role="list"
-        >
+        <ul className="relative flex flex-wrap px-1.5 py-1.5 list-none rounded-md bg-slate-100" data-tabs="tabs" role="list">
           <li className="z-30 flex-auto text-center">
-            <a
-              className="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
-              data-tab-target=""
-              role="tab"
-              aria-selected="true"
-              aria-controls="app"
-            >
-              <span className="ml-1">App</span>
+            <a className="z-30 flex items-center justify-center w-full px-0 py-2 text-sm mb-0 transition-all ease-in-out border-0 rounded-md cursor-pointer text-slate-600 bg-inherit"
+            data-tab-target="" role="tab" aria-selected="true" aria-controls="dashboard">
+              Dashboard
             </a>
           </li>
           <li className="z-30 flex-auto text-center">
-            <a
-              className="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
-              data-tab-target=""
-              role="tab"
-              aria-selected="false"
-              aria-controls="message"
-            >
-              <span className="ml-1">Messages</span>
+            <a className="z-30 flex items-center justify-center w-full px-0 py-2 mb-0 text-sm transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-600 bg-inherit"
+            data-tab-target="" role="tab" aria-selected="false"  aria-controls="profile">
+              Profile
             </a>
           </li>
           <li className="z-30 flex-auto text-center">
-            <a
-              className="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
-              data-tab-target=""
-              role="tab"
-              aria-selected="false"
-              aria-controls="settings"
-            >
-              <span className="ml-1">Settings</span>
+            <a className="z-30 flex items-center justify-center w-full px-0 py-2 text-sm mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-700 bg-inherit"
+            data-tab-target="" role="tab" aria-selected="false" aria-controls="settings">
+              Settings
             </a>
           </li>
         </ul>
+
         <div data-tab-content="" className="p-5">
-          <div className="block opacity-100" id="app" role="tabpanel">
-            <p className="block font-sans text-base font-light leading-relaxed text-inherit text-blue-gray-500 antialiased">
+          <div id="dashboard" role="tabpanel">
+            <p className="text-slate-400 font-light">
               Because it&apos;s about motivating the doers. Because I&apos;m
               here to follow my dreams and inspire other people to follow their
               dreams, too.
             </p>
           </div>
-          <div className="hidden opacity-0" id="message" role="tabpanel">
-            <p className="block font-sans text-base font-light leading-relaxed text-inherit text-blue-gray-500 antialiased">
+          <div className="hidden opacity-0" id="profile" role="tabpanel">
+            <p className="text-slate-400 font-light">
               The reading of all good books is like a conversation with the
               finest minds of past centuries.
             </p>
           </div>
           <div className="hidden opacity-0" id="settings" role="tabpanel">
-            <p className="block font-sans text-base font-light leading-relaxed text-inherit text-blue-gray-500 antialiased">
+            <p className="text-slate-400 font-light">
               Comparing yourself to others is the thief of joy.
             </p>
           </div>
