24c329697e2718e4806741432908ef5c369c19f3
dragosct10 <dragos@creative-tim.com>
2024-09-10 18:03:01 +0300

    Update default-collapse.tsx

--- docs-content/html/collapse/default-collapse.tsx
+++ docs-content/html/collapse/default-collapse.tsx
@@ -1,20 +1,18 @@
 export function DefaultCollapse() {
   return (
     <>
-      <button
-        data-ripple-light="true"
+      <button 
         data-collapse-target="collapse"
-        className="mb-4 select-none rounded-lg bg-gray-900 px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      >
+        className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
         Open Collapse
       </button>
       <div
         data-collapse="collapse"
         className="block h-0 w-full basis-full overflow-hidden transition-all duration-300 ease-in-out"
       >
-        <div className="relative mx-auto flex w-8/12 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-          <div className="p-6">
-            <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+        <div className="relative mx-auto flex w-8/12 flex-col rounded-lg bg-white border border-slate-200 shadow-sm mt-4">
+          <div className="p-4">
+            <p className="text-slate-600 font-light">
               Use our Tailwind CSS collapse for your website. You can use if for
               accordion, collapsible items and much more.
             </p>
