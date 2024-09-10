527523aeeb7936259e98cfa8a10d3145cf6c2b57
dragosct10 <dragos@creative-tim.com>
2024-09-10 17:13:26 +0300

    Update avatar-with-dot-indicator.tsx

--- docs-content/html/avatar/avatar-with-dot-indicator.tsx
+++ docs-content/html/avatar/avatar-with-dot-indicator.tsx
@@ -7,7 +7,7 @@ export function AvatarWithDotIndicator() {
           alt="avatar"
           className="relative inline-block h-12 w-12 rounded-full object-cover object-center"
         />
-        <span className="absolute right-[14%] top-[14%] grid min-h-[12px] min-w-[12px] -translate-y-2/4 translate-x-2/4 place-items-center rounded-full border-2 border-white bg-green-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
+        <span className="absolute right-[14%] top-[14%] grid min-h-[12px] min-w-[12px] -translate-y-2/4 translate-x-2/4 place-items-center rounded-full border border-white bg-green-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
       </div>
       <div className="relative inline-flex">
         <img
@@ -15,7 +15,7 @@ export function AvatarWithDotIndicator() {
           alt="avatar"
           className="relative inline-block h-12 w-12 rounded-lg object-cover object-center"
         />
-        <span className="absolute right-[2%] top-[4%] grid min-h-[12px] min-w-[12px] -translate-y-2/4 translate-x-2/4 place-items-center rounded-full border-2 border-white bg-green-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
+        <span className="absolute right-[2%] top-[4%] grid min-h-[12px] min-w-[12px] -translate-y-2/4 translate-x-2/4 place-items-center rounded-full border border-white bg-green-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
       </div>
       <div className="relative inline-flex">
         <img
@@ -23,7 +23,7 @@ export function AvatarWithDotIndicator() {
           alt="avatar"
           className="relative inline-block h-12 w-12 rounded-full object-cover object-center"
         />
-        <span className="absolute bottom-[14%] right-[14%] grid min-h-[12px] min-w-[12px] translate-x-2/4 translate-y-2/4 place-items-center rounded-full border-2 border-white bg-red-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
+        <span className="absolute bottom-[14%] right-[14%] grid min-h-[12px] min-w-[12px] translate-x-2/4 translate-y-2/4 place-items-center rounded-full border border-white bg-red-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
       </div>
       <div className="relative inline-flex">
         <img
@@ -31,7 +31,7 @@ export function AvatarWithDotIndicator() {
           alt="avatar"
           className="relative inline-block h-12 w-12 rounded-lg object-cover object-center"
         />
-        <span className="absolute bottom-[4%] right-[2%] grid min-h-[12px] min-w-[12px] translate-x-2/4 translate-y-2/4 place-items-center rounded-full border-2 border-white bg-red-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
+        <span className="absolute bottom-[4%] right-[2%] grid min-h-[12px] min-w-[12px] translate-x-2/4 translate-y-2/4 place-items-center rounded-full border border-white bg-red-500 px-1 py-1 text-xs font-medium leading-none text-white content-['']"></span>
       </div>
     </div>
   );
