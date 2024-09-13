b4220b1dd6e24284c94e69181e060645358b7c06
dragosct10 <dragos@creative-tim.com>
2024-09-13 17:05:53 +0300

    Create select-with-label-and-helper-text.tsx

---
+++ docs-content/html/select/select-with-label-and-helper-text.tsx
@@ -0,0 +1,37 @@
+export function SelectWithLabelAndHelperText() {
+  return (
+    <div className="w-full max-w-sm min-w-[200px]">      
+      <label className="block mb-1 text-sm text-slate-800">
+        Your Country
+      </label>
+
+      <div className="relative">
+        <select
+            className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+            <option value="brazil">Brazil</option>
+            <option value="bucharest">Bucharest</option>
+            <option value="london">London</option>
+            <option value="washington">Washington</option>
+        </select>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+        </svg>
+      </div>
+      <p className="flex items-center mt-2 text-xs text-slate-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-5 h-5 mr-2"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        Your location helps us to provide location-specific services.
+      </p>
+    </div>
+  );
+}
