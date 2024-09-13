817ed259af3c02b531c5178fa9ba38fd6159d378
dragosct10 <dragos@creative-tim.com>
2024-09-13 17:05:51 +0300

    Update select-with-label.tsx

--- docs-content/html/select/select-with-label.tsx
+++ docs-content/html/select/select-with-label.tsx
@@ -7,7 +7,7 @@ export function SelectWithLabel() {
       
       <div className="relative">
         <select
-            className="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+            className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
             <option value="brazil">Brazil</option>
             <option value="bucharest">Bucharest</option>
             <option value="london">London</option>
