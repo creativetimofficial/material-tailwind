34506cdde637fd4a31d43d2c6835f2ece26f7e19
dragosct10 <dragos@creative-tim.com>
2024-09-13 17:06:01 +0300

    Update select-disabled.tsx

--- docs-content/html/select/select-disabled.tsx
+++ docs-content/html/select/select-disabled.tsx
@@ -1,15 +1,19 @@
-import { Select, Option } from "@material-tailwind/react";
-
 export function SelectDisabled() {
   return (
-    <div className="w-72">
-      <Select label="Select Version" disabled>
-        <Option>Material Tailwind HTML</Option>
-        <Option>Material Tailwind React</Option>
-        <Option>Material Tailwind Vue</Option>
-        <Option>Material Tailwind Angular</Option>
-        <Option>Material Tailwind Svelte</Option>
-      </Select>
+    <div className="w-full max-w-sm min-w-[200px]">
+      <div className="relative">
+        <select
+            disabled
+            className="pointer-events-none opacity-50 w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+            <option value="brazil">Brazil</option>
+            <option value="bucharest">Bucharest</option>
+            <option value="london">London</option>
+            <option value="washington">Washington</option>
+        </select>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="opacity-50 h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+        </svg>
+      </div>
     </div>
   );
 }
