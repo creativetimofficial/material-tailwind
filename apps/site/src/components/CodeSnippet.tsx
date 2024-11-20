fdabdd4f57fa8d9857b9a77f2f28ed005c13e871
Beniamin Marcu <marqbeniamin@yahoo.com>
2024-11-20 14:52:03 +0200

    Update CodeSnippet.tsx

--- apps/site/src/components/CodeSnippet.tsx
+++ apps/site/src/components/CodeSnippet.tsx
@@ -1,12 +1,46 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+// @components
+import * as React from "react";
+import { Check, Copy } from "iconoir-react";
+import { Fira_Code } from "next/font/google";
+import { IconButton, Button, Tooltip } from "@material-tailwind/react";
 
-interface CodeSnippetProps {
-  codeContent: string;
-  language: string;
+// @hooks
+import { useTheme } from "next-themes";
+import { useCopyToClipboard } from "usehooks-ts";
+
+// @utils
+import { twMerge } from "tailwind-merge";
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+// Import the styles
+import { github, atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+// Import the languages
+import javascript from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
+import typescript from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+import html from 'react-syntax-highlighter/dist/esm/languages/hljs/xml';
+
+// Register the languages
+SyntaxHighlighter.registerLanguage('javascript', javascript);
+SyntaxHighlighter.registerLanguage('typescript', typescript);
+SyntaxHighlighter.registerLanguage('html', html);
+
+const firaCode = Fira_Code({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-fira-code",
+});
+
+const MIN_CODE_HEIGHT = 400;
+
+interface CodePreview {
+  codeBlock: any;
+  className?: string;
+  language?: string;
 }
-
 
 function getHtmlCodeContent(codeContent: string) {
   const regex = /__html:\s*`([\s\S]*?)`/;
@@ -21,19 +55,120 @@ function getHtmlCodeContent(codeContent: string) {
   }
 }
 
-function CodeSnippet({ codeContent, language }: CodeSnippetProps) {
+export function CodePreview({ codeBlock, className, language }: CodePreview) {
+  const { resolvedTheme } = useTheme();
+  const codeRef = React.useRef<any>(null);
+
+  const [, copy] = useCopyToClipboard();
+  const [isCopied, setIsCopied] = React.useState(false);
+  const [showCode, setShowCode] = React.useState(false);
+  const [codeHeight, setCodeHeight] = React.useState(0);
+
+  async function copyCode() {
+    setIsCopied(true);
+
+    const codeContent = language === "html" ? getHtmlCodeContent(codeBlock) : codeBlock;
+
+    copy(codeContent);
+  }
+
+  function resetCopy() {
+    setIsCopied(false);
+  }
+
+  const containerStyles = twMerge(
+    "border-surface rounded-lg border mt-4 lg:max-w-[calc(80rem-480px-2rem-52px)] max-w-full",
+    className,
+  );
+
+  const codeBlockStyles = twMerge(
+    firaCode.className,
+    "code-preview relative overflow-hidden",
+    showCode && "pb-12",
+  );
+
+  React.useEffect(() => {
+    const element = codeRef.current;
+  
+    if (element) {
+      const preElement = element.querySelector("pre");
+  
+      if (preElement) {
+        setCodeHeight(preElement.clientHeight);
+      }
+    }
+  
+    return () => {
+      setCodeHeight(0);
+    };
+  }, [resolvedTheme]);
+
+  React.useEffect(() => {
+    if (codeRef.current) {
+      const preElement = codeRef.current.querySelector("pre");
+  
+      if (preElement) {
+        setCodeHeight(preElement.clientHeight);
+      }
+    }
+  }, [codeBlock]);
+  
 
   return (
-    <pre className={`language-${language} p-4 bg-gray-100 rounded`}>
-      {
-        language === 'html' ? (
-          <code>{getHtmlCodeContent(codeContent)}</code> 
-        ) : (
-          <code>{codeContent}</code>
-        )
-      }
-    </pre>
+    <div className={containerStyles} data-theme={resolvedTheme}>
+      <div className={codeBlockStyles}>
+        <Tooltip>
+          <Tooltip.Trigger as="span" className="absolute right-1 top-1 z-10">
+            <IconButton
+              size="sm"
+              variant="ghost"
+              color="secondary"
+              ripple={false}
+              onClick={copyCode}
+              onMouseLeave={resetCopy}
+            >
+              {isCopied ? <Check /> : <Copy />}
+            </IconButton>
+          </Tooltip.Trigger>
+          <Tooltip.Content>
+            {isCopied ? "Copied" : "Copy"}
+            <Tooltip.Arrow />
+          </Tooltip.Content>
+        </Tooltip>
+        <div
+          ref={codeRef}
+          style={{
+            height: showCode
+              ? "auto"
+              : codeHeight <= MIN_CODE_HEIGHT
+                ? "unset"
+                : `${MIN_CODE_HEIGHT}px`,
+          }}
+        >
+          <SyntaxHighlighter
+            language={language}
+            style={resolvedTheme === 'dark' ? github : github}
+            showLineNumbers
+          >
+            {codeBlock && language === "html"
+              ? getHtmlCodeContent(codeBlock)
+              : codeBlock}
+          </SyntaxHighlighter>
+        </div>
+        {codeHeight > MIN_CODE_HEIGHT && (
+          <div className="absolute bottom-0 left-1/2 grid h-16 w-full -translate-x-1/2 items-end justify-center rounded-b-lg bg-gradient-to-t from-background via-background/75 to-transparent pb-2">
+            <Button
+              size="xs"
+              color="secondary"
+              onClick={() => setShowCode((cur) => !cur)}
+            >
+              {showCode ? "Hide Code" : "Show Code"}
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
   );
-};
+}
 
-export default CodeSnippet;
+export default CodePreview;
