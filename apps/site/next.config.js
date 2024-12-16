78640c82b53b1a626375a38219bb0f666d1f3c4f
Beniamin Marcu <marqbeniamin@yahoo.com>
2024-12-16 18:15:03 +0200

    Update next.config.js

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -17,75 +17,75 @@ const nextConfig = {
   },
   transpilePackages: ["next-mdx-remote"],
   assetPrefix: process.env.NEXT_PUBLIC_SITE_URL,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/docs/react/installation",
-        permanent: true,
-      },
-      {
-        source: "/documentation/quick-start",
-        destination: "/docs/react/installation",
-        permanent: true,
-      },
-      {
-        source: "/components",
-        destination: "/docs/react/button",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/theme/:slug*",
-        destination: "/docs/react/:slug*",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/props/:slug*",
-        destination: "/docs/react/:slug*",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/breadcrumbs",
-        destination: "/docs/react/breadcrumb",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/img",
-        destination: "/docs/react/image",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/progress-bar",
-        destination: "/docs/react/progress",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/radio-button",
-        destination: "/docs/react/radio",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/plugins/charts",
-        destination: "/docs/react/plugins/apexcharts",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/carousel",
-        destination: "/docs/react/plugins/swiper",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/form",
-        destination: "/docs/react/plugins/forms",
-        permanent: true,
-      },
-      {
-        source: "/docs/react/rating-bar",
-        destination: "/docs/react/rating",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/docs/react/installation",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/documentation/quick-start",
+  //       destination: "/docs/react/installation",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/components",
+  //       destination: "/docs/react/button",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/theme/:slug*",
+  //       destination: "/docs/react/:slug*",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/props/:slug*",
+  //       destination: "/docs/react/:slug*",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/breadcrumbs",
+  //       destination: "/docs/react/breadcrumb",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/img",
+  //       destination: "/docs/react/image",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/progress-bar",
+  //       destination: "/docs/react/progress",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/radio-button",
+  //       destination: "/docs/react/radio",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/plugins/charts",
+  //       destination: "/docs/react/plugins/apexcharts",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/carousel",
+  //       destination: "/docs/react/plugins/swiper",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/form",
+  //       destination: "/docs/react/plugins/forms",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/react/rating-bar",
+  //       destination: "/docs/react/rating",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
