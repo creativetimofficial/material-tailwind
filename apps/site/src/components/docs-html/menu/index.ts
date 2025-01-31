3658b5817bc3cd8e252bf872b2b5de85180b6a46
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-01-31 17:31:44 +0200

    WIP: sidebar and menu

---
+++ apps/site/src/components/docs-html/menu/index.ts
@@ -0,0 +1,12 @@
+"use client";;
+
+export * from "./menu-demo";
+export * from "./nested-menu";
+export * from "./nav-menu";
+export * from "./menu-with-divider";
+export * from "./menu-with-checkbox";
+export * from "./menu-with-search";
+export * from "./notification-menu";
+export * from "./profile-menu";
+export * from "./menu-with-scrolling-content";
+export * from "./menu-placement";
---
+++ apps/site/src/components/docs-html/sidebar/index.ts
@@ -0,0 +1,9 @@
+"use client";;
+
+export * from "./sidebar-demo";
+export * from "./multi-level-sidebar";
+export * from "./sidebar-with-content-separator";
+export * from "./sidebar-with-cta";
+export * from "./sidebar-with-logo";
+export * from "./sidebar-with-search";
+export * from "./sidebar-with-burger-menu";
