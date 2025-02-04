18bc8513e6d49720793e7dde125f9d363574516a
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-04 16:37:12 +0200

    added menu

--- apps/site/src/components/docs-html/menu/index.ts
+++ apps/site/src/components/docs-html/menu/index.ts
@@ -1,12 +1,8 @@
-"use client";;
-
+"use client";
 export * from "./menu-demo";
-export * from "./nested-menu";
-export * from "./nav-menu";
-export * from "./menu-with-divider";
-export * from "./menu-with-checkbox";
-export * from "./menu-with-search";
-export * from "./notification-menu";
-export * from "./profile-menu";
-export * from "./menu-with-scrolling-content";
 export * from "./menu-placement";
+export * from "./menu-with-checkbox";
+export * from "./menu-with-divider";
+export * from "./menu-with-scrolling-content";
+export * from "./nested-menu";
+export * from "./profile-menu";
