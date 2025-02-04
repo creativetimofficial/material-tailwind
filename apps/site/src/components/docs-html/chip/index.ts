7adcf3b796d344f65dcd496742616b3898ca0d25
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-04 16:09:48 +0200

    Update index.ts

--- apps/site/src/components/docs-html/chip/index.ts
+++ apps/site/src/components/docs-html/chip/index.ts
@@ -3,6 +3,7 @@ export * from "./chip-colors";
 export * from "./chip-demo";
 export * from "./chip-dismissible";
 export * from "./chip-icon";
+export * from "./chip-rounded";
 export * from "./chip-sizes";
 export * from "./chip-variants";
 export * from "./chip-with-avatar";
