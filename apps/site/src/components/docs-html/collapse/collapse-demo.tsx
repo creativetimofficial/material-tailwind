c8bf3203ff19677819f7747c4cc8215dc05e7d55
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-04 16:10:01 +0200

    Update collapse-demo.tsx

--- apps/site/src/components/docs-html/collapse/collapse-demo.tsx
+++ apps/site/src/components/docs-html/collapse/collapse-demo.tsx
@@ -1,18 +1,13 @@
-export default function CollapseDemo() {
-            return (
-              <div
-                className="w-full place-items-center"
-                dangerouslySetInnerHTML={{
-                  __html: 
+
+export function CollapseDemo() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<ul class="flex flex-col gap-0.5 min-w-60">
+  <li class="flex items-center py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in aria-disabled:opacity-50 aria-disabled:pointer-events-none bg-transparent text-slate-600 hover:text-slate-800 dark:hover:text-white hover:bg-slate-200 focus:bg-slate-200 focus:text-slate-800 dark:focus:text-white data-[selected=true]:bg-surface data-[selected=true]:text-black dark:data-[selected=true]:text-white dark:bg-opacity-70">Components<span class="grid place-items-center shrink-0 ps-2.5 ms-auto"><svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-5 w-5 stroke-[1.5] "><path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></svg></span></li>
+</ul>
 `
-<button data-dui-toggle="collapse" data-dui-target="#collapseButton" aria-expanded="false" aria-controls="collapseButton" class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed focus:shadow-none text-sm py-2 px-4 shadow-sm hover:shadow-md bg-stone-800 hover:bg-stone-700 relative bg-gradient-to-b from-stone-700 to-stone-800 border-stone-900 text-stone-50 rounded-lg hover:bg-gradient-to-b hover:from-stone-800 hover:to-stone-800 hover:border-stone-900 after:absolute after:inset-0 after:rounded-[inherit] after:box-shadow after:shadow-[inset_0_1px_0px_rgba(255,255,255,0.25),inset_0_-2px_0px_rgba(0,0,0,0.35)] after:pointer-events-none transition antialiased">Button with Collapse</button>
-<div class="overflow-hidden transition-[max-height] duration-300 ease-in-out max-h-0 mt-1" id="collapseButton">
-  <div class="bg-white border border-stone-200 rounded-lg shadow-sm p-4 z-10">
-    <p class="text-stone-500 text-sm">Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</p>
-  </div>
-</div>
-`
-                }}
-              />
-            );
-          }
+      }}
+    />
+  );
+}
