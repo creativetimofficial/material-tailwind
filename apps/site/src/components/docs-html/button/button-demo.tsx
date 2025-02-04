3d9df3ef8bd60aebb89fbdf722d9036b31a3e68b
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-04 17:03:08 +0200

    Update button-demo.tsx

--- apps/site/src/components/docs-html/button/button-demo.tsx
+++ apps/site/src/components/docs-html/button/button-demo.tsx
@@ -4,7 +4,7 @@ export default function ButtonDemo() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<button data-ripple-light="true" class="inline-flex select-none items-center justify-center rounded-md border border-slate-800 bg-slate-800 px-3.5 py-2.5 text-center align-middle font-sans text-sm font-medium leading-none text-slate-50 transition-all duration-300 ease-in hover:border-slate-700 hover:bg-slate-700 disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">Button</button>
+        __html: `<button class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-base rounded-md py-2.5 px-5 shadow-sm hover:shadow-lg bg-slate-800 border-slate-800 text-slate-50 hover:bg-slate-700 hover:border-slate-700 shrink-0" data-shape="default" data-width="default">Button</button>
 `
       }}
     />
