6d57920e14c0f4094e2668f733260c5aa87dbe0e
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-04 16:18:14 +0200

    Create index.ts

---
+++ apps/site/src/components/docs-html/dialog/index.ts
@@ -0,0 +1,7 @@
+"use client";
+export * from "./dialog-demo";
+export * from "./dialog-long";
+export * from "./dialog-notification";
+export * from "./dialog-sizes";
+export * from "./dialog-web3";
+export * from "./dialog-with-image";
