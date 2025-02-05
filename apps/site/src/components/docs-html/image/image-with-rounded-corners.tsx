42ab68fa5390af206cc0cca72ca8388546177e60
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-05 17:38:50 +0200

    update images

--- apps/site/src/components/docs-html/image/image-with-rounded-corners.tsx
+++ apps/site/src/components/docs-html/image/image-with-rounded-corners.tsx
@@ -4,7 +4,7 @@ export default function ImageWithRoundedCorners() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<img src="https://dub.sh/Y0NxRWv" alt="nature-image" class="h-96 w-full rounded-lg object-cover object-center" />
+        __html: `<img src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80"  alt="nature-image" class="h-96 w-full rounded-lg object-cover object-center" />
 `
       }}
     />
--- apps/site/src/components/docs/image/image-with-caption.tsx
+++ apps/site/src/components/docs/image/image-with-caption.tsx
@@ -4,7 +4,7 @@ export function ImageWithCaption() {
   return (
     <figure>
       <img
-        src="https://dub.sh/Y0NxRWv"
+        src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2832&amp;q=80" 
         alt="nature-image"
         className="h-96 w-full rounded-lg object-cover object-center"
       />
