f96fe8c20c14d09a338010354744125db67dbd4b
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-05 11:26:29 +0200

    Update alert-colors.tsx

--- apps/site/src/components/docs-html/alert/alert-colors.tsx
+++ apps/site/src/components/docs-html/alert/alert-colors.tsx
@@ -6,24 +6,25 @@ export default function AlertColors() {
       dangerouslySetInnerHTML={{
         __html: `<div class="w-full space-y-2">
   <div role="alert" class="relative flex w-full items-start rounded-md border border-slate-800 bg-slate-800 p-2 text-slate-50">
-    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with color=&quot;primary&quot;</div>
+    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with primary color</div>
   </div>
   <div role="alert" class="relative flex w-full items-start rounded-md border border-slate-200 bg-slate-200 p-2 text-slate-800">
-    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with color=&quot;secondary&quot;</div>
+    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with secondary color</div>
   </div>
   <div role="alert" class="relative flex w-full items-start rounded-md border border-blue-500 bg-blue-500 p-2 text-blue-50">
-    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with color=&quot;info&quot;</div>
+    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with info color</div>
   </div>
   <div role="alert" class="relative flex w-full items-start rounded-md border border-green-500 bg-green-500 p-2 text-green-50">
-    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with color=&quot;success&quot;</div>
+    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with success color</div>
   </div>
   <div role="alert" class="relative flex w-full items-start rounded-md border border-amber-500 bg-amber-500 p-2 text-amber-50">
-    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with color=&quot;warning&quot;</div>
+    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with warning color</div>
   </div>
   <div role="alert" class="relative flex w-full items-start rounded-md border border-red-500 bg-red-500 p-2 text-red-50">
-    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with color=&quot;error&quot;</div>
+    <div class="m-1.5 w-full font-sans text-base leading-none">A simple alert for showing message, with error color</div>
   </div>
 </div>
+
 `
       }}
     />
