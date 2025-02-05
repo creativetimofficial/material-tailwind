b36ec780ed68792bf3212148f86c78980ebb0253
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-05 17:01:54 +0200

    update breadcrumbs

--- apps/site/src/components/docs-html/breadcrumb/breadcrumb-custom-separator.tsx
+++ apps/site/src/components/docs-html/breadcrumb/breadcrumb-custom-separator.tsx
@@ -4,7 +4,13 @@ export default function BreadcrumbCustomSeparator() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<nav class="flex flex-wrap items-center gap-1 p-1"><a href="/docs" class="inline-flex items-center gap-1.5 text-sm text-slate-800 transition-colors duration-300 ease-in hover:text-primary dark:text-white dark:hover:text-primary">Docs</a><span class="inline-block mx-1 text-sm text-slate-800 opacity-50 pointer-events-none select-none dark:text-white">-</span><a href="/docs/components" class="inline-flex items-center gap-1.5 text-sm text-slate-800 transition-colors duration-300 ease-in hover:text-primary dark:text-white dark:hover:text-primary">Components</a><span class="inline-block mx-1 text-sm text-slate-800 opacity-50 pointer-events-none select-none dark:text-white">-</span><a href="/docs/components/breadcrumb" class="inline-flex items-center gap-1.5 text-sm text-slate-800 transition-colors duration-300 ease-in hover:text-primary dark:text-white dark:hover:text-primary">Breadcrumb</a></nav>
+        __html: `<nav class="flex flex-wrap items-center gap-1 p-1">
+    <a href="#" class="inline-flex items-center gap-1.5 text-sm text-slate-800 transition-colors duration-300 ease-in hover:text-primary dark:text-white dark:hover:text-primary">Docs</a>
+    <span class="inline-block mx-1 text-sm text-slate-800 opacity-50 pointer-events-none select-none dark:text-white">-</span>
+    <a href="#" class="inline-flex items-center gap-1.5 text-sm text-slate-800 transition-colors duration-300 ease-in hover:text-primary dark:text-white dark:hover:text-primary">Components</a>
+    <span class="inline-block mx-1 text-sm text-slate-800 opacity-50 pointer-events-none select-none dark:text-white">-</span>
+    <a href="#" class="inline-flex items-center gap-1.5 text-sm text-slate-800 transition-colors duration-300 ease-in hover:text-primary dark:text-white dark:hover:text-primary">Breadcrumb</a>
+</nav>
 `
       }}
     />
