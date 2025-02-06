605e0e736d59b84d318063bbc27d96595f489573
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-06 15:55:51 +0200

    update menu

--- apps/site/src/components/docs-html/menu/profile-menu.tsx
+++ apps/site/src/components/docs-html/menu/profile-menu.tsx
@@ -2,6 +2,7 @@
 export default function ProfileMenu() {
   return (
     <div
+      className="w-full place-items-center"
       dangerouslySetInnerHTML={{
         __html: `<img data-open="false" data-nested="false" aria-expanded="false" aria-haspopup="menu" id=":R0H2:" src="https://dub.sh/iu8bOfU" alt="profile-picture" class="inline-block object-cover object-center data-[shape=square]:rounded-none data-[shape=circular]:rounded-full data-[shape=rounded]:rounded-[current] w-11 h-11 rounded-md outline-none group" data-shape="circular" />
 `
