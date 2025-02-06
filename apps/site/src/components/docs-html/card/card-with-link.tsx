016868d096c2bdb4ed7f10586ae14c40082b0a3d
Alexandru Paduraru <axelut@yahoo.com>
2025-02-06 21:58:28 +0200

    Update card-with-link.tsx

--- apps/site/src/components/docs-html/card/card-with-link.tsx
+++ apps/site/src/components/docs-html/card/card-with-link.tsx
@@ -29,7 +29,7 @@ export default function CardWithLink() {
     </p>
   </div>
   <div class="w-full rounded px-3 pb-3 pt-1.5">
-    <a href="#" class="inline-flex w-full rounded-md border border-slate-800 bg-slate-800 px-4 py-2 text-center font-sans text-sm font-medium text-slate-50 transition-all duration-300 ease-in hover:border-slate-700 hover:bg-slate-700 disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">
+    <a href="#" class="block w-full rounded-md border border-slate-800 bg-slate-800 px-4 py-2 text-center font-sans text-sm font-medium text-slate-50 transition-all duration-300 ease-in hover:border-slate-700 hover:bg-slate-700 disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">
       Read More
     </a>
   </div>
