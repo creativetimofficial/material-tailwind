649c08ff2359129628646045b044f8cb1aeb0e3b
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-06 15:20:48 +0200

    regenerate accordion

--- apps/site/src/components/docs-html/accordion/accordion-demo.tsx
+++ apps/site/src/components/docs-html/accordion/accordion-demo.tsx
@@ -4,23 +4,15 @@ export default function AccordionDemo() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<div class="w-full">
-  <div id="accordion-demo" data-open="true" class="items-center justify-between block w-full border-b cursor-pointer group border-slate-200"><button data-open="true" class="flex items-center justify-between w-full py-3 font-medium text-left text-slate-800 dark:text-white">Material Tailwind React<svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-4 w-4 group-data-[open=true]:rotate-180">
-        <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-      </svg></button>
-    <div class="block h-auto pb-3 text-slate-600" data-content="true" data-open="true">Material Tailwind is an open-source crafted in Tailwind CSS. Get Material Tailwind and take advantage of its free components and features that will help you set up your web project quickly.</div>
-  </div>
-  <div id="accordion-demo2" data-open="false" class="items-center justify-between block w-full border-b cursor-pointer group border-slate-200"><button data-open="false" class="flex items-center justify-between w-full py-3 font-medium text-left text-slate-800 dark:text-white">Material Tailwind HTML<svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-4 w-4 group-data-[open=true]:rotate-180">
-        <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-      </svg></button>
-    <div class="hidden h-0 pb-3 text-slate-600" data-content="true" data-open="false">Material Tailwind is an open-source crafted in Tailwind CSS. Get Material Tailwind and take advantage of its free components and features that will help you set up your web project quickly.</div>
-  </div>
-  <div id="accordion-demo3" data-open="false" class="items-center justify-between block w-full border-b cursor-pointer group border-slate-200"><button data-open="false" class="flex items-center justify-between w-full py-3 font-medium text-left text-slate-800 dark:text-white">Material Tailwind Vue<svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-4 w-4 group-data-[open=true]:rotate-180">
-        <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-      </svg></button>
-    <div class="hidden h-0 pb-3 text-slate-600" data-content="true" data-open="false">Material Tailwind is an open-source crafted in Tailwind CSS. Get Material Tailwind and take advantage of its free components and features that will help you set up your web project quickly.</div>
-  </div>
-</div>
+        __html: `<div data-open="false" class="group block w-full items-center justify-between cursor-pointer border-b border-slate-200 aria-disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:select-none" aria-disabled="false"><button data-open="false" class="flex items-center justify-between w-full py-5 text-left font-medium dark:text-white text-slate-800">Material Tailwind React<svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-4 w-4 group-data-[open=true]:rotate-180">
+      <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg></button></div>
+<div data-open="false" class="group block w-full items-center justify-between cursor-pointer border-b border-slate-200 aria-disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:select-none" aria-disabled="false"><button data-open="false" class="flex items-center justify-between w-full py-5 text-left font-medium dark:text-white text-slate-800">Material Tailwind HTML<svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-4 w-4 group-data-[open=true]:rotate-180">
+      <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg></button></div>
+<div data-open="false" class="group block w-full items-center justify-between cursor-pointer border-b border-slate-200 aria-disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:select-none" aria-disabled="false"><button data-open="false" class="flex items-center justify-between w-full py-5 text-left font-medium dark:text-white text-slate-800">Material Tailwind Vue<svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" class="h-4 w-4 group-data-[open=true]:rotate-180">
+      <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg></button></div>
 `
       }}
     />
