7b4afa1283b60a4f97cdef581573feb64e31c8f1
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-06 15:09:41 +0200

    regenerate cards

--- apps/site/src/components/docs-html/card/ecommerce-card.tsx
+++ apps/site/src/components/docs-html/card/ecommerce-card.tsx
@@ -1,18 +1,17 @@
 
-
-export default function EcommerceCard() {
+export function EcommerceCard() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<div class="w-96 overflow-hidden rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-950/5"><img src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80" alt="card-image" class="m-1.5 h-96 w-[calc(100%-12px)] rounded-[5px] object-cover" />
-  <div class="h-max w-full rounded px-3 py-2">
+        __html: `<div class="rounded-lg border shadow-sm overflow-hidden bg-white border-slate-200 shadow-slate-950/5 w-96"><img src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80" alt="card-image" class="w-[calc(100%-16px)] rounded m-2 h-96 object-cover" />
+  <div class="w-full h-max rounded px-3.5 py-2.5">
     <div class="mb-2 flex items-center justify-between">
-      <h6 class="font-sans text-base font-bold text-current antialiased md:text-lg lg:text-xl">Apple AirPods</h6>
-      <h6 class="font-sans text-base font-bold text-current antialiased md:text-lg lg:text-xl">$95.00</h6>
+      <h6 class="font-sans antialiased font-bold text-base md:text-lg lg:text-xl text-current">Apple AirPods</h6>
+      <h6 class="font-sans antialiased font-bold text-base md:text-lg lg:text-xl text-current">$95.00</h6>
     </div>
-    <p class="font-sans text-base text-slate-600 antialiased">With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.</p>
+    <p class="font-sans antialiased text-base text-slate-600">With plenty of talk and listen time, voice-activated Siri access, and an available wireless charging case.</p>
   </div>
-  <div class="w-full rounded px-3 pb-3 pt-1.5"><button class="inline-flex w-full select-none items-center justify-center rounded-md border border-slate-200 bg-slate-200 px-3.5 py-2.5 text-center align-middle font-sans text-sm font-medium leading-none text-slate-800 transition-all duration-300 ease-in hover:bg-slate-100 hover:bg-slate-100 disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none">Add to Cart</button></div>
+  <div class="w-full px-3.5 pt-2 pb-3.5 rounded"><button class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-2 px-4 shadow-sm hover:shadow-md bg-slate-200 border-slate-200 text-slate-800 hover:bg-slate-100 hover:bg-slate-100" data-shape="default" data-width="full">Add to Cart</button></div>
 </div>
 `
       }}
--- apps/site/src/components/docs-html/card/testimonial-card.tsx
+++ apps/site/src/components/docs-html/card/testimonial-card.tsx
@@ -1,15 +1,14 @@
 
-
-export default function TestimonialCard() {
+export function TestimonialCard() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<div class="w-full max-w-[26rem] overflow-hidden rounded-lg border border-none border-slate-200 bg-white shadow-none shadow-slate-950/5">
-  <div class="m-1.5 mx-0 flex h-max w-[calc(100%-12px)] items-center gap-4 rounded-[5px] pb-4 pt-0"><img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80" alt="tania andrew" class="inline-block h-14 w-14 rounded-md object-cover object-center" />
+        __html: `<div class="rounded-lg border overflow-hidden bg-white border-slate-200 shadow-slate-950/5 w-full max-w-[26rem] border-none shadow-none">
+  <div class="w-[calc(100%-16px)] h-max rounded m-2 mx-0 flex items-center gap-4 pb-4 pt-0"><img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80" alt="tania andrew" class="inline-block object-cover object-center data-[shape=square]:rounded-none data-[shape=circular]:rounded-full data-[shape=rounded]:rounded-[current] w-14 h-14 rounded-md" data-shape="rounded" />
     <div class="flex w-full flex-col gap-0.5">
       <div class="flex items-center justify-between">
-        <h6 class="font-sans text-base font-bold text-current antialiased md:text-lg lg:text-xl">Tania Andrew</h6>
-        <div class="inline-flex items-center text-amber-500"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" data-slot="icon">
+        <h6 class="font-sans antialiased font-bold text-base md:text-lg lg:text-xl text-current">Tania Andrew</h6>
+        <div class="inline-flex items-center [&amp;_data-slot=icon]:w-5 [&amp;_data-slot=icon]:h-5 [&amp;_data-slot=icon]:text-inherit [&amp;_data-slot=icon]:cursor-pointer text-amber-500"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" data-slot="icon">
             <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd"></path>
           </svg><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" data-slot="icon">
             <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd"></path>
@@ -21,11 +20,11 @@ export default function TestimonialCard() {
             <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd"></path>
           </svg></div>
       </div>
-      <p class="font-sans text-base text-current antialiased">Frontend Lead @ Google</p>
+      <p class="font-sans antialiased text-base text-current">Frontend Lead @ Google</p>
     </div>
   </div>
-  <div class="h-max w-full rounded p-0">
-    <p class="font-sans text-base text-slate-600 antialiased">&quot;I found solution to all my design needs from Creative Tim. I use them as a freelancer in my hobby projects for fun! And its really affordable, very humble guys !!!&quot;</p>
+  <div class="w-full h-max rounded p-0">
+    <p class="font-sans antialiased text-base text-slate-600">&quot;I found solution to all my design needs from Creative Tim. I use them as a freelancer in my hobby projects for fun! And its really affordable, very humble guys !!!&quot;</p>
   </div>
 </div>
 `
