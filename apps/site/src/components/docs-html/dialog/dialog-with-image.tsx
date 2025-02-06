3d0c152dc3a25ca8b8f8805569b8a0da969a6ac3
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-06 16:43:02 +0200

    update dialog

--- apps/site/src/components/docs-html/dialog/dialog-with-image.tsx
+++ apps/site/src/components/docs-html/dialog/dialog-with-image.tsx
@@ -2,6 +2,7 @@
 export default function DialogWithImage() {
   return (
     <div
+      className="w-full place-items-center"
       dangerouslySetInnerHTML={{
         __html: `<button data-open="false" class="outline-none" aria-expanded="false" aria-haspopup="dialog"><img alt="nature" src="https://dub.sh/xxo2Jek" class="h-64 w-auto rounded-lg object-cover object-center" /></button>
 `
--- apps/site/src/components/docs-html/dialog/index.ts
+++ apps/site/src/components/docs-html/dialog/index.ts
@@ -1,7 +1,9 @@
 "use client";
 export * from "./dialog-demo";
 export * from "./dialog-long";
+export * from "./dialog-message";
 export * from "./dialog-notification";
 export * from "./dialog-sizes";
 export * from "./dialog-web3";
+export * from "./dialog-with-form";
 export * from "./dialog-with-image";
