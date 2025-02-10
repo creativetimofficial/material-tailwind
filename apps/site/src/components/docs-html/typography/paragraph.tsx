2072d9f3c8ce869e728c0a085e434c008be8f658
Alexandru Paduraru <axelut@yahoo.com>
2025-02-10 18:32:14 +0200

    adjustments typography

--- apps/site/src/components/docs-html/typography/paragraph.tsx
+++ apps/site/src/components/docs-html/typography/paragraph.tsx
@@ -4,7 +4,7 @@ export default function Paragraph() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<p class="font-sans text-base text-current antialiased">Material Tailwind is an easy to use components library for Tailwind CSS and Material Design. It provides a simple way to customize your components, you can change the colors, fonts, breakpoints and everything you need.</p>
+        __html: `<p class="font-sans text-base antialiased">Material Tailwind is an easy to use components library for Tailwind CSS and Material Design. It provides a simple way to customize your components, you can change the colors, fonts, breakpoints and everything you need.</p>
 `
       }}
     />
