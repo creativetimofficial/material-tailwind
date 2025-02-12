bf5d1efc038e784fdfa10e3d20cf8d5cdfad32ce
Beniamin Marcu <marqbeniamin@yahoo.com>
2025-02-12 12:38:55 +0200

    inport checkbox in mdx

--- apps/site/src/components/docs/checkbox/checkbox-with-label.tsx
+++ apps/site/src/components/docs/checkbox/checkbox-with-label.tsx
@@ -1,6 +1,6 @@
 import { Checkbox, Typography } from "@material-tailwind/react";
 
-export function CheckboxWithLabel() {
+export default function CheckboxWithLabel() {
   return (
     <div className="flex items-center gap-2">
       <Checkbox id="checkbox">
--- apps/site/src/components/docs/checkbox/custom-checkbox.tsx
+++ apps/site/src/components/docs/checkbox/custom-checkbox.tsx
@@ -1,6 +1,6 @@
 import { Checkbox } from "@material-tailwind/react";
 
-export function CustomCheckbox() {
+export default function CustomCheckbox() {
   return (
     <Checkbox className="rounded-full">
       <Checkbox.Indicator className="scale-75 data-[checked=true]:scale-90" />
