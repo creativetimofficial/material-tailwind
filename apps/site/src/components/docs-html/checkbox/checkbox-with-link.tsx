f94c94c6261e2b40de0f9bdf4f1b6b4553879f5d
dragosct10 <dragos@creative-tim.com>
2025-02-12 15:30:18 +0200

    Update checkbox-with-link.tsx

--- apps/site/src/components/docs-html/checkbox/checkbox-with-link.tsx
+++ apps/site/src/components/docs-html/checkbox/checkbox-with-link.tsx
@@ -6,7 +6,7 @@ export default function CheckboxWithLink() {
       dangerouslySetInnerHTML={{
         __html: `<div class="flex items-center gap-2">
   <label class="flex items-center cursor-pointer relative" for="checkbox-with-link">
-    <input type="checkbox" class="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow-sm hover:shadow-md border border-slate-200 checked:bg-slate-800 checked:border-slate-800" id="checkbox-with-link" />
+    <input type="checkbox" class="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow-sm  border border-slate-200 checked:bg-slate-800 checked:border-slate-800" id="checkbox-with-link" />
     <span class="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <svg fill="none" width="18px" height="18px" stroke-width="2" color="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5 13L9 17L19 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
     </span>
