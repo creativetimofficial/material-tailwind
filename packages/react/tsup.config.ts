63caeda5f389055198aeebd1f637fd94bc7d262c
dragosct10 <dragos@creative-tim.com>
2025-07-30 12:16:15 +0300

    Update tsup.config.ts

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -2,5 +2,28 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   clean: true,
+  dts: {
+    resolve: true,
+  },
   format: ["cjs", "esm"],
+  minify: true,
+  sourcemap: false,
+  splitting: false,
+  treeshake: true,
+  external: ["react", "react-dom"],
+  noExternal: ["tailwindcss", "hex-rgb", "deepmerge"],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client";',
+    };
+    options.alias = {
+      "@context": "./src/context",
+      "@plugins": "./src/plugins",
+      "@theme": "./src/theme",
+      "@types": "./src/types",
+      "@utils": "./src/utils",
+      "@components": "./src/components"
+    };
+  },
 });
+
